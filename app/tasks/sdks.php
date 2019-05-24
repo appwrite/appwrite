fe06d71e980b837430f512c74d716df19a7bc01d
eldadfux <eldad.fux@gmail.com>
2019-05-24 08:27:16 +0300

    Updated SDK generator task

--- app/tasks/sdks.php
+++ app/tasks/sdks.php
@@ -1,20 +1,21 @@
 #!/bin/env php
 <?php
 
-require_once __DIR__ . '/../../../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Utopia\CLI\CLI;
 use Utopia\CLI\Console;
-use Spec\Swagger2;
-use SDK\SDK;
-use SDK\Language\PHP;
-use SDK\Language\JS;
+use Appwrite\Spec\Swagger2;
+use Appwrite\SDK\SDK;
+use Appwrite\SDK\Language\PHP;
+use Appwrite\SDK\Language\JS;
+use Appwrite\SDK\Language\Python;
+use Appwrite\SDK\Language\Ruby;
 
 $cli = new CLI();
 
 $cli
     ->task('generate')
-    //->param('language', '', new Text(64), 'Version Name', false)
     ->action(function () {
 
         function getSSLPage($url) {
@@ -31,6 +32,7 @@ $cli
 
         Console::success('Fetching API Spec');
 
+        $spec   = getSSLPage('https://appwrite.test/v1/open-api-2.json');
         $spec   = getSSLPage('https://appwrite.io/v1/open-api-2.json');
 
         $clients = [
@@ -50,6 +52,22 @@ $cli
                 'gitRepoName'   => 'sdk-for-js',
                 'gitUserName'   => 'appwrite',
             ],
+            'python' => [
+                'version'       => 'v1.0.0',
+                'result'        => __DIR__ . '/../sdks/python/',
+                'gitURL'        => 'https://github.com/appwrite/sdk-for-python.git',
+                'gitRepo'       => 'git@github.com:appwrite/sdk-for-python.git',
+                'gitRepoName'   => 'sdk-for-python',
+                'gitUserName'   => 'appwrite',
+            ],
+            'ruby' => [
+                'version'       => 'v1.0.0',
+                'result'        => __DIR__ . '/../sdks/ruby/',
+                'gitURL'        => 'https://github.com/appwrite/sdk-for-ruby.git',
+                'gitRepo'       => 'git@github.com:appwrite/sdk-for-ruby.git',
+                'gitRepoName'   => 'sdk-for-ruby',
+                'gitUserName'   => 'appwrite',
+            ],
         ];
 
         foreach ($clients as $name => $client) {
@@ -60,17 +78,29 @@ $cli
                         ->setComposerVendor('appwrite')
                         ->setComposerPackage('appwrite')
                     ;
-                break;
+                    break;
                 case 'js':
                     $language = new JS();
                     $language
                         ->setNPMPackage('appwrite')
                         ->setBowerPackage('appwrite')
                     ;
-                break;
+                    break;
+                case 'python':
+                    $language = new Python();
+                    $language
+                        ->setPipPackage('appwrite')
+                    ;
+                    break;
+                case 'ruby':
+                    $language = new Ruby();
+                    $language
+                        ->setGemPackage('appwrite')
+                    ;
+                    break;
                 default:
                     throw new Exception('Language not supported');
-                break;
+                    break;
             }
 
             $sdk = new SDK($language, new Swagger2($spec));
@@ -103,12 +133,13 @@ SOFTWARE.")
                 ->setGitURL($client['gitURL'])
                 ->setGitRepoName($client['gitRepoName'])
                 ->setGitUserName($client['gitUserName'])
-                ->setLogo('https://appwrite.io/images/github.png')
+                ->setLogo('https://appwrite.io/v1/images/github.png')
                 ->setURL('https://appwrite.io')
                 ->setShareText('Appwrite is a backend as a service for building web or mobile apps')
                 ->setShareURL('http://appwrite.io')
                 ->setShareTags('JS,javascript,reactjs,angular,ios,android')
                 ->setShareVia('appwrite_io')
+                ->setWarning('**WORK IN PROGRESS - NOT READY FOR USAGE**')
             ;
 
             $target = __DIR__ . '/../sdks/git/' . $name;
