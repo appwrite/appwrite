52da391f1534a5b04be95384866af20955d7d3a0
eldadfux <eldad.fux@gmail.com>
2019-08-03 12:57:50 +0300

    Updated SDKs logo paths

--- app/config/sdks.php
+++ app/config/sdks.php
@@ -4,89 +4,89 @@ return [
     [
         'label' => 'JS',
         'versions' => ['v1.0.0'],
-        'logo' => '/images/platforms/js.svg',
+        'logo' => 'js.svg',
         'link' => 'https://github.com/appwrite/sdk-for-js',
     ],
     [
         'label' => 'PHP',
         'versions' => ['v1.0.0'],
-        'logo' => '/images/platforms/php.svg',
+        'logo' => 'php.svg',
         'link' => 'https://github.com/appwrite/sdk-for-php',
     ],
     [
         'label' => 'Ruby',
         'versions' => [],
-        'logo' => '/images/platforms/ruby.svg',
+        'logo' => 'ruby.svg',
         'link' => 'https://...',
         'tag' => 'Beta'
     ],
     [
         'label' => 'Node.js',
         'versions' => [],
-        'logo' => '/images/platforms/nodejs.svg',
+        'logo' => 'nodejs.svg',
         'link' => 'https://...',
         'tag' => 'Soon'
     ],
     [
         'label' => 'Go',
         'versions' => [],
-        'logo' => '/images/platforms/go.svg',
+        'logo' => 'go.svg',
         'link' => 'https://...',
         'tag' => 'Soon'
     ],
     [
         'label' => 'Scala',
         'versions' => [],
-        'logo' => '/images/platforms/scala.svg',
+        'logo' => 'scala.svg',
         'link' => 'https://...',
         'tag' => 'Soon'
     ],
     [
         'label' => 'Swift',
         'versions' => [],
-        'logo' => '/images/platforms/swift.svg',
+        'logo' => 'swift.svg',
         'link' => 'https://...',
         'tag' => 'Soon'
     ],
     [
         'label' => 'C',
         'versions' => [],
-        'logo' => '/images/platforms/c.svg',
+        'logo' => 'c.svg',
         'link' => 'https://...',
         'tag' => 'Soon'
     ],
     [
         'label' => '.NET',
         'versions' => [],
-        'logo' => '/images/platforms/dotnet.svg',
+        'logo' => 'dotnet.svg',
         'link' => 'https://...',
         'tag' => 'Soon'
     ],
     [
         'label' => 'Erlang',
         'versions' => [],
-        'logo' => '/images/platforms/erlang.svg',
+        'logo' => 'erlang.svg',
         'link' => 'https://...',
         'tag' => 'Soon'
     ],
     [
         'label' => 'Python',
         'versions' => [],
-        'logo' => '/images/platforms/python.svg',
+        'logo' => 'python.svg',
         'link' => 'https://...',
         'tag' => 'Soon'
     ],
     [
         'label' => 'Kotlin',
         'versions' => [],
-        'logo' => '/images/platforms/kotlin.svg',
+        'logo' => 'kotlin.svg',
         'link' => 'https://...',
         'tag' => 'Soon'
     ],
     [
         'label' => 'Java',
         'versions' => [],
-        'logo' => '/images/platforms/java.svg',
+        'logo' => 'java.svg',
         'link' => 'https://...',
         'tag' => 'Soon'
     ],
