7d1a2c6d3aa74358b7a0eab0bededf8aa12257e8
eldadfux <eldad.fux@gmail.com>
2019-08-07 10:02:12 +0300

    Enabled stats containers

--- public/scripts/views/service.js
+++ public/scripts/views/service.js
@@ -18,10 +18,52 @@
                 if (debug) console.log('%c[service init]: ' + action + ' (' + service + ')', 'color:red');
 
                 let resolve = function(target) {
-                    let FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
-                    let text = target.toString();
-                    let args = text.match(FN_ARGS)[1].split(',');
                     let data = ('FORM' === element.tagName) ? form.toJson(element) : {};
+
+                    const REGEX_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+                    const REGEX_FUNCTION_PARAMS = /(?:\s*(?:function\s*[^(]*)?\s*)((?:[^'"]|(?:(?:(['"])(?:(?:.*?[^\\]\2)|\2))))*?)\s*(?=(?:=>)|{)/m;
+                    const REGEX_PARAMETERS_VALUES = /\s*([\w\\$]+)\s*(?:=\s*((?:(?:(['"])(?:\3|(?:.*?[^\\]\3)))((\s*\+\s*)(?:(?:(['"])(?:\6|(?:.*?[^\\]\6)))|(?:[\w$]*)))*)|.*?))?\s*(?:,|$)/gm;
+
+                    /**
+                     * Original Solution From:
+                     * @see https://stackoverflow.com/a/41322698/2299554
+                     *  Notice: this version add support for $ sign in arg name.
+                     *
+                     * Retrieve a function's parameter names and default values
+                     * Notes:
+                     *  - parameters with default values will not show up in transpiler code (Babel) because the parameter is removed from the function.
+                     *  - does NOT support inline arrow functions as default values
+                     *      to clarify: ( name = "string", add = defaultAddFunction )   - is ok
+                     *                  ( name = "string", add = ( a )=> a + 1 )        - is NOT ok
+                     *  - does NOT support default string value that are appended with a non-standard ( word characters or $ ) variable name
+                     *      to clarify: ( name = "string" + b )         - is ok
+                     *                  ( name = "string" + $b )        - is ok
+                     *                  ( name = "string" + b + "!" )   - is ok
+                     *                  ( name = "string" + λ )         - is NOT ok
+                     * @param {function} func
+                     * @returns {Array} - An array of the given function's parameter [key, default value] pairs.
+                     */
+                    function getParams(func) {
+                        let functionAsString = func.toString();
+                        let params = [];
+                        let match;
+
+                        functionAsString = functionAsString.replace(REGEX_COMMENTS, '');
+                        functionAsString = functionAsString.match(REGEX_FUNCTION_PARAMS)[1];
+
+                        if (functionAsString.charAt(0) === '(') {
+                            functionAsString = functionAsString.slice(1, -1);
+                        }
+
+                        while (match = REGEX_PARAMETERS_VALUES.exec(functionAsString)) {
+                            //params.push([match[1], match[2]]); // with default values
+                            params.push(match[1]); // only with arg name
+                        }
+
+                        return params;
+                    }
+
+                    let args = getParams(target);
 
                     if (debug) console.log('%c[form data]: ', 'color:green', data);
 
