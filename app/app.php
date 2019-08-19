0cb118b2a591ea65e82042744e9a9c23ef09c33d
eldadfux <eldad.fux@gmail.com>
2019-08-19 23:55:41 +0300

    Updated docs

--- app/app.php
+++ app/app.php
@@ -474,12 +474,7 @@ $utopia->get('/v1/open-api-2.json')
             }
 
             function fromCamelCaseToDash($input) {
-                preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
-                $ret = $matches[0];
-                foreach ($ret as &$match) {
-                    $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
-                }
-                return implode('-', $ret);
+                return str_replace([' ', '_'], '-', strtolower(preg_replace('/([a-zA-Z])(?=[A-Z])/', '$1-', $input)));
             }
 
             /*$scopes = [
