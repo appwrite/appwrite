83812578a02585ee9ad0b391ae3c79f87a3114d6
Bogdan Covrig <bogcov@gmail.com>
2019-09-28 03:37:39 +0200

    Add RO locale continents

---
+++ app/config/locale/ro.continents.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'AF' => 'Africa',
+    'AN' => 'Antarctica',
+    'AS' => 'Asia',
+    'EU' => 'Europa',
+    'NA' => 'America de Nord',
+    'OC' => 'Oceania',
+    'SA' => 'America de Sud',
+];
