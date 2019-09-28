fd5673c2b42396e9aa9179a9322ac15ce57327c8
eldadfux <eldad.fux@gmail.com>
2019-09-28 23:28:44 +0300

    Debug tests

--- tests/e2e/ConsoleHealthTest.php
+++ tests/e2e/ConsoleHealthTest.php
@@ -13,6 +13,8 @@ class ConsoleHealthTest extends BaseConsole
             'content-type' => 'application/json',
         ], []);
 
+        var_dump($response);
+
         $this->assertEquals(200, $response['headers']['status-code']);
         $this->assertEquals('OK', $response['body']['status']);
     }
