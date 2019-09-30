b74ffc20c4feb79bef59362e60eba4b44aab801e
Kolkies <stijn@kolkies.dev>
2019-09-30 14:54:56 +0000

    Add nl continents

---
+++ app/config/locale/nl.continents.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'AF' => 'Afrika',
+    'AN' => 'Antarctica',
+    'AS' => 'Azië',
+    'EU' => 'Europa',
+    'NA' => 'Noord Amerika',
+    'OC' => 'Oceanië',
+    'SA' => 'Zuid Amerika',
+];
