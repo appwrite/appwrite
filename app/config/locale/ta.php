409246442dc7b45dd9e21aa99b0299e77294a235
Sibi Bose <30630364+the-bose@users.noreply.github.com>
2019-10-01 22:42:49 +0530

    Create ta.php

---
+++ app/config/locale/ta.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'settings.inspire' => '"புத்திசாலித்தனம் என்னும் கலை என்பது எதனை புறக்கணிப்பது என்னும் கலையாகும்.."', // This is the line printed in the homepage and console 'view-source'
+    'settings.locale' => 'ta',
+    'settings.direction' => 'ltr',
+
+    // Service - Users
+    'auth.emails.team' => '%s குழு',
+    'auth.emails.confirm.title' => 'கணக்கினை  உறுதி செய்தல்',
+    'auth.emails.confirm.body' => 'ta.email.auth.confirm.tpl',
+    'auth.emails.recovery.title' => 'கடவுச்சொல் மீட்டமைப்பு',
+    'auth.emails.recovery.body' => 'ta.email.auth.recovery.tpl',
+    'auth.emails.invitation.title' => '%s குழுவின் அழைப்பு: %s',
+    'auth.emails.invitation.body' => 'ta.email.auth.invitation.tpl',
+
+    'locale.country.unknown' => 'அறியவில்லை',
+
+    'countries' => include 'ta.countries.php',
+    'continents' => include 'ta.continents.php',
+];
