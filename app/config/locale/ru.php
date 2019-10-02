333153a4cd38d712497306642d37392bac3fcfbe
Alexey Pyltsyn <lex61rus@gmail.com>
2019-10-02 20:43:45 +0300

    Improve Russian translation

--- app/config/locale/ru.php
+++ app/config/locale/ru.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-    'settings.inspire' => '"The art of being wise is the art of knowing what to overlook."', // This is the line printed in the homepage and console 'view-source'
+    'settings.inspire' => '"Искусство быть мудрым — это искусство знать, чем можно пренебречь."', // This is the line printed in the homepage and console 'view-source'
     'settings.locale' => 'ru',
-    'settings.direction' => 'rtl',
+    'settings.direction' => 'ltr',
 
-    'auth.emails.team' => '%s Команда',
-    'auth.emails.confirm.title' => 'Подтверждение Аккаунта',
+    'auth.emails.team' => 'Команда %s',
+    'auth.emails.confirm.title' => 'Подтверждение аккаунта',
     'auth.emails.confirm.body' => 'ru.email.auth.confirm.tpl',
-    'auth.emails.recovery.title' => 'Сброс Пароля',
+    'auth.emails.recovery.title' => 'Сброс пароля',
     'auth.emails.recovery.body' => 'ru.email.auth.recovery.tpl',
-    'auth.emails.invitation.title' => 'Приглашение в %s Команду в %s',
+    'auth.emails.invitation.title' => 'Приглашение в команду %s по проекту %s',
     'auth.emails.invitation.body' => 'ru.email.auth.invitation.tpl',
 
     'locale.country.unknown' => 'Неизвестно',
