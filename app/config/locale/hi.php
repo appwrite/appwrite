6b7bae1554f68c840732645c3e6f0fcf5f8f279d
Tanmay Mohan <tanmay@Tanmays-MacBook-Air.local>
2019-10-03 17:41:57 +0530

    ADD : Hindi Locale Files

---
+++ app/config/locale/hi.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'settings.inspire' => '"बुद्धिमान होने की कला यह जानने की कला है कि क्या अनदेखी करनी चाहिए।"', // This is the line printed in the homepage and console 'view-source'
+    'settings.locale' => 'hi',
+    'settings.direction' => 'ltr',
+
+    // Service - Users
+    'auth.emails.team' => '%s टीम',
+    'auth.emails.confirm.title' => 'खाता पुष्टि',
+    'auth.emails.confirm.body' => 'hi.email.auth.confirm.tpl',
+    'auth.emails.recovery.title' => 'पासवर्ड रीसेट',
+    'auth.emails.recovery.body' => 'hi.email.auth.recovery.tpl',
+    'auth.emails.invitation.title' => 'निमंत्रण %s टीम का  %s',
+    'auth.emails.invitation.body' => 'hi.email.auth.invitation.tpl',
+
+    'locale.country.unknown' => 'Unknown',
+
+    'countries' => include 'hi.countries.php',
+    'continents' => include 'hi.continents.php',
+];
---
+++ app/config/locale/templates/hi.email.auth.invitation.tpl
@@ -0,0 +1,27 @@
+<style>
+    * {
+        font-family: sans-serif,Arial;
+        -webkit-font-smoothing: antialiased;
+        font-weight: lighter;
+    }
+</style>
+
+<div style="direction: {{direction}}">
+    नमस्ते,
+    <br />
+    <br />
+    यह मेल आपको इसलिए भेजा गया था क्योंकि <b>{{owner}}</b> आपको टीम मेंबर बनने के लिए आमंत्रित करना चाहता था <b>{{team}}</b> इस {{project}} पर
+    <br />
+    <br />
+    इसमें शामिल होने के लिए इस लिंक को खोलें <b>{{team}}</b> टीम:
+    <br />
+    <a href="{{redirect}}">{{redirect}}</a>
+    <br />
+    <br />
+    यदि आप रुचि नहीं रखते हैं, तो आप इस संदेश को अनदेखा कर सकते हैं।
+    <br />
+    <br />
+    धन्यवाद,
+    <br />
+    {{project}} टीम
+</div>
