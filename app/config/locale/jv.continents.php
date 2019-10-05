f36d266a649b88c8aae4a40b8183bffdd24c8787
lucianosz7 <luciano_z7@hotmail.com>
2019-10-05 09:13:13 -0300

    Added javanese translation.

---
+++ app/config/locale/jv.continents.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'AF' => 'Afrika',
+    'AN' => 'Antartika',
+    'AS' => 'Asia',
+    'EU' => 'Eropa',
+    'NA' => 'Amérika Lor',
+    'OC' => 'Oseania',
+    'SA' => 'Amérika Kidul',
+];
