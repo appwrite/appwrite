2db6cc032eecc97ed9c67740519bf59473da3878
Murillo <mukarillo@gmail.com>
2019-10-12 13:13:26 +0200

    adding translation to pt

---
+++ app/config/locale/pt.continents.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'AF' => 'Africa',
+    'AN' => 'Antárctica',
+    'AS' => 'Ásia',
+    'EU' => 'Europa',
+    'NA' => 'América do norte',
+    'OC' => 'Oceânia',
+    'SA' => 'América do sul',
+];
