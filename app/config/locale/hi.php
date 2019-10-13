83e923782d01e54b4a55ac664350c0672266eb8a
Palash Dubey <palashdubey11@gmail.com>
2019-10-13 22:24:01 +0530

    fix: semantic errors in hindi locale

--- app/config/locale/hi.php
+++ app/config/locale/hi.php
@@ -1,17 +1,17 @@
 <?php
 
 return [
-    'settings.inspire' => '"बुद्धिमान होने की कला यह जानने की कला है कि क्या अनदेखा करना चाहिए |"', // This is the line printed in the homepage and console 'view-source'
+    'settings.inspire' => '"क्या अनदेखा करना चाहिए, यह जानने की कला ही बुद्धिमान होने की कला है | "', // This is the line printed in the homepage and console 'view-source'
     'settings.locale' => 'hi',
     'settings.direction' => 'ltr',
 
     // Service - Users
     'auth.emails.team' => '%s टीम',
-    'auth.emails.confirm.title' => 'अकाउंट  कन्फर्मेशन ',
+    'auth.emails.confirm.title' => 'अकाउंट प्रमाणीकरण',
     'auth.emails.confirm.body' => 'hi.email.auth.confirm.tpl',
-    'auth.emails.recovery.title' => 'पासवर्ड  रिसेट ',
+    'auth.emails.recovery.title' => 'पासवर्ड रिसेट ',
     'auth.emails.recovery.body' => 'hi.email.auth.recovery.tpl',
-    'auth.emails.invitation.title' => 'इनविटेशन  %s टीम  %s',
+    'auth.emails.invitation.title' => '%s टीम को %s पर आमंत्रण',
     'auth.emails.invitation.body' => 'hi.email.auth.invitation.tpl',
 
     'locale.country.unknown' => 'अज्ञात',
