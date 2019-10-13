b16ee3f9166c445df01024d18bc08fb6e54fdeaa
MIP2000 <49375670+MIP2000@users.noreply.github.com>
2019-10-13 23:41:52 +0300

    Create sq.continents.php

---
+++ app/config/locale/sq.continents.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'AF' => 'Afrika',
+    'AN' => 'Antarktik',
+    'AS' => 'Azi',
+    'EU' => 'Evropë',
+    'NA' => 'Amerika e Veriut',
+    'OC' => 'Oqeani',
+    'SA' => 'Amerika e Jugut',
+];
