1d34eec44bc642f3fd9cb89066646db4a6befa2a
Christy Jacob <christyjacob4@gmail.com>
2019-10-13 22:05:39 +0400

    feat: added stackoverflow class

--- src/Auth/OAuth/Google.php
+++ src/Auth/OAuth/Google.php
@@ -5,15 +5,13 @@ namespace Auth\OAuth;
 use Auth\OAuth;
 
 // Reference Material 
-// https://developers.google.com/oauthplayground/
-// https://developers.google.com/identity/protocols/OAuth2
-// https://developers.google.com/identity/protocols/OAuth2WebServer
-class Google extends OAuth
+// https://api.stackexchange.com/docs/authentication
+class Stackoverflow extends OAuth
 {
-    /**
-     * @var string
-     */
-    protected $version = 'v4';
+    // /**
+    //  * @var string
+    //  */
+    // protected $version = 'v4';
     /**
      * @var array
      */
@@ -24,7 +22,7 @@ class Google extends OAuth
      */
     public function getName(): string
     {
-        return 'google';
+        return 'stackoverflow';
     }
 
     /**
