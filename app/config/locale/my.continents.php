059b108644c9bd68406d844505a42c2cdcfdca2b
YMsyaraf <50256986+YMsyaraf@users.noreply.github.com>
2019-10-15 06:52:31 +0000

    Adding Malaysian local
    adding local language for malaysian

---
+++ app/config/locale/my.continents.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'AF' => 'Afrika',
+    'AN' => 'Antartika',
+    'AS' => 'Asia',
+    'EU' => 'Eropah',
+    'NA' => 'Amerika Utara',
+    'OC' => 'Oceania',
+    'SA' => 'Amerika Selatan',
+];
