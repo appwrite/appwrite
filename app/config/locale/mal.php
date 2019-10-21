a555aeb7a5ee79de43b96b5c8a29497740f43586
Eric Brian Anil <38664231+EricBrianAnil@users.noreply.github.com>
2019-10-21 10:25:12 +0530

    Malayalam translation
    Adding mal.php for local Malayalam translation as to enhance the repo as per mentioned in issue #25

---
+++ app/config/locale/mal.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'settings.inspire' => '"എന്താണ് അവഗണിക്കേണ്ടതെന്ന് അറിയാനുള്ള കലയാണ് ബുദ്ധിമാനായിരിക്കുക."', // This is the line printed in the homepage and console 'view-source'
+    'settings.locale' => 'mal',
+    'settings.direction' => 'ltr',
+    
+    // Service - Users
+    'auth.emails.team' => '%s ടീം',
+    'auth.emails.confirm.title' => 'അക്കൗണ്ട് സ്ഥിരീകരണം',
+    'auth.emails.confirm.body' => 'en.email.auth.confirm.tpl',
+    'auth.emails.recovery.title' => 'പാസ്‌വേഡ് പുന.സജ്ജമാക്കുക',
+    'auth.emails.recovery.body' => 'en.email.auth.recovery.tpl',
+    'auth.emails.invitation.title' => '%s ന് %s ടീമിലേക്കുള്ള ക്ഷണം',
+    'auth.emails.invitation.body' => 'en.email.auth.invitation.tpl',
+    
+    'locale.country.unknown' => 'അജ്ഞാതം',
+    
+    'countries' => include 'en.countries.php',
+    'continents' => include 'en.continents.php',
+];
