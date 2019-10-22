3ef44507b5bbb1191616d18b9519f6e98f694b35
Azmi Nawwar <azminawwar@gmail.com>
2019-10-22 10:38:06 +0700

    Add Sundanese Language

---
+++ app/config/locale/su.continents.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'AF' => 'Afrika',
+    'AN' => 'Antartika',
+    'AS' => 'Asia',
+    'EU' => 'Eropa',
+    'NA' => 'Amerika Utara',
+    'OC' => 'Oseania',
+    'SA' => 'Amerika Selatan',
+];
