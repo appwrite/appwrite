d03377c0b3e58fd1b433a5d0cae61c10397e2610
Azmi Nawwar <azminawwar@gmail.com>
2019-10-22 10:36:06 +0700

    Add Sundanese Language

---
+++ app/config/locale/su.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'settings.inspire' => '"Kasenian anu bijaksana nyaéta seni terang naon anu teu dipalire."', // This is the line printed in the homepage and console 'view-source'
+    'settings.locale' => 'su',
+    'settings.direction' => 'ltr',
+    // Service - Users
+    'auth.emails.team' => 'Tim %s',
+    'auth.emails.confirm.title' => 'Mastikeun Akun',
+    'auth.emails.confirm.body' => 'su.email.auth.confirm.tpl',
+    'auth.emails.recovery.title' => 'Reset Sandi',
+    'auth.emails.recovery.body' => 'su.email.auth.recovery.tpl',
+    'auth.emails.invitation.title' => 'Undangan ka Tim %s di %s',
+    'auth.emails.invitation.body' => 'su.email.auth.invitation.tpl',
+    'locale.country.unknown' => 'Teu Dikenal',
+    'countries' => include 'su.countries.php',
+    'continents' => include 'su.continents.php',
+];
