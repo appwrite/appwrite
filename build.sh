9e39750fd4dbea58c09e2352f01e1bc6e197f218
Eldad Fux <eldadfux@Eldads-MacBook-Pro-2.local>
2019-12-07 23:20:33 +0200

    Updated build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-echo "Updating git repository"
+echo "Updating git repository $1 / $2"
 
 git fetch origin
 git reset --hard origin/master
@@ -15,7 +15,7 @@ fi
 
 echo 'Starting build...'
 
-docker build --build-arg VERSION="$1" --tag appwrite/appwrite:"$1" .
+docker build --build-arg VERSION="$2" --tag appwrite/appwrite:"$1" .
 
 echo 'Pushing build to registry...'
 
