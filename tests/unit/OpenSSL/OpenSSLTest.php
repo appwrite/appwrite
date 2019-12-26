70ca5239f556d62c6a464b39eee11766cd7f41ad
Eldad Fux <eldad.fux@gmail.com>
2019-12-26 14:09:17 +0200

    Added unit tests for OpenSSL lib

---
+++ tests/unit/OpenSSL/OpenSSLTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Appwrite\Tests;
+
+use OpenSSL\OpenSSL;
+use PHPUnit\Framework\TestCase;
+
+class OpenSSLTest extends TestCase
+{
+    /**
+     * @var Cron
+     */
+    protected $object = null;
+
+    public function setUp()
+    {
+    }
+
+    public function tearDown()
+    {
+    }
+
+    public function testEncryptionAndDecryption()
+    {
+        $key = 'my-secret-key';
+        $iv = '';
+        $method = OpenSSL::CIPHER_AES_128_GCM;
+        $iv = OpenSSL::randomPseudoBytes(OpenSSL::cipherIVLength($method));
+        $tag = null;
+        $secret = 'my secret data';
+        $data = OpenSSL::encrypt($secret, OpenSSL::CIPHER_AES_128_GCM, $key, 0, $iv, $tag);
+
+        $this->assertEquals(OpenSSL::decrypt($data, $method, $key, 0, $iv, $tag), $secret);
+    }
+}
