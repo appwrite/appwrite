9901683c07d800fe3d93cc3667290a0cf7cb101a
Eldad Fux <eldad.fux@gmail.com>
2019-12-27 19:00:22 +0200

    Added more tests to device lib

--- tests/unit/Storage/Devices/LocalTest.php
+++ tests/unit/Storage/Devices/LocalTest.php
@@ -15,7 +15,7 @@ class LocalTest extends TestCase
 
     public function setUp()
     {
-        $this->object = new Local(__DIR__ . '/../../../resources/disk-a');
+        $this->object = new Local(realpath(__DIR__ . '/../../../resources/disk-a'));
     }
 
     public function tearDown()
@@ -34,13 +34,51 @@ class LocalTest extends TestCase
 
     public function testRoot()
     {
-        $this->assertEquals($this->object->getRoot(), '/storage/uploads//usr/share/nginx/html/tests/unit/Storage/Devices/../../../resources/disk-a');
+        $this->assertEquals($this->object->getRoot(), '/usr/share/nginx/html/tests/resources/disk-a');
     }
 
     public function testPath()
     {
-        $this->assertEquals($this->object->getPath('image.png'), '/storage/uploads//usr/share/nginx/html/tests/unit/Storage/Devices/../../../resources/disk-a/i/m/a/g/image.png');
-        $this->assertEquals($this->object->getPath('x.png'), '/storage/uploads//usr/share/nginx/html/tests/unit/Storage/Devices/../../../resources/disk-a/x/./p/n/x.png');
-        $this->assertEquals($this->object->getPath('y'), '/storage/uploads//usr/share/nginx/html/tests/unit/Storage/Devices/../../../resources/disk-a/y/x/x/x/y');
+        $this->assertEquals($this->object->getPath('image.png'), '/usr/share/nginx/html/tests/resources/disk-a/i/m/a/g/image.png');
+        $this->assertEquals($this->object->getPath('x.png'), '/usr/share/nginx/html/tests/resources/disk-a/x/./p/n/x.png');
+        $this->assertEquals($this->object->getPath('y'), '/usr/share/nginx/html/tests/resources/disk-a/y/x/x/x/y');
+    }
+    
+    public function testFileSize()
+    {
+        $this->assertEquals($this->object->getFileSize(__DIR__ . '/../../../resources/disk-a/kitten-1.jpg'), 599639);
+        $this->assertEquals($this->object->getFileSize(__DIR__ . '/../../../resources/disk-a/kitten-2.jpg'), 131958);
+    }
+    
+    public function testFileMimeType()
+    {
+        $this->assertEquals($this->object->getFileMimeType(__DIR__ . '/../../../resources/disk-a/kitten-1.jpg'), 'image/jpeg');
+        $this->assertEquals($this->object->getFileMimeType(__DIR__ . '/../../../resources/disk-a/kitten-2.jpg'), 'image/jpeg');
+        $this->assertEquals($this->object->getFileMimeType(__DIR__ . '/../../../resources/disk-b/kitten-1.png'), 'image/png');
+        $this->assertEquals($this->object->getFileMimeType(__DIR__ . '/../../../resources/disk-b/kitten-2.png'), 'image/png');
+    }
+    
+    public function testFileHash()
+    {
+        $this->assertEquals($this->object->getFileHash(__DIR__ . '/../../../resources/disk-a/kitten-1.jpg'), '7551f343143d2e24ab4aaf4624996b6a');
+        $this->assertEquals($this->object->getFileHash(__DIR__ . '/../../../resources/disk-a/kitten-2.jpg'), '81702fdeef2e55b1a22617bce4951cb5');
+        $this->assertEquals($this->object->getFileHash(__DIR__ . '/../../../resources/disk-b/kitten-1.png'), '03010f4f02980521a8fd6213b52ec313');
+        $this->assertEquals($this->object->getFileHash(__DIR__ . '/../../../resources/disk-b/kitten-2.png'), '8a9ed992b77e4b62b10e3a5c8ed72062');
+    }
+    
+    public function testDirectorySize()
+    {
+        $this->assertEquals($this->object->getDirectorySize(__DIR__ . '/../../../resources/disk-a/'), 731597);
+        $this->assertEquals($this->object->getDirectorySize(__DIR__ . '/../../../resources/disk-b/'), 3728550);
+    }
+    
+    public function testPartitionFreeSpace()
+    {
+        $this->assertGreaterThan(0, $this->object->getPartitionFreeSpace());
+    }
+    
+    public function testPartitionTotalSpace()
+    {
+        $this->assertGreaterThan(0, $this->object->getPartitionTotalSpace());
     }
 }
