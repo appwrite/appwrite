6210964de2e3745ceeb53a0b14e2fc43e9962faf
Armino Popp <devel@boioiong.com>
2020-01-23 17:11:18 +0200

    Trying ...

--- src/Auth/OAuth/Twitter.php
+++ src/Auth/OAuth/Twitter.php
@@ -4,6 +4,8 @@ namespace Auth\OAuth;
 
 use Auth\OAuth;
 
+// OAuth v1 implementation
+
 // Reference Material
 // https://developer.twitter.com/en/docs/basics/authentication/guides/log-in-with-twitter
 // Step 1: POST oauth/request_token
@@ -22,7 +24,135 @@ class Twitter extends OAuth
     protected $user = [];
 
 
+    private $requestTokenURL = 'https://api.twitter.com/oauth/request_token';
+    private $accessTokenURL = 'https://api.twitter.com/oauth/access_token';
+    private $authorizeURL = 'https://api.twitter.com/oauth/authorize';
+    private $showUserURL = 'https://api.twitter.com/1.1/account/verify_credentials.json';
 
+    private $oauthUnauthorizedToken = '';
+    private $oauthVerifier = '';
+    private $oauthToken = '';
+    private $oauthTokenSecret = '';
+
+    private $oauth = [];
+
+    private $method = '';
+    private $endpoint = '';
+
+
+    private function buildOAuthData(array $params = [], bool $isApiRequest = false)
+    {
+        $time = time();
+        $this->oauth = [
+            'oauth_nonce' => md5($time),
+            'oauth_callback' => $this->callback,
+            'oauth_consumer_key' => $this->appID,
+            'oauth_signature_method' => 'HMAC-SHA1',
+            'oauth_timestamp' => $time,
+            'oauth_version' => '1.0'
+        ];
+
+        if ($isApiRequest) {
+            $this->oauth['oauth_token'] = $this->oauthToken;
+        }
+
+        if (!empty($params)) {
+            foreach ($params as $k => $v) {
+                $this->oauth[$k] = $v;
+            }
+        }
+
+        ksort($this->oauth);
+
+        $this->oauth['oauth_signature'] = $this->createSignature();
+
+    }
+
+    private function buildOAuthHeader():string
+    {
+        $buffer = [];
+        ksort($this->oauth);
+        $oauthAllowedKeys = [
+            'oauth_consumer_key',
+            'oauth_nonce',
+            'oauth_callback',
+            'oauth_signature',
+            'oauth_signature_method',
+            'oauth_timestamp',
+            'oauth_token',
+            'oauth_version'
+        ];
+        foreach ($this->oauth as $k => $v) {
+            if (in_array($k, $oauthAllowedKeys)) {
+                $buffer[] = $k . '="' . rawurlencode($v) . '"';
+            }
+        }
+        return 'Authorization: OAuth ' . implode(', ', $buffer);
+    }
+
+    private function createSignatureBaseString():string
+    {
+        $buffer = [];
+        foreach ($this->oauth as $k => $v) {
+            $buffer[] = rawurlencode($k) . '=' . rawurlencode($v);
+        }
+
+        $parameterString = implode('&', $buffer);
+
+        return strtoupper($this->method) . '&' . rawurlencode($this->endpoint) . '&' . rawurlencode($parameterString);
+    }
+
+    private function createSigningKey():string
+    {
+        return rawurlencode($this->appSecret) . "&" . (empty($this->oauthTokenSecret) ? '' : rawurlencode($this->oauthTokenSecret));
+    }
+
+    private function createSignature():string
+    {
+        return base64_encode(hash_hmac('sha1', $this->createSignatureBaseString(), $this->createSigningKey(), true));
+    }
+
+
+    private function requestOAuthRequestToken()
+    {
+        $this->method = 'POST';
+        $this->endpoint = $this->requestTokenURL;
+
+        $this->buildOAuthData();
+        $header = $this->buildOAuthHeader();
+
+        $response = request($this->method, $this->endpoint, [$header]);
+
+        $buffer = [];
+        parse_str($response, $buffer);
+
+        if (is_array($buffer)) {
+            $this->oauthUnauthorizedToken = $buffer['oauth_token'];
+            $this->oauthVerifier = $buffer['oauth_verifier'];
+        }
+    }
+
+    private function requestOAuthAccessToken()
+    {
+        $this->method = 'POST';
+        $this->endpoint = $this->accessTokenURL;
+
+        $payload = http_build_query([
+            'oauth_consumer_key' => $this->appID,
+            'oauth_token' => $this->oauthUnauthorizedToken,
+            'oauth_verifier' => $this->oauthVerifier
+        ]);
+
+        $response = request($this->method, $this->endpoint, [], $payload);
+
+        if ($response) {
+            $buffer = [];
+            parse_str($response, $buffer);
+            $this->oauthToken = $buffer['oauth_token'];
+            $this->oauthTokenSecret = $buffer['oauth_token_secret'];
+        }
+
+    }
 
     /**
      * @return string
@@ -37,7 +167,11 @@ class Twitter extends OAuth
      */
     public function getLoginURL():string
     {
-
+        if ( empty($this->oauthToken) ) {
+            $this->requestOAuthRequestToken();
+            return $this->authorizeURL . "?oauth_token=" . $this->oauthUnauthorizedToken;
+        }
+        return '';
     }
 
     /**
@@ -47,8 +181,11 @@ class Twitter extends OAuth
      */
     public function getAccessToken(string $code):string
     {
-
-
+        $this->requestOAuthAccessToken();
+        $code = '';
+        if ( !empty($this->oauthToken) ) {
+            return $this->oauthToken;
+        }
         return '';
     }
 
@@ -93,8 +230,8 @@ class Twitter extends OAuth
     {
         $user = $this->getUser($accessToken);
 
-        if (isset($user['display_name'])) {
-            return $user['display_name'];
+        if (isset($user['name'])) {
+            return $user['name'];
         }
 
         return '';
@@ -107,8 +244,19 @@ class Twitter extends OAuth
      */
     protected function getUser(string $accessToken)
     {
-        if (empty($this->user)) {
+        if ( empty($this->user) ) {
+            $this->method = 'GET';
+            $this->endpoint = $this->showUserURL;
+            $params = [
+                'include_email' => 'true',
+                'include_entities' => 'true'
+            ];
+            $this->buildOAuthData($params, true);
+            $header = $this->buildOAuthHeader();
 
+            $response = request($this->method, $this->endpoint . '?' . http_build_query($params), [$header]);
+
+            $this->user = json_decode($response, true);
         }
 
         return $this->user;
