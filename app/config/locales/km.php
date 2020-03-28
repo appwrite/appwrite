cf2c9daafa9ec29f9bd17c01da9ec6f54374490d
SuuSoJeat <suusojeat@gmail.com>
2020-03-28 17:50:58 +0700

    patch: careless mistakes

--- app/config/locales/km.php
+++ app/config/locales/km.php
@@ -2,20 +2,20 @@
 
 return [
     'settings.inspire' => '"សិល្បៈនៃប្រាជ្ញាគឺជាសិល្បៈនៃការស្គាល់ពីអ្វីដែលត្រូវមើលរំលង។"', // This is the line printed in the homepage and console 'view-source'
-    'settings.locale' => 'en',
+    'settings.locale' => 'km',
     'settings.direction' => 'ltr',
 
     // Service - Users
     'account.emails.team' => 'ក្រុម %s',
     'account.emails.verification.title' => 'ការផ្ទៀងផ្ទាត់គណនី',
-    'account.emails.verification.body' => 'en.email.auth.confirm.tpl',
+    'account.emails.verification.body' => 'km.email.auth.confirm.tpl',
     'account.emails.recovery.title' => 'កំណត់ពាក្យសម្ងាត់ឡើងវិញ',
-    'account.emails.recovery.body' => 'en.email.auth.recovery.tpl',
+    'account.emails.recovery.body' => 'km.email.auth.recovery.tpl',
     'account.emails.invitation.title' => 'ការអញ្ជើញចូលក្នុងក្រុម %s នៅ %s',
-    'account.emails.invitation.body' => 'en.email.auth.invitation.tpl',
+    'account.emails.invitation.body' => 'km.email.auth.invitation.tpl',
 
     'locale.country.unknown' => 'មិនស្គាល់',
 
-    'countries' => include 'en.countries.php',
-    'continents' => include 'en.continents.php',
+    'countries' => include 'km.countries.php',
+    'continents' => include 'km.continents.php',
 ];
