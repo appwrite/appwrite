fa782ec8a5ee45baafa8a2f6d40d38375e9ada35
Eldad Fux <eldad.fux@gmail.com>
2020-04-12 12:35:15 +0300

    Enabled Fultter platform settings

--- app/config/platforms.php
+++ app/config/platforms.php
@@ -124,7 +124,7 @@ return [
         'key' => APP_PLATFORM_FLUTTER,
         'name' => 'Flutter',
         'description' => 'Client libraries for integrating with '.APP_NAME.' to build cross-platform Flutter applications. Read the [getting started for Flutter](/docs/getting-started-for-flutter) tutorial to start building your first Flutter application.',
-        'enabled' => false,
+        'enabled' => true,
         'beta' => true,
         'languages' => [
             [
