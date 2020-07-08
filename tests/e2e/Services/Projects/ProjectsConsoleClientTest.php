4cafa75a22409f6d13dc07eaca3abb6029ac1afe
Eldad Fux <eldad.fux@gmail.com>
2020-07-08 18:20:18 +0300

    Added more tests

--- tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
+++ tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
@@ -78,6 +78,33 @@ class ProjectsConsoleClientTest extends Scope
     /**
      * @depends testCreateProject
      */
+    public function testListProject($data):array
+    {
+        $id = (isset($data['projectId'])) ? $data['projectId'] : '';
+
+        /**
+         * Test for SUCCESS
+         */
+        $response = $this->client->call(Client::METHOD_GET, '/projects', array_merge([
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+        ], $this->getHeaders()));
+
+        $this->assertEquals(200, $response['headers']['status-code']);
+        $this->assertNotEmpty($response['body']);
+        $this->assertEquals($id, $response['body']['projects'][0]['$id']);
+        $this->assertEquals('Project Test', $response['body']['projects'][0]['name']);
+
+        /**
+         * Test for FAILURE
+         */
+
+        return [];
+    }
+
+    /**
+     * @depends testCreateProject
+     */
     public function testGetProject($data):array
     {
         $id = (isset($data['projectId'])) ? $data['projectId'] : '';
@@ -94,6 +121,65 @@ class ProjectsConsoleClientTest extends Scope
         $this->assertNotEmpty($response['body']);
         $this->assertEquals($id, $response['body']['$id']);
         $this->assertEquals('Project Test', $response['body']['name']);
+
+        /**
+         * Test for FAILURE
+         */
+
+        $response = $this->client->call(Client::METHOD_GET, '/projects/empty', array_merge([
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+        ], $this->getHeaders()));
+
+        $this->assertEquals(404, $response['headers']['status-code']);
+
+        $response = $this->client->call(Client::METHOD_GET, '/projects/id-is-really-long-id-is-really-long-id-is-really-long-id-is-really-long', array_merge([
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+        ], $this->getHeaders()));
+
+        $this->assertEquals(400, $response['headers']['status-code']);
+
+         return [];
+    }
+
+    /**
+     * @depends testCreateProject
+     */
+    public function testGetProjectUsage($data):array
+    {
+        $id = (isset($data['projectId'])) ? $data['projectId'] : '';
+
+        /**
+         * Test for SUCCESS
+         */
+        $response = $this->client->call(Client::METHOD_GET, '/projects/'.$id.'/usage', array_merge([
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+        ], $this->getHeaders()));
+
+        $this->assertEquals(200, $response['headers']['status-code']);
+        $this->assertNotEmpty($response['body']);
+        $this->assertArrayHasKey('collections', $response['body']);
+        $this->assertArrayHasKey('documents', $response['body']);
+        $this->assertArrayHasKey('network', $response['body']);
+        $this->assertArrayHasKey('requests', $response['body']);
+        $this->assertArrayHasKey('storage', $response['body']);
+        $this->assertArrayHasKey('tasks', $response['body']);
+        $this->assertArrayHasKey('users', $response['body']);
+        $this->assertIsArray($response['body']['collections']['data']);
+        $this->assertIsInt($response['body']['collections']['total']);
+        $this->assertIsArray($response['body']['documents']['data']);
+        $this->assertIsInt($response['body']['documents']['total']);
+        $this->assertIsArray($response['body']['network']['data']);
+        $this->assertIsInt($response['body']['network']['total']);
+        $this->assertIsArray($response['body']['requests']['data']);
+        $this->assertIsInt($response['body']['requests']['total']);
+        $this->assertIsInt($response['body']['storage']['total']);
+        $this->assertIsArray($response['body']['tasks']['data']);
+        $this->assertIsInt($response['body']['tasks']['total']);
+        $this->assertIsArray($response['body']['users']['data']);
+        $this->assertIsInt($response['body']['users']['total']);
 
         /**
          * Test for FAILURE
