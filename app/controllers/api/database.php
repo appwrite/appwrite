abdae9b0956930716d727e501e7a0ac5584816e7
Eldad Fux <eldad.fux@gmail.com>
2020-07-11 19:18:04 +0300

    Fixed permission updating bug

--- app/controllers/api/database.php
+++ app/controllers/api/database.php
@@ -613,7 +613,7 @@ $utopia->patch('/v1/database/collections/:collectionId/documents/:documentId')
             }
 
             if (!empty($write)) { // Overwrite permissions only when passed
-                $data['$permissions']['write'] = $read;
+                $data['$permissions']['write'] = $write;
             }
 
             $data = \array_merge($document->getArrayCopy(), $data);
