a27bc1a583ee0bfb9fa56be7ab333b6006d3e2f1
Eldad Fux <eldad.fux@gmail.com>
2020-08-21 17:58:17 +0300

    WIP: db worker

--- app/workers/database.php
+++ app/workers/database.php
@@ -1,6 +1,10 @@
 <?php
 
+use Appwrite\Database\Database;
+use Appwrite\Database\Adapter\MySQL as MySQLAdapter;
+use Appwrite\Database\Adapter\Redis as RedisAdapter;
 use Utopia\CLI\Console;
+use Utopia\Config\Config;
 
 require_once __DIR__.'/../init.php';
 
@@ -18,6 +22,43 @@ class DatabaseV1
 
     public function perform()
     {
+        global $register;
+
+        $projectId = $this->args['projectId'];
+        $operation = $this->args['operation'];
+        
+        $collection = $this->args['collection'];
+        $id = $this->args['id'];
+        $type = $this->args['type'];
+        $array = $this->args['array'];
+        $attributes = $this->args['attributes'];
+
+        $database = new Database();
+        $database->setAdapter(new RedisAdapter(new MySQLAdapter($register), $register));
+        $database->setNamespace('app_'.$projectId);
+        $database->setMocks(Config::getParam('collections', []));
+
+        switch ($operation) {
+            case 'createAttribute':
+                $database->createAttribute($collection, $id, $type, $array);
+                break;
+
+            case 'deleteAttribute':
+                $database->deleteAttribute($collection, $id, $array);
+                break;
+
+            case 'createIndex':
+                $database->createIndex($collection, $id, $attributes);
+                break;
+
+            case 'deleteIndex':
+                $database->deleteIndex($collection, $id);
+                break;
+            
+            default:
+                # code...
+                break;
+        }
     }
 
     public function tearDown()
