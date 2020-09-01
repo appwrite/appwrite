7b5f8c4d25f48ab75c99a3b053d2059ebfd34e47
Bradley Schofield <ionicisere@gmail.com>
2020-09-01 05:49:39 +0100

    First iteration of dropdown for tags
    + Dropdown in tags
    + Remove webkit transform in select css

--- public/scripts/views/forms/tags.js
+++ public/scripts/views/forms/tags.js
@@ -1,15 +1,26 @@
-(function(window) {
+(function (window) {
   "use strict";
 
   window.ls.container.get("view").add({
     selector: "data-forms-tags",
-    controller: function(element) {
+    controller: function (element, sdk) {
       let array = [];
       let tags = window.document.createElement("div");
       let preview = window.document.createElement("ul");
+      let addContainer = window.document.createElement("div");
       let add = window.document.createElement("input");
+      let recomendations = { users: [], teams: [] };
+      let dropdown = window.document.createElement("div");
+      let searchIcon = window.document.createElement("i");
 
-      let listen = function(event) {
+      dropdown.className = "dropdown";
+      searchIcon.className = "icon-search";
+
+      let focus = function (event) {
+        rerenderRecomendations();
+      }
+
+      let listen = function (event) {
         if (
           (event.key === "Enter" || event.key === " " || event.key === "Tab") &&
           add.value.length > 0
@@ -22,10 +33,10 @@
 
           check();
 
-          if(event.key !== "Tab") { // Don't lock accessibility
+          if (event.key !== "Tab") { // Don't lock accessibility
             event.preventDefault();
-          }
-        }
+          };
+        };
         if (
           (event.key === "Backspace" || event.key === "Delete") &&
           add.value === ""
@@ -37,19 +48,87 @@
           check();
         }
 
+        autoComplete();
+
         return false;
       };
-      
-      let check = function() {
-        try {
-          array = JSON.parse(element.value) || [];
-        } catch (error) {
-          array = [];
+
+      let rerenderRecomendations = function () {
+        let childNodesLength = dropdown.childNodes.length;
+
+        while (childNodesLength--) {
+          dropdown.removeChild(dropdown.lastChild);
         }
+
+        recomendations.teams.forEach(element => {
+          let child = window.document.createElement("div");
+          child.innerText = `t: ${element.name}`;
+          child.addEventListener('mousedown', function () {
+            add.value = element.$id;
+          });
+          dropdown.appendChild(child);
+        });
+
+        recomendations.users.forEach(element => {
+          let child = window.document.createElement("div");
+          child.innerText = `u: ${element.name}`;
+          child.addEventListener('mousedown', function () {
+            add.value = element.$id;
+          });
+          dropdown.appendChild(child);
+        });
+
+        if (recomendations.users.length <= 0 && recomendations.teams.length <= 0) {
+          let child = window.document.createElement("div");
+          child.innerText = 'No Results';
+          dropdown.appendChild(child);
+        }
+
+        addContainer.appendChild(dropdown);
+      }
+
+      let autoComplete = function () {
+        if (add.value.length <= 0) {
+          rerenderRecomendations();
+          return;
+        }
+
+        recomendations = { users: [], teams: [] };
+
+        // http://localhost/v1/teams?search=test&limit=15&orderType=DESC
+
+        sdk.users.list(add.value, 5, 0, 'DESC').then((response) => {
+          recomendations.users = [];
+          response.users.forEach(element => {
+            recomendations.users.push(element);
+          });
+          rerenderRecomendations();
+        }, function (error) {
+          console.log(error);
+        });
+
+        sdk.teams.list(add.value, 5, 0, 'DESC').then((response) => {
+          recomendations.teams = [];
+          response.teams.forEach(element => {
+            recomendations.teams.push(element);
+          });
+          rerenderRecomendations();
+        }, function (error) {
+          console.log(error);
+        });
+      };
+
+      let check = function () {
+        // Issue.
+        // try {
+        //   array = JSON.parse(element.value) || [];
+        // } catch (error) {
+        //   array = [];
+        // }
 
         if (!Array.isArray(array)) {
           array = [];
-        }
+        };
 
         preview.innerHTML = "";
 
@@ -57,14 +136,15 @@
           let value = array[index];
           let tag = window.document.createElement("li");
 
-          if(!value || value === ' ') {
+          if (!value || value === ' ') {
+            console.log('error');
             continue;
-          }
+          };
 
           tag.className = "tag";
           tag.textContent = value;
 
-          tag.addEventListener("click", function() {
+          tag.addEventListener("click", function () {
             array.splice(index, 1);
 
             element.value = JSON.stringify(array);
@@ -90,15 +170,21 @@
       add.className = "add";
       add.placeholder = element.placeholder;
 
-      //add.required = element.required;
+      addContainer.className = "addContainer";
 
-      tags.addEventListener("click", function() {
+      // add.required = element.required;
+
+      add.addEventListener('focus', function () {
+        focus();
+      })
+
+      tags.addEventListener("click", function () {
         add.focus();
       });
 
-      add.addEventListener("keydown", listen);
-      add.addEventListener("blur", function(event) {
-        if(add.value !== '') {
+      add.addEventListener("keyup", listen);
+      add.addEventListener("focusout", function (event) {
+        if (add.value !== '') {
           array.push(add.value);
 
           add.value = "";
@@ -107,10 +193,14 @@
 
           check();
         }
+
+        dropdown.remove();
       });
 
       tags.appendChild(preview);
-      tags.appendChild(add);
+      addContainer.appendChild(add);
+      addContainer.appendChild(searchIcon);
+      tags.appendChild(addContainer);
 
       element.parentNode.insertBefore(tags, element);
 
