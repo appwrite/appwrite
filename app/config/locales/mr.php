eaab36a907e0dfe149646bf0cf659f84a23ed708
siddhantvyas-tech <56452291+siddhantvyas-tech@users.noreply.github.com>
2020-10-01 18:20:04 +0530

    Marathi language support added

---
+++ app/config/locales/mr.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'settings.inspire' => '"शहाणे होण्याची कला म्हणजे कोणत्या गोष्टीकडे दुर्लक्ष करावे हे जाणून घेण्याची कला."', // This is the line printed in the homepage and console 'view-source'
+    'settings.locale' => 'mr',
+    'settings.direction' => 'ltr',
+
+    // Service - Users
+    'account.emails.team' => '%s कार्यसंघ',
+    'account.emails.verification.title' => 'खाते पुष्टीकरण',
+    'account.emails.verification.body' => 'mr.email.auth.confirm.tpl',
+    'account.emails.recovery.title' => 'संकेतशब्द रीसेट',
+    'account.emails.recovery.body' => 'mr.email.auth.recovery.tpl',
+    'account.emails.invitation.title' => 'आमंत्रण %s कार्यसंघ  %s',
+    'account.emails.invitation.body' => 'mr.email.auth.invitation.tpl',
+
+    'locale.country.unknown' => 'अज्ञात',
+
+    'countries' => include 'mr.countries.php',
+    'continents' => include 'mr.continents.php',
+];
