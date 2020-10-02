e30ec3b0fb7d822aa63c4a85ffafc439ab002010
Naman Shankhydhar <naman.geek@gmail.com>
2020-10-02 12:11:55 +0530

    translate local and templates to Punjabi(pu)

---
+++ app/config/locale/translations/pu.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'settings.inspire' => '"ਬੁੱਧੀਮਾਨ ਬਣਨ ਦੀ ਕਲਾ ਇਹ ਜਾਣਨ ਦੀ ਕਲਾ ਹੈ ਕਿ ਕਿਸ ਨੂੰ ਨਜ਼ਰਅੰਦਾਜ਼ ਕਰਨਾ ਹੈ."', // This is the line printed in the homepage and console 'view-source'
+    'settings.locale' => 'pu',
+    'settings.direction' => 'ltr',
+
+    // Service - Users
+    'account.emails.team' => '%s ਟੀਮ',
+    'account.emails.verification.title' => 'ਖਾਤਾ ਪੁਸ਼ਟੀਕਰਣ',
+    'account.emails.verification.body' => 'pu.email.auth.confirm.tpl',
+    'account.emails.recovery.title' => 'ਪਾਸਵਰਡ ਰੀਸੈੱਟ',
+    'account.emails.recovery.body' => 'pu.email.auth.recovery.tpl',
+    'account.emails.invitation.title' => 'ਸੱਦਾ %s ਟੀਮ %s',
+    'account.emails.invitation.body' => 'pu.email.auth.invitation.tpl',
+
+    'locale.country.unknown' => 'ਅਣਜਾਣ',
+
+    'countries' => include 'pu.countries.php',
+    'continents' => include 'pu.continents.php',
+];
