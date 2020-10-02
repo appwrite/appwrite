1232f403dbdd29b7831f3c5546d22ecb3511ccb7
specxybeardo <54959744+specxybeardo@users.noreply.github.com>
2020-10-02 17:04:52 +0530

    add locales and email templates in sindhi(sd)
    add locales and email templates in sindhi (sd) language

---
+++ app/config/locales/sd.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'settings.inspire' => '"عقلمند ٿيڻ جو فن ofاڻڻ جو فن آهي ڇا نظر انداز ٿيڻ."', // This is the line printed in the homepage and console 'view-source'
+    'settings.locale' => 'sd',
+    'settings.direction' => 'ltr',
+
+    // Service - Users
+    'account.emails.team' => '%s ٽيم',
+    'account.emails.verification.title' => 'کاتي جي تصديق',
+    'account.emails.verification.body' => 'sd.email.auth.confirm.tpl',
+    'account.emails.recovery.title' => 'پاسورڊ ري سيٽ ڪيو',
+    'account.emails.recovery.body' => 'sd.email.auth.recovery.tpl',
+    'account.emails.invitation.title' => 'جي دعوت %s تي ٽيم %s',
+    'account.emails.invitation.body' => 'sd.email.auth.invitation.tpl',
+
+    'locale.country.unknown' => 'اڻ سڃاتل',
+
+    'countries' => include 'sd.countries.php',
+    'continents' => include 'sd.continents.php',
+];
+
