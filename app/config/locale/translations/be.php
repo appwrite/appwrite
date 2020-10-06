1406e471eb893d02a199c40e12073fba605f68fc
eliely <irysia.mykoliuk@gmail.com>
2020-10-06 15:49:47 +0200

    requested changes

--- app/config/locale/translations/be.php
+++ app/config/locale/translations/be.php
@@ -1,21 +1,21 @@
 <?php
 
 return [
-    'settings.inspire' => '"The art of being wise is the art of knowing what to overlook."', // This is the line printed in the homepage and console 'view-source'
+    'settings.inspire' => '"Мастацтва быць мудрым - гэта мастацтва ведаць, на што нельга звярнуць увагу."', // This is the line printed in the homepage and console 'view-source'
     'settings.locale' => 'be',
     'settings.direction' => 'ltr',
 
     // Service - Users
     'account.emails.team' => 'Каманда %s',
     'account.emails.verification.title' => 'Праверка ўліковага запісу',
-    'account.emails.verification.body' => 'en.email.auth.confirm.tpl',
+    'account.emails.verification.body' => 'be.email.auth.confirm.tpl',
     'account.emails.recovery.title' => 'Скід пароля',
-    'account.emails.recovery.body' => 'en.email.auth.recovery.tpl',
+    'account.emails.recovery.body' => 'be.email.auth.recovery.tpl',
     'account.emails.invitation.title' => 'Запрашэнне да каманды %s y %s',
-    'account.emails.invitation.body' => 'en.email.auth.invitation.tpl',
+    'account.emails.invitation.body' => 'be.email.auth.invitation.tpl',
 
     'locale.country.unknown' => 'Невядомы',
 
-    'countries' => include 'en.countries.php',
-    'continents' => include 'en.continents.php',
+    'countries' => include 'be.countries.php',
+    'continents' => include 'be.continents.php',
 ];
