abf5ad3dc8b17fc3895b26daed7b4de82d1d90fe
spathak98 <54447950+spathak98@users.noreply.github.com>
2020-10-09 21:57:41 +0530

    Create fil.continents.php

---
+++ app/config/locale/translations/fil.continents.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'AF' => 'Africa',
+    'AN' => 'Antarctica',
+    'AS' => 'Asya',
+    'EU' => 'Europa',
+    'NA' => 'Hilagang Amarica',
+    'OC' => 'Oceania',
+    'SA' => 'Timog Amarica',
+];
