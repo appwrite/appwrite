9055993f59f0e467a8a9f519a110a5a6df3987d4
Prasannjit Mohapatra <prasanjit.mohapatra15@gmail.com>
2020-10-13 12:45:27 +0530

    Update od.php

--- app/config/locale/translations/od.php
+++ app/config/locale/translations/od.php
@@ -1,18 +1,18 @@
 <?php
 
 return [
-    'settings.inspire' => '"ବୁଦ୍ଧିମାନ ହବାର କଲା ଏମିତି ଏକ କଲା ଯାହା ଜଣାଇଥାଏ କୋଉ ଜିନ୍ସ କୁ ଅଣଦେଖା କରିବା ଉଚିତ |"', // This is the line printed in the homepage and console 'view-source'
+    'settings.inspire' => '"ଜ୍ଞାନୀ ହେବାର କଳା ହେଉଛି କ’ଣ ଅଣଦେଖା କରାଯିବ ଜାଣିବାର କଳା |"', // This is the line printed in the homepage and console 'view-source'
     'settings.locale' => 'od',
     'settings.direction' => 'ltr',
 
     // Service - Users
     'account.emails.team' => '%s ଟିମ',
     'account.emails.verification.title' => 'ଏକାଉଣ୍ଟ ଭେରିଫିକେସନ',
-    'account.emails.verification.body' => 'en.email.auth.confirm.tpl',
+    'account.emails.verification.body' => 'od.email.auth.confirm.tpl',
     'account.emails.recovery.title' => 'ପାସୱାର୍ଡ ରିସେଟ',
-    'account.emails.recovery.body' => 'en.email.auth.recovery.tpl',
+    'account.emails.recovery.body' => 'od.email.auth.recovery.tpl',
     'account.emails.invitation.title' => 'ଈଂଭିଟେସନ %s ଟିମ at %s',
-    'account.emails.invitation.body' => 'en.email.auth.invitation.tpl',
+    'account.emails.invitation.body' => 'od.email.auth.invitation.tpl',
 
     'locale.country.unknown' => 'ଅଜ୍ଞାତ',
 
