0e9ff0754765f8b4e107c0cd90a3654002c1c454
Prasannjit Mohapatra <prasanjit.mohapatra15@gmail.com>
2020-10-13 12:38:52 +0530

    Create od.php

---
+++ app/config/locale/translations/od.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'settings.inspire' => '"ବୁଦ୍ଧିମାନ ହବାର କଲା ଏମିତି ଏକ କଲା ଯାହା ଜଣାଇଥାଏ କୋଉ ଜିନ୍ସ କୁ ଅଣଦେଖା କରିବା ଉଚିତ |"', // This is the line printed in the homepage and console 'view-source'
+    'settings.locale' => 'od',
+    'settings.direction' => 'ltr',
+
+    // Service - Users
+    'account.emails.team' => '%s ଟିମ',
+    'account.emails.verification.title' => 'ଏକାଉଣ୍ଟ ଭେରିଫିକେସନ',
+    'account.emails.verification.body' => 'en.email.auth.confirm.tpl',
+    'account.emails.recovery.title' => 'ପାସୱାର୍ଡ ରିସେଟ',
+    'account.emails.recovery.body' => 'en.email.auth.recovery.tpl',
+    'account.emails.invitation.title' => 'ଈଂଭିଟେସନ %s ଟିମ at %s',
+    'account.emails.invitation.body' => 'en.email.auth.invitation.tpl',
+
+    'locale.country.unknown' => 'ଅଜ୍ଞାତ',
+
+    'countries' => include 'od.countries.php',
+    'continents' => include 'od.continents.php',
+];
