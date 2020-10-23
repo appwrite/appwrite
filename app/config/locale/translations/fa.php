3a296ccb2544fc6f40d06a57425b6ca876f38425
iraj <irajtaghlidi@gmail.com>
2020-10-23 18:56:06 +0300

    improve Farsi translation.

--- app/config/locale/translations/fa.php
+++ app/config/locale/translations/fa.php
@@ -7,14 +7,14 @@ return [
 
     // Service - Users
     'account.emails.team' => 'تیم %s',
-    'account.emails.verification.title' => 'تاییدیه حساب کاربری',
+    'account.emails.verification.title' => 'تایید حساب کاربری',
     'account.emails.verification.body' => 'fa.email.auth.confirm.tpl',
     'account.emails.recovery.title' => 'بازیابی رمز عبور',
     'account.emails.recovery.body' => 'fa.email.auth.recovery.tpl',
     'account.emails.invitation.title' => 'دعوتنامه به تیم %s در %s',
     'account.emails.invitation.body' => 'fa.email.auth.invitation.tpl',
 
-    'locale.country.unknown' => 'ناشناس',
+    'locale.country.unknown' => 'ناشناخته',
 
     'countries' => include 'fa.countries.php',
     'continents' => include 'fa.continents.php',
