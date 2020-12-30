bddb720f5905f93eeb01a3ea6e64a55bef5acc04
Eldad Fux <eldad.fux@gmail.com>
2020-12-30 09:26:52 +0200

    Removed log, force old function deletion

--- app/workers/functions.php
+++ app/workers/functions.php
@@ -109,8 +109,6 @@ $stdout = \explode("\n", $stdout);
     }
 }, $stdout);
 
-var_dump(json_encode($list));
-
 Console::info(count($list)." functions listed in " . ($executionEnd - $executionStart) . " seconds with exit code {$exitCode}");
 
 /*
@@ -507,7 +505,7 @@ class FunctionsV1
                 $stdout = '';
                 $stderr = '';
 
-                if(Console::execute("docker stop {$first['name']}", '', $stdout, $stderr, 30) !== 0) {
+                if(Console::execute("docker rm -f {$first['name']}", '', $stdout, $stderr, 30) !== 0) {
                     Console::error('Failed to remove container: '.$stderr);
                 }
 
