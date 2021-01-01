5d45eebf221047207fb66f6baa4ef921ab69bb4d
kodumbeats <brandon.leckemby@gmail.com>
2021-01-01 12:54:59 -0500

    allow toggle for secret placed above/below input

--- public/scripts/views/forms/show-secret.js
+++ public/scripts/views/forms/show-secret.js
@@ -1,32 +1,38 @@
-(function(window) {
-    "use strict";
+(function (window) {
+  "use strict";
 
-    window.ls.container.get("view").add({
-      selector: "data-forms-show-secret",
-      controller: function(element, document) {
-        let button = document.createElement("a");    
-        button.type = "button";
-        button.className = "icon-eye";
-        button.innerHTML = "show/hide";
-        button.style.cursor = "pointer";
-        button.style.fontSize = "10px";
-        
+  window.ls.container.get("view").add({
+    selector: "data-forms-show-secret",
+    controller: function (element, document) {
+      let button = document.createElement("a");
+      button.type = "button";
+      button.className = "icon-eye";
+      button.innerHTML = "show/hide";
+      button.style.cursor = "pointer";
+      button.style.fontSize = "10px";
+
+      if (element.attributes.getNamedItem("data-forms-show-secret-above")) {
         element.insertAdjacentElement("beforebegin", button);
-  
-        const toggle = function(event) {
-          switch (element.type) { //TODO@kodumbeats: add closed-eye icon to public/styles/icons.less and toggle onClick
-            case "password":
-              element.type = "text";
-              break;
-            case "text":
-              element.type = "password";
-              break;
-            default:
-              console.warn("data-forms-show-secret: element.type NOT text NOR password")
-          };
-        };
-  
-        button.addEventListener("click", toggle);
+      } else {
+        element.parentNode.insertBefore(button, element.nextSibling);
       }
-    });
-  })(window);
+
+      const toggle = function (event) {
+        switch (element.type) {
+          case "password":
+            element.type = "text";
+            break;
+          case "text":
+            element.type = "password";
+            break;
+          default:
+            console.warn(
+              "data-forms-show-secret: element.type NOT text NOR password"
+            );
+        }
+      };
+
+      button.addEventListener("click", toggle);
+    },
+  });
+})(window);
