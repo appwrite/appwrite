73e08974e48b5b8d276300cba2d0340f861240a0
Eldad Fux <eldad.fux@gmail.com>
2021-01-02 01:48:10 +0200

    Build function locally on dev mode

--- app/workers/functions.php
+++ app/workers/functions.php
@@ -39,7 +39,12 @@ Co\run(function() use ($environments) {  // Warmup: make sure images are ready t
         
             Console::info('Warming up '.$environment['name'].' environment...');
         
-            Console::execute('docker pull '.$environment['image'], '', $stdout, $stderr);
+            if(App::isDevelopment()) {
+                Console::execute('docker build '.$environment['build'].' -t '.$environment['image'], '', $stdout, $stderr);
+            }
+            else {
+                Console::execute('docker pull '.$environment['image'], '', $stdout, $stderr);
+            }
         
             if(!empty($stdout)) {
                 Console::log($stdout);
