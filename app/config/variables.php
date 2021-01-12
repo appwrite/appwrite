73f2bb025ad38a1ac338d39a56cb80876367ae78
Damodar Lohani <dlohani48@gmail.com>
2021-01-12 17:41:40 +0545

    introducing new temp environment variable

--- app/config/variables.php
+++ app/config/variables.php
@@ -93,6 +93,14 @@ return [
                 'required' => false,
                 'question' => '',
             ],
+            [
+                'name' => '_APP_USAGE_STATS',
+                'description' => 'This variable allows you to disable the collection and displaying of usage stats. This value is set to \'enabled\' by default, to disable the usage stats set the value to \'disabled\'. When disabled, it\'s recommended to turn off the Worker Usage, influxdb and telegraf containers for better resource usage.',
+                'introduction' => '0.7.0',
+                'default' => 'enabled',
+                'required' => false,
+                'question' => '',
+            ],
         ],
     ],
     [
