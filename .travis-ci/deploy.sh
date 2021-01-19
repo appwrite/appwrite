2f941fc08b844254515b27a1d9f1a78f9aa71e69
Eldad Fux <eldad.fux@gmail.com>
2021-01-19 09:46:37 +0200

    Deploy buildx

--- .travis-ci/deploy.sh
+++ .travis-ci/deploy.sh
@@ -1,1 +1,2 @@
-echo 'Nothing to deploy right now.'
+# echo 'Nothing to deploy right now.'
+docker buildx build --platform linux/amd64,linux/arm64,linux/386,linux/ppc64le -t appwrite/env-ruby-3.0:1.0.0 ./docker/environments/ruby-3.0/ --push
