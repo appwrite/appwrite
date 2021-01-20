6108b7bf9c35fcb7f33aced35c2ac5d507202900
Eldad Fux <eldad.fux@gmail.com>
2021-01-20 06:46:35 +0200

    Removed CI tests

--- .travis-ci/deploy.sh
+++ .travis-ci/deploy.sh
@@ -1,2 +1,1 @@
-# echo 'Nothing to deploy right now.'
-docker buildx build --platform linux/amd64,linux/arm64,linux/386,linux/ppc64le -t appwrite/env-ruby-3.0:1.0.0 ./docker/environments/ruby-3.0/ --push
+echo 'Nothing to deploy right now.'
