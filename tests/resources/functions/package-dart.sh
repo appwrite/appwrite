139b9efcff1fb944ab9dfc9fe4f902fd439b624b
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-01-25 22:12:56 +0100

    remove debug line from dart env

--- tests/resources/functions/package-dart.sh
+++ tests/resources/functions/package-dart.sh
@@ -5,7 +5,6 @@ cp -r $(pwd)/tests/resources/functions/dart $(pwd)/tests/resources/functions/pac
 
 docker run --rm -v $(pwd)/tests/resources/functions/packages/dart:/app -w /app appwrite/env-dart-2.10:1.0.0 ls
 docker run --rm -v $(pwd)/tests/resources/functions/packages/dart:/app -w /app --env PUB_CACHE=./.appwrite appwrite/env-dart-2.10:1.0.0 pub get
-docker run --rm -v $(pwd)/tests/resources/functions/packages/dart:/app -w /app --env PUB_CACHE=./.appwrite appwrite/env-dart-2.10:1.0.0 pub get --offline
 
 docker run --rm -v $(pwd)/tests/resources/functions/packages/dart:/app -w /app appwrite/env-dart-2.10:1.0.0 tar -zcvf code.tar.gz .
 
