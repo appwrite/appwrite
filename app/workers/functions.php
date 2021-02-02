8cdb184636683f40ea915e166f130735f1efb281
Eldad Fux <eldad.fux@gmail.com>
2021-02-02 09:26:11 +0200

    First Attempt

--- app/workers/functions.php
+++ app/workers/functions.php
@@ -27,6 +27,17 @@ $environments = Config::getParam('environments');
  */
 $warmupStart = \microtime(true);
 
+$dockerUser = App::getEnv('DOCKERHUB_PULL_USERNAME', null);
+$dockerPass = App::getEnv('DOCKERHUB_PULL_PASSWORD', null);
+
+if($dockerUser) {
+    $stdout = '';
+    $stderr = '';
+
+    Console::execute('echo '.$dockerPass.' | docker login --username '.$dockerUser.' --password-stdin', '', $stdout, $stderr);
+    Console::log('Docker Login'. $stdout.$stderr);
+}
+
 Co\run(function() use ($environments) {  // Warmup: make sure images are ready to run fast 🚀
     Runtime::enableCoroutine(SWOOLE_HOOK_ALL);
     
