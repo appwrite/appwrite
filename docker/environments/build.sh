277ee1b18bd40a292396a04b12a4ae80f17cc71a
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-02-05 18:32:44 +0100

    fix(env): remove arm support from dotnet 3.1

--- docker/environments/build.sh
+++ docker/environments/build.sh
@@ -34,7 +34,7 @@ echo 'Dart 2.10...'
 docker buildx build --platform linux/amd64 -t appwrite/env-dart-2.10:1.0.0 ./docker/environments/dart-2.10/ --push
 
 echo '.NET 3.1...'
-docker buildx build --platform linux/amd64,linux/arm64 -t appwrite/env-dotnet-3.1:1.0.0 ./docker/environments/dotnet-3.1/ --push
+docker buildx build --platform linux/amd64 -t appwrite/env-dotnet-3.1:1.0.0 ./docker/environments/dotnet-3.1/ --push
 
 echo '.NET 5.0...'
 docker buildx build --platform linux/amd64,linux/arm64 -t appwrite/env-dotnet-5.0:1.0.0 ./docker/environments/dotnet-5.0/ --push
