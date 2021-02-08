da9d7a8acaebaab8693daf2044e278b4f9214d56
Eldad Fux <eldad.fux@gmail.com>
2021-02-08 10:05:50 +0200

    Updated GEO DB

--- app/init.php
+++ app/init.php
@@ -208,7 +208,7 @@ $register->set('smtp', function () {
     return $mail;
 });
 $register->set('geodb', function () {
-    return new Reader(__DIR__.'/db/DBIP/dbip-country-lite-2020-01.mmdb');
+    return new Reader(__DIR__.'/db/DBIP/dbip-country-lite-2021-02.mmdb');
 });
 
 /*
