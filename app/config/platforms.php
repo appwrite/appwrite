3910952fa9addb4607b855f96346c33b77b3a4a9
Eldad Fux <eldad.fux@gmail.com>
2021-02-18 19:01:45 +0200

    Updated CLI version

--- app/config/platforms.php
+++ app/config/platforms.php
@@ -301,7 +301,7 @@ return [
             [
                 'key' => 'cli',
                 'name' => 'Command Line',
-                'version' => '0.4.0',
+                'version' => '0.5.0',
                 'url' => 'https://github.com/appwrite/sdk-for-cli',
                 'package' => 'https://github.com/appwrite/sdk-for-cli',
                 'enabled' => true,
