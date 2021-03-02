bbabeaf0260e44d53fc6dc60884f7dab29b5549a
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-03-02 10:56:18 +0100

    add ratchet websocket client to e2e test

---
+++ tests/e2e/Services/Realtime/RealtimeCustomClientTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\E2E\Services\Realtime;
+
+use Tests\E2E\Client;
+use Tests\E2E\Scopes\Scope;
+use Tests\E2E\Scopes\ProjectCustom;
+use Tests\E2E\Scopes\SideClient;
+
+class RealtimeCustomClientTest extends Scope
+{
+    use RealtimeBase;
+    use ProjectCustom;
+    use SideClient;
+}
