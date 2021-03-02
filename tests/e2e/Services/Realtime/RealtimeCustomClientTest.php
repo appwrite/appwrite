844d09d1faddaf06e2a2b26e870ddb5ee0269d41
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-03-02 19:06:15 +0100

    remove unnecessary import

--- tests/e2e/Services/Realtime/RealtimeCustomClientTest.php
+++ tests/e2e/Services/Realtime/RealtimeCustomClientTest.php
@@ -2,13 +2,10 @@
 
 namespace Tests\E2E\Services\Realtime;
 
-use Tests\E2E\Client;
 use Tests\E2E\Scopes\Scope;
 use Tests\E2E\Scopes\ProjectCustom;
 use Tests\E2E\Scopes\SideClient;
 
-use WebSocket\Client as WebSocketClient;
-use WebSocket\ConnectionException;
 
 class RealtimeCustomClientTest extends Scope
 {
