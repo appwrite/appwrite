66099e06bec06575a34259c98fc8744f979cb3b5
Eldad Fux <eldad.fux@gmail.com>
2021-03-10 08:52:10 +0200

    Updated benchmarks

--- tests/benchmarks/ws.js
+++ tests/benchmarks/ws.js
@@ -9,25 +9,30 @@ export let options = {
         { duration: '20s', target: 10 },
         { duration: '20s', target: 100 },
         { duration: '20s', target: 0 },
-      ],
+    ],
 }
 
 export default function () {
     const url = new URL('ws://localhost/v1/realtime');
-    url.searchParams.append('project', '60479fe35d95d');
+    url.searchParams.append('project', '60479391b1c3f');
     url.searchParams.append('channels[]', 'files');
+    
     const res = ws.connect(url.toString(), function (socket) {
         socket.on('open', () => {
             console.log('connected')
             });
+        
         socket.on('message', (data) => {
             console.log('Message received: ', data)
         });
+
         socket.on('close', () => console.log('disconnected'));
+        
         socket.setTimeout(function () {
             console.log('2 seconds passed, closing the socket');
             socket.close();
-          }, 2000);
+        }, 2000);
     });
+
     check(res, { 'status is 101': (r) => r && r.status === 101 });
 }
