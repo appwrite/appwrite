b33d54ef35e35b0b6c6c149e7300dbac529ba684
kodumbeats <brandon.leckemby@gmail.com>
2021-03-19 11:47:06 -0400

    Reducing sleep time

--- tests/e2e/Services/Functions/FunctionsCustomClientTest.php
+++ tests/e2e/Services/Functions/FunctionsCustomClientTest.php
@@ -176,7 +176,7 @@ class FunctionsCustomClientTest extends Scope
 
         $this->assertEquals(201, $execution['headers']['status-code']);
 
-        sleep(30);
+        sleep(10);
         $executions = $this->client->call(Client::METHOD_GET, '/functions/'.$functionId.'/executions', [
             'content-type' => 'application/json',
             'x-appwrite-project' => $projectId,
