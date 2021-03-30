c49166248795cf49860f6934f06fe6c65a134750
kodumbeats <brandon.leckemby@gmail.com>
2021-03-30 07:25:21 -0400

    Remove utopia license comments

--- src/Appwrite/Network/Validator/URL.php
+++ src/Appwrite/Network/Validator/URL.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * Utopia PHP Framework
- *
- * @package Framework
- * @subpackage Validator
- *
- * @link https://github.com/utopia-php/framework
- * @author Appwrite Team <team@appwrite.io>
- * @license The MIT License (MIT) <http://www.opensource.org/licenses/mit-license.php>
- */
 
 namespace Appwrite\Network\Validator;
 
