07198068856d2d4a341ea5f1c5b987927702c492
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-04-21 13:27:02 +0200

    fix: runtime whitelist

--- app/config/runtimes.php
+++ app/config/runtimes.php
@@ -8,12 +8,12 @@ use Appwrite\Runtimes\Runtimes;
  */
 $runtimes = Runtimes::get();
 
-$allowList = empty(App::getEnv('_APP_FUNCTIONS_ENVS', null)) ? false : \explode(',', App::getEnv('_APP_FUNCTIONS_ENVS', null));
+$allowList = empty(App::getEnv('_APP_FUNCTIONS_ENVS')) ? false : \explode(',', App::getEnv('_APP_FUNCTIONS_ENVS'));
 
 $runtimes = array_filter($runtimes, function ($key) use ($allowList) {
     $isAllowed = $allowList && in_array($key, $allowList);
 
-    return $isAllowed;
+    return $allowList ? $isAllowed : true;
 }, ARRAY_FILTER_USE_BOTH);
 
 return $runtimes;
