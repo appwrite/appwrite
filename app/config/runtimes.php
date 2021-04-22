26a747d29e271eda079ca75adc967af53c37aa38
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-04-22 11:26:10 +0200

    feat: update new php-runtimes version

--- app/config/runtimes.php
+++ app/config/runtimes.php
@@ -6,11 +6,11 @@ use Appwrite\Runtimes\Runtimes;
 /**
  * List of Appwrite Cloud Functions supported runtimes
  */
-$runtimes = Runtimes::get();
+$runtimes = new Runtimes();
 
 $allowList = empty(App::getEnv('_APP_FUNCTIONS_ENVS')) ? false : \explode(',', App::getEnv('_APP_FUNCTIONS_ENVS'));
 
-$runtimes = array_filter($runtimes, function ($key) use ($allowList) {
+$runtimes = array_filter($runtimes->getAll(), function ($key) use ($allowList) {
     $isAllowed = $allowList && in_array($key, $allowList);
 
     return $allowList ? $isAllowed : true;
