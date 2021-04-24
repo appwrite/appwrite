0c75125fb13f02ef8d2e224e7b37c47df8179baf
Caden Kriese <caden2k3@gmail.com>
2021-04-23 23:55:16 -0600

    Change theme based on system preference

--- public/scripts/views/general/theme.js
+++ public/scripts/views/general/theme.js
@@ -1,4 +1,17 @@
 (function(window) {
+  darkThemeMq = window.matchMedia("(prefers-color-scheme: light)");
+  darkThemeMq.addEventListener("change", event => {
+    if (event.matches) {
+      document.body.classList.remove('theme-dark');
+      document.body.classList.add('theme-light');
+      window.localStorage.setItem('user-theme', 'theme-light');
+    } else {
+      document.body.classList.remove('theme-light');
+      document.body.classList.add('theme-dark');
+      window.localStorage.setItem('user-theme', 'theme-dark')
+    }
+  });
+
   window.ls.container.get("view").add({
     selector: "data-general-theme",
     controller: function(element, router, document) {
