347ae809d8f73e4a879673472c1663aea1280029
Eldad Fux <eldad.fux@gmail.com>
2021-05-21 17:36:48 +0300

    Updated web sdk version

--- app/config/platforms.php
+++ app/config/platforms.php
@@ -15,7 +15,7 @@ return [
             [
                 'key' => 'web',
                 'name' => 'Web',
-                'version' => '3.0.0',
+                'version' => '3.0.1',
                 'url' => 'https://github.com/appwrite/sdk-for-web',
                 'package' => 'https://www.npmjs.com/package/appwrite',
                 'enabled' => true,
