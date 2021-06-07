ffd53053a7ec0bdd5926c7255a64d0290366addc
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-06-07 12:09:31 +0200

    feat(dbip): update to 2021-06

--- .travis-ci/build.sh
+++ .travis-ci/build.sh
@@ -15,7 +15,7 @@ then
       exit 1
 fi
 
-if test $(find "./app/db/DBIP/dbip-country-lite-2021-02.mmdb" -mmin +259200)
+if test $(find "./app/db/DBIP/dbip-country-lite-2021-06.mmdb" -mmin +259200)
 then
     printf "${RED}GEO country DB has not been updated for more than 6 months. Go to https://db-ip.com/db/download/ip-to-country-lite to download a newer version${NC}\n"
 fi
@@ -27,3 +27,6 @@ docker build --build-arg VERSION="$2" --tag appwrite/appwrite:"$1" .
 echo 'Pushing build to registry...'
 
 docker push appwrite/appwrite:"$1"
+
+
+docker run -it --rm --volume /var/run/docker.sock:/var/run/docker.sock --volume "$(pwd)"/appwrite:/usr/src/code/appwrite:rw --entrypoint="install" torstendittmann/appwrite:realtime
