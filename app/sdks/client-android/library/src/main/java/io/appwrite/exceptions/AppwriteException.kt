b9d8f65637a652379c9d19c9d1cb86a93f51bb52
Christy Jacob <christyjacob4@gmail.com>
2021-06-09 17:50:13 +0530

    feat: remove sdk files

--- app/sdks/client-android/library/src/main/java/io/appwrite/exceptions/AppwriteException.kt
+++
@@ -1,9 +0,0 @@
-package io.appwrite.exceptions
-
-import java.lang.Exception
-
-class AppwriteException(
-    message: String? = null,
-    val code: Int? = null,
-    val response: String? = null
-) : Exception(message)
--- app/sdks/client-android/library/src/main/java/io/appwrite/models/Error.kt
+++
@@ -1,6 +0,0 @@
-package io.appwrite.models
-
-data class Error(
-    val message: String,
-    val code: Int
-)
