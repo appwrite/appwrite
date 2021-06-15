b1ebb150cb6e2a5e830a6421363c6b9948d68e42
Damodar Lohani <dlohani48@gmail.com>
2021-06-15 14:29:36 +0545

    fix response model

--- src/Appwrite/Utopia/Response/Model/Bucket.php
+++ src/Appwrite/Utopia/Response/Model/Bucket.php
@@ -17,12 +17,19 @@ class Bucket extends Model
             'default' => '',
             'example' => '5e5ea5c16897e',
         ])
-        ->addRule('$permissions', [
-            'type' => Response::MODEL_PERMISSIONS,
-            'description' => 'Bucket permissions.',
-            'default' => new \stdClass,
-            'example' => new \stdClass,
-            'array' => false,
+        ->addRule('$read', [
+            'type' => self::TYPE_STRING,
+            'description' => 'File read permissions.',
+            'default' => [],
+            'example' => ['role:all'],
+            'array' => true,
+        ])
+        ->addRule('$write', [
+            'type' => self::TYPE_STRING,
+            'description' => 'File write permissions.',
+            'default' => [],
+            'example' => ['user:608f9da25e7e1'],
+            'array' => true,
         ])
         ->addRule('dateCreated', [
             'type' => self::TYPE_INTEGER,
