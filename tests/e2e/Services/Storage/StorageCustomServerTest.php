1bf770ed3482156105ae1aa9d9eb84b4d8482fa0
Damodar Lohani <dlohani48@gmail.com>
2021-06-16 11:35:40 +0545

    moving storage buckets test to server scope

--- tests/e2e/Services/Storage/StorageCustomServerTest.php
+++ tests/e2e/Services/Storage/StorageCustomServerTest.php
@@ -5,10 +5,104 @@ namespace Tests\E2E\Services\Storage;
 use Tests\E2E\Scopes\ProjectCustom;
 use Tests\E2E\Scopes\Scope;
 use Tests\E2E\Scopes\SideServer;
+use Tests\E2E\Client;
 
 class StorageCustomServerTest extends Scope
 {
     use StorageBase;
     use ProjectCustom;
     use SideServer;
+
+    public function testCreateBucket():array
+    {
+        /**
+         * Test for SUCCESS
+         */
+        $bucket = $this->client->call(Client::METHOD_POST, '/storage/buckets', array_merge([
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+        ], $this->getHeaders()), [
+            'name' => 'Test Bucket',
+        ]);
+        $this->assertEquals(201, $bucket['headers']['status-code']);
+        $this->assertNotEmpty($bucket['body']['$id']);
+        $this->assertIsInt($bucket['body']['dateCreated']);
+        $this->assertEquals('Test Bucket', $bucket['body']['name']);
+        $this->assertEquals(true, $bucket['body']['enabled']);
+        $bucketId = $bucket['body']['$id'];
+        /**
+         * Test for FAILURE
+         */
+        $bucket = $this->client->call(Client::METHOD_POST, '/storage/buckets', array_merge([
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+        ], $this->getHeaders()), [
+            'name' => '',
+        ]);
+        $this->assertEquals(400, $bucket['headers']['status-code']);
+
+        return ['bucketId' => $bucketId];
+    }
+
+    /**
+     * @depends testCreateBucket
+     */
+    public function testListBucket($data): array
+    {
+        $id = $data['bucketId'] ?? '';
+        /**
+         * Test for SUCCESS
+         */
+        $response = $this->client->call(Client::METHOD_GET, '/storage/buckets',
+            array_merge([
+                'content-type' => 'application/json',
+                'x-appwrite-project' => $this->getProject()['$id'],
+            ], $this->getHeaders()));
+        $this->assertEquals(200, $response['headers']['status-code']);
+        $this->assertNotEmpty($response['body']);
+        $this->assertEquals($id, $response['body']['buckets'][0]['$id']);
+        $this->assertEquals('Test Bucket', $response['body']['buckets'][0]['name']);
+
+        return $data;
+    }
+
+    /**
+     * @depends testCreateBucket
+     */
+    public function testGetBucket($data): array
+    {
+        $id = $data['bucketId'] ?? '';
+        /**
+         * Test for SUCCESS
+         */
+        $response = $this->client->call(Client::METHOD_GET, '/storage/buckets/' . $id,
+            array_merge([
+                'content-type' => 'application/json',
+                'x-appwrite-project' => $this->getProject()['$id'],
+            ], $this->getHeaders()));
+        $this->assertEquals(200, $response['headers']['status-code']);
+        $this->assertNotEmpty($response['body']);
+        $this->assertEquals($id, $response['body']['$id']);
+        $this->assertEquals('Test Bucket', $response['body']['name']);
+        
+        /**
+         * Test for FAILURE
+         */
+        
+        $response = $this->client->call(Client::METHOD_GET, '/storage/buckets/empty',
+            array_merge([
+                'content-type' => 'application/json',
+                'x-appwrite-project' => $this->getProject()['$id'],
+            ], $this->getHeaders()));
+        $this->assertEquals(404, $response['headers']['status-code']);
+        
+        $response = $this->client->call(Client::METHOD_GET, '/storage/buckets/id-is-really-long-id-is-really-long-id-is-really-long-id-is-really-long',
+            array_merge([
+                'content-type' => 'application/json',
+                'x-appwrite-project' => $this->getProject()['$id'],
+            ], $this->getHeaders()));
+        $this->assertEquals(400, $response['headers']['status-code']);
+
+        return $data;
+    }
 }
