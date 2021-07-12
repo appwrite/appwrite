76af58c646ce8e0564afff66f1a78f9f24726ee1
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-07-12 17:27:26 +0200

    fix(pdo): throw on general to parent

--- app/controllers/general.php
+++ app/controllers/general.php
@@ -304,6 +304,10 @@ App::error(function ($error, $utopia, $request, $response, $layout, $project) {
     /** @var Utopia\View $layout */
     /** @var Appwrite\Database\Document $project */
 
+    if (get_class($error) == 'PDOException') {
+        throw $error;
+    }
+
     $route = $utopia->match($request);
     $template = ($route) ? $route->getLabel('error', null) : null;
 
