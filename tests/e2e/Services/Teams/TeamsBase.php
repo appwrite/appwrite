825788c05b0dfed4db8fae1f6622972c35707cbc
Damodar Lohani <dlohani48@gmail.com>
2021-07-19 13:38:57 +0545

    fix naming

--- tests/e2e/Services/Teams/TeamsBase.php
+++ tests/e2e/Services/Teams/TeamsBase.php
@@ -29,18 +29,18 @@ trait TeamsBase
         $teamUid = $response1['body']['$id'];
         $teamName = $response1['body']['name'];
 
-        $membershipId = \uniqid();
+        $teamId = \uniqid();
         $response2 = $this->client->call(Client::METHOD_POST, '/teams', array_merge([
             'content-type' => 'application/json',
             'x-appwrite-project' => $this->getProject()['$id'],
         ], $this->getHeaders()), [
-            'teamId' => $membershipId,
+            'teamId' => $teamId,
             'name' => 'Manchester United'
         ]);
 
         $this->assertEquals(201, $response2['headers']['status-code']);
         $this->assertNotEmpty($response2['body']['$id']);
-        $this->assertEquals($membershipId, $response2['body']['$id']);
+        $this->assertEquals($teamId, $response2['body']['$id']);
         $this->assertEquals('Manchester United', $response2['body']['name']);
         $this->assertGreaterThan(-1, $response2['body']['sum']);
         $this->assertIsInt($response2['body']['sum']);
