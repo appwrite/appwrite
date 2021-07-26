823a76d73da942b84074c213128a89df4f0c1e4a
Christy Jacob <christyjacob4@gmail.com>
2021-07-26 23:25:14 +0530

    feat(email-tempaltes): apply nl2p filters

--- src/Appwrite/Template/Template.php
+++ src/Appwrite/Template/Template.php
@@ -74,7 +74,7 @@ class Template extends View
         if (\is_readable($this->path)) {
             $template = \file_get_contents($this->path); // Include template file
         } else if (!empty($this->content)) {
-            $template = $this->print($this->content);
+            $template = $this->print($this->content, self::FILTER_NL2P);
         } else {
             throw new Exception('"'.$this->path.'" template is not readable or not found');
         }
