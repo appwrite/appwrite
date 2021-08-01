5af858429dfe960128c5c6864781417912ad4089
Eldad Fux <eldad.fux@gmail.com>
2021-08-01 11:00:13 +0300

    Updated tests

--- tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
+++ tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
@@ -536,13 +536,16 @@ class ProjectsConsoleClientTest extends Scope
         $this->assertNotEmpty($project['body']['$id']);
 
         $id = $project['body']['$id'];
-
-        $services = ['functions', 'webhooks', 'avatars', 'health', 'locale', 'storage', 'teams'];
+        $services = require('app/config/services.php');
 
         /**
          * Test for Disabled
          */
         foreach ($services as $service) {
+            if(!$service['optional']) {
+                continue;
+            }
+            
             $response = $this->client->call(Client::METHOD_PATCH, '/projects/'.$id.'/service', array_merge([
                 'content-type' => 'application/json',
                 'x-appwrite-project' => $this->getProject()['$id'],
