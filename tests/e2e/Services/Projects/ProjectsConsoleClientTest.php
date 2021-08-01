aff747a9b84fc47c0f5f24870fe9a919d0ee0bee
Eldad A. Fux <eldad.fux@gmail.com>
2021-08-01 12:09:14 +0300

    Update tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
    Co-authored-by: Damodar Lohani <lohanidamodar@users.noreply.github.com>

--- tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
+++ tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
@@ -566,7 +566,7 @@ class ProjectsConsoleClientTest extends Scope
 
             $this->assertEquals(200, $response['headers']['status-code']);
             $this->assertNotEmpty($response['body']['$id']);
-            $this->assertEquals(false, $response['body']['serviceStatusFor'.ucfirst($service)][$service]);
+            $this->assertEquals(false, $response['body']['serviceStatusFor'.ucfirst($service)]);
         }
         
         /**
