d009b8ab89274e368fe9c87cc4284342921447ee
Damodar Lohani <dlohani48@gmail.com>
2021-08-27 17:22:52 +0545

    delete older usage stats

--- app/tasks/maintenance.php
+++ app/tasks/maintenance.php
@@ -39,11 +39,22 @@ $cli
             ]);
         }
 
+        function notifyDeleteUsageStats(int $interval30m, int $interval1d) 
+        {
+            Resque::enqueue(Event::DELETE_QUEUE_NAME, Event::DELETE_CLASS_NAME, [
+                'type' => DELETE_TYPE_USAGE_STATS,
+                'timestamp1d' => $interval1d,
+                'timestamp30m' => $interval30m,
+            ]);
+        }
+
         // # of days in seconds (1 day = 86400s)
         $interval = (int) App::getEnv('_APP_MAINTENANCE_INTERVAL', '86400');
         $executionLogsRetention = (int) App::getEnv('_APP_MAINTENANCE_RETENTION_EXECUTION', '1209600');
         $auditLogRetention = (int) App::getEnv('_APP_MAINTENANCE_RETENTION_AUDIT', '1209600');
         $abuseLogsRetention = (int) App::getEnv('_APP_MAINTENANCE_RETENTION_ABUSE', '86400');
+        $usageStatsRetention30m = (int) App::getEnv('_APP_MAINTENANCE_RETENTION_USAGE_30M', '129600');//36 hours
+        $usageStatsRetention1d = (int) App::getEnv('_APP_MAINTENANCE_RETENTION_USAGE_1D', '8640000'); // 100 days
 
         Console::loop(function() use ($interval, $executionLogsRetention, $abuseLogsRetention, $auditLogRetention){
             $time = date('d-m-Y H:i:s', time());
