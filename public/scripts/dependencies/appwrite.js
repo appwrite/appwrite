a944975dcbd75f188b99f81a8a5cbabf6872123d
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-08-27 17:26:26 +0200

    fix(realtime): update console sdk

--- public/scripts/dependencies/appwrite.js
+++ public/scripts/dependencies/appwrite.js
@@ -1,6 +1,21 @@
 (function (exports, isomorphicFormData, crossFetch) {
     'use strict';
 
+    /*! *****************************************************************************
+    Copyright (c) Microsoft Corporation.
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose with or without fee is hereby granted.
+
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+    REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+    INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+    LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+    OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+    PERFORMANCE OF THIS SOFTWARE.
+    ***************************************************************************** */
+
     function __awaiter(thisArg, _arguments, P, generator) {
         function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
         return new (P || (P = Promise))(function (resolve, reject) {
@@ -32,7 +47,7 @@
                 mode: '',
             };
             this.headers = {
-                'x-sdk-version': 'appwrite:web:2.0.0',
+                'x-sdk-version': 'appwrite:web:2.1.0',
                 'X-Appwrite-Response-Format': '0.9.0',
             };
             this.realtime = {
@@ -41,7 +56,7 @@
                 channels: {},
                 lastMessage: undefined,
                 createSocket: () => {
-                    var _a;
+                    var _a, _b;
                     const channels = new URLSearchParams();
                     channels.set('project', this.config.project);
                     for (const property in this.realtime.channels) {
@@ -51,6 +66,7 @@
                         this.realtime.socket.close();
                     }
                     this.realtime.socket = new WebSocket(this.config.endpointRealtime + '/realtime?' + channels.toString());
+                    (_b = this.realtime.socket) === null || _b === void 0 ? void 0 : _b.addEventListener('message', this.realtime.authenticate);
                     for (const channel in this.realtime.channels) {
                         this.realtime.channels[channel].forEach(callback => {
                             var _a;
@@ -58,8 +74,8 @@
                         });
                     }
                     this.realtime.socket.addEventListener('close', event => {
-                        var _a, _b;
-                        if (((_b = (_a = this.realtime) === null || _a === void 0 ? void 0 : _a.lastMessage) === null || _b === void 0 ? void 0 : _b.code) === 1008) {
+                        var _a, _b, _c;
+                        if (((_b = (_a = this.realtime) === null || _a === void 0 ? void 0 : _a.lastMessage) === null || _b === void 0 ? void 0 : _b.type) === 'error' && ((_c = this.realtime) === null || _c === void 0 ? void 0 : _c.lastMessage.data).code === 1008) {
                             return;
                         }
                         console.error('Realtime got disconnected. Reconnect will be attempted in 1 second.', event.reason);
@@ -68,15 +84,35 @@
                         }, 1000);
                     });
                 },
+                authenticate: (event) => {
+                    var _a, _b;
+                    const message = JSON.parse(event.data);
+                    if (message.type === 'connected') {
+                        const cookie = JSON.parse((_a = window.localStorage.getItem('cookieFallback')) !== null && _a !== void 0 ? _a : "{}");
+                        const session = cookie === null || cookie === void 0 ? void 0 : cookie[`a_session_${this.config.project}`];
+                        const data = message.data;
+                        if (session && !data.user) {
+                            (_b = this.realtime.socket) === null || _b === void 0 ? void 0 : _b.send(JSON.stringify({
+                                type: "authentication",
+                                data: {
+                                    session
+                                }
+                            }));
+                        }
+                    }
+                },
                 onMessage: (channel, callback) => (event) => {
                     try {
-                        const data = JSON.parse(event.data);
-                        this.realtime.lastMessage = data;
-                        if (data.channels && data.channels.includes(channel)) {
-                            callback(data);
+                        const message = JSON.parse(event.data);
+                        this.realtime.lastMessage = message;
+                        if (message.type === 'event') {
+                            let data = message.data;
+                            if (data.channels && data.channels.includes(channel)) {
+                                callback(data);
+                            }
                         }
-                        else if (data.code) {
-                            throw data;
+                        else if (message.type === 'error') {
+                            throw message.data;
                         }
                     }
                     catch (e) {
@@ -507,12 +543,20 @@
                  * first. Use the success and failure arguments to provide a redirect URL's
                  * back to your app when login is completed.
                  *
+                 * If there is already an active session, the new session will be attached to
+                 * the logged-in account. If there are no active sessions, the server will
+                 * attempt to look for a user with the same email address as the email
+                 * received from the OAuth2 provider and attach the new session to the
+                 * existing user. If no matching user is found - the server will create a new
+                 * user..
+                 *
+                 *
                  * @param {string} provider
                  * @param {string} success
                  * @param {string} failure
                  * @param {string[]} scopes
                  * @throws {AppwriteException}
-                 * @returns {void|URL}
+                 * @returns {void|string}
                  */
                 createOAuth2Session: (provider, success, failure, scopes) => {
                     if (typeof provider === 'undefined') {
@@ -3602,14 +3646,17 @@
                 /**
                  * Create Team Membership
                  *
-                 * Use this endpoint to invite a new member to join your team. An email with a
-                 * link to join the team will be sent to the new member email address if the
-                 * member doesn't exist in the project it will be created automatically.
+                 * Use this endpoint to invite a new member to join your team. If initiated
+                 * from Client SDK, an email with a link to join the team will be sent to the
+                 * new member's email address if the member doesn't exist in the project it
+                 * will be created automatically. If initiated from server side SDKs, new
+                 * member will automatically be added to the team.
                  *
                  * Use the 'URL' parameter to redirect the user from the invitation email back
                  * to your app. When the user is redirected, use the [Update Team Membership
                  * Status](/docs/client/teams#teamsUpdateMembershipStatus) endpoint to allow
-                 * the user to accept the invitation to the team.
+                 * the user to accept the invitation to the team.  While calling from side
+                 * SDKs the redirect url can be empty string.
                  *
                  * Please note that in order to avoid a [Redirect
                  * Attacks](https://github.com/OWASP/CheatSheetSeries/blob/master/cheatsheets/Unvalidated_Redirects_and_Forwards_Cheat_Sheet.md)
@@ -4155,7 +4202,14 @@
          * - documents.[ID]
          * - files
          * - files.[ID]
-         * @param {(payload: unknown) => void} callback Is called on every realtime update.
+         * - executions
+         * - executions.[ID]
+         * - functions.[ID]
+         * - teams
+         * - teams.[ID]
+         * - memberships
+         * - memberships.[ID]
+         * @param {(payload: RealtimeMessage) => void} callback Is called on every realtime update.
          * @returns {() => void} Unsubscribes from events.
          */
         subscribe(channels, callback) {
