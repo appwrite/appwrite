23ba9e7a54952e768f64cc4872c9037564af38cd
Damodar Lohani <dlohani48@gmail.com>
2021-09-28 13:36:09 +0545

    test for large function code

--- tests/e2e/Services/Functions/FunctionsCustomServerTest.php
+++ tests/e2e/Services/Functions/FunctionsCustomServerTest.php
@@ -229,6 +229,41 @@ class FunctionsCustomServerTest extends Scope
         $this->assertIsInt($tag['body']['dateCreated']);
         $this->assertEquals('php index.php', $tag['body']['command']);
         $this->assertGreaterThan(10000, $tag['body']['size']);
+
+        /**
+         * Test for Large Code File SUCCESS
+         */
+        $source = realpath(__DIR__ . '/../../../resources/functions/php-large.tar.gz');
+        $chunkSize = 5*1024*1024;
+        $handle = @fopen($source, "rb");
+        $mimeType = 'application/x-gzip';
+        $counter = 0;
+        $size = filesize($source);
+        $headers = [
+            'content-type' => 'multipart/form-data',
+            'x-appwrite-project' => $this->getProject()['$id']
+        ];
+        $id = '';
+        while (!feof($handle)) {
+            $curlFile = new \CURLFile('data://' . $mimeType . ';base64,' . base64_encode(@fread($handle, $chunkSize)), $mimeType, 'php-large-fx.tar.gz');
+            $headers['content-range'] = 'bytes ' . ($counter * $chunkSize) . '-' . min(((($counter * $chunkSize) + $chunkSize) - 1), $size) . '/' . $size;
+            if(!empty($id)) {
+                $headers['x-appwrite-id'] = $id;
+            }
+            $largeTag = $this->client->call(Client::METHOD_POST, '/functions/'.$data['functionId'].'/tags', array_merge($headers, $this->getHeaders()), [
+                'command' => 'php index.php',
+                'code' => $curlFile,
+            ]);
+            $counter++;
+            $id = $largeTag['body']['$id'];
+        }
+        @fclose($handle);
+
+        $this->assertEquals(201, $largeTag['headers']['status-code']);
+        $this->assertNotEmpty($largeTag['body']['$id']);
+        $this->assertIsInt($largeTag['body']['dateCreated']);
+        $this->assertEquals('php index.php', $largeTag['body']['command']);
+        $this->assertGreaterThan(10000, $largeTag['body']['size']);
        
         /**
          * Test for FAILURE
@@ -279,9 +314,9 @@ class FunctionsCustomServerTest extends Scope
         ], $this->getHeaders()));
 
         $this->assertEquals($function['headers']['status-code'], 200);
-        $this->assertEquals($function['body']['sum'], 1);
+        $this->assertEquals($function['body']['sum'], 2);
         $this->assertIsArray($function['body']['tags']);
-        $this->assertCount(1, $function['body']['tags']);
+        $this->assertCount(2, $function['body']['tags']);
 
         return $data;
     }
