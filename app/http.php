dfa5f6e6c4a0794009f25a1758ed0e9b1c6c9de6
Damodar Lohani <dlohani48@gmail.com>
2021-09-30 14:22:03 +0545

    correct comment

--- app/http.php
+++ app/http.php
@@ -19,7 +19,7 @@ use Utopia\Swoole\Request;
 
 $http = new Server("0.0.0.0", App::getEnv('PORT', 80));
 
-$payloadSize = 6 * (1024 * 1024); // 5MB
+$payloadSize = 6 * (1024 * 1024); // 6MB
 
 $http
     ->set([
