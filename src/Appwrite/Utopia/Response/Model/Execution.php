1b6f4514b2017823a32899d2ccdd084d19fa3427
Lakshay-Pruthi <91469717+Lakshay-Pruthi@users.noreply.github.com>
2021-10-07 13:06:10 +0530

    Update Execution.php

--- src/Appwrite/Utopia/Response/Model/Execution.php
+++ src/Appwrite/Utopia/Response/Model/Execution.php
@@ -69,7 +69,7 @@ class Execution extends Model
 
     /**
      * Get Name
-     * 
+     *
      * @return string
      */
     public function getName():string
@@ -79,7 +79,7 @@ class Execution extends Model
 
     /**
      * Get Collection
-     * 
+     *
      * @return string
      */
     public function getType():string
