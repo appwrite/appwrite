2b4bbc368b5031e5cb3579fbc5b02b49ac8e47c4
Lakshay-Pruthi <91469717+Lakshay-Pruthi@users.noreply.github.com>
2021-10-07 12:48:54 +0530

    Update Response.php

--- src/Appwrite/Utopia/Response.php
+++ src/Appwrite/Utopia/Response.php
@@ -175,7 +175,7 @@ class Response extends SwooleResponse
 
     /**
      * Response constructor.
-     * 
+     *
      * @param float $time
      */
     public function __construct(SwooleHTTPResponse $response)
@@ -275,7 +275,7 @@ class Response extends SwooleResponse
 
     /**
      * Set Model Object
-     * 
+     *
      * @return self
      */
     public function setModel(Model $instance)
@@ -287,7 +287,7 @@ class Response extends SwooleResponse
 
     /**
      * Get Model Object
-     * 
+     *
      * @return Model
      */
     public function getModel(string $key): Model
@@ -301,7 +301,7 @@ class Response extends SwooleResponse
 
     /**
      * Get Models List
-     * 
+     *
      * @return Model[]
      */
     public function getModels(): array
@@ -312,10 +312,10 @@ class Response extends SwooleResponse
     /**
      * Validate response objects and outputs
      *  the response according to given format type
-     * 
+     *
      * @param Document $document
      * @param string $model
-     * 
+     *
      * return void
      */
     public function dynamic(Document $document, string $model): void
@@ -323,7 +323,7 @@ class Response extends SwooleResponse
         $output = $this->output($document, $model);
 
         // If filter is set, parse the output
-        if(self::isFilter()){
+        if (self::isFilter()) {
             $output = self::getFilter()->parse($output, $model);
         }
 
@@ -332,10 +332,10 @@ class Response extends SwooleResponse
 
     /**
      * Generate valid response object from document data
-     * 
+     *
      * @param Document $document
      * @param string $model
-     * 
+     *
      * return array
      */
     public function output(Document $document, string $model): array
@@ -372,7 +372,7 @@ class Response extends SwooleResponse
                                 $condition = false;
                                 foreach ($this->getModel($type)->conditions as $attribute => $val) {
                                     $condition = $item->getAttribute($attribute) === $val;
-                                    if(!$condition) {
+                                    if (!$condition) {
                                         break;
                                     }
                                 }
@@ -437,32 +437,32 @@ class Response extends SwooleResponse
 
     /**
      * Function to set a response filter
-     * 
+     *
      * @param $filter the response filter to set
-     * 
+     *
      * @return void
      */
-    public static function setFilter(?Filter $filter) 
+    public static function setFilter(?Filter $filter)
     {
         self::$filter = $filter;
     }
 
     /**
      * Return the currently set filter
-     * 
+     *
      * @return Filter
      */
-    public static function getFilter(): ?Filter 
+    public static function getFilter(): ?Filter
     {
         return self::$filter;
     }
 
     /**
      * Check if a filter has been set
-     * 
+     *
      * @return bool
      */
-    public static function isFilter(): bool 
+    public static function isFilter(): bool
     {
         return self::$filter != null;
     }
