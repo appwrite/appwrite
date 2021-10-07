ac75201f702bdcbea7b452b065829fdad8967b28
Harsh Mehta <73291466+1n40@users.noreply.github.com>
2021-10-07 16:25:43 +0530

    Created welcome email feature

--- ruby/welcome-email.rb
+++ ruby/welcome-email.rb
@@ -1,1 +1,12 @@
-RB
+require 'json'
+require 'http'
+
+# Parsing data
+data = JSON.parse(ENV['APPWRITE_FUNCTION_EVENT_DATA'])
+name = data["name"]
+email = data["email"]
+
+# Using Mailgun API to send email
+response = HTTP.basic_auth(:user => 'auth', :pass => '%s' % [ENV['MAILGUN_API_KEY']]).post("https://api.mailgun.net/v3/%s/messages" % [ENV['MAILGUN_DOMAIN']], :form => {'from' => 'Excited User <mailgun@%s>' % [ENV['MAILGUN_DOMAIN']], 'to' => email, 'subject': 'Hello, %' % [name], 'text' => 'Testing some Mailgun awesomness!'})
+
+puts 'response code: %s' % [response.code.to_s]
