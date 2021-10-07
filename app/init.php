8537e140057150f6b53c2e4d607410c1351db08b
Tatiana <43054051+tacoelho@users.noreply.github.com>
2021-10-07 11:24:20 -0300

    Update init.php

--- app/init.php
+++ app/init.php
@@ -273,8 +273,8 @@ Locale::setLanguageFromJSON('da', __DIR__.'/config/locale/translations/da.json')
 Locale::setLanguageFromJSON('de', __DIR__.'/config/locale/translations/de.json');
 Locale::setLanguageFromJSON('el', __DIR__.'/config/locale/translations/el.json');
 Locale::setLanguageFromJSON('en', __DIR__.'/config/locale/translations/en.json');
+Locale::setLanguageFromJSON('eo', __DIR__.'/config/locale/translations/eo.json');
 Locale::setLanguageFromJSON('es', __DIR__.'/config/locale/translations/es.json');
-Locale::setLanguageFromJSON('esp', __DIR__.'/config/locale/translations/esp.json');
 Locale::setLanguageFromJSON('fa', __DIR__.'/config/locale/translations/fa.json');
 Locale::setLanguageFromJSON('fi', __DIR__.'/config/locale/translations/fi.json');
 Locale::setLanguageFromJSON('fo', __DIR__.'/config/locale/translations/fo.json');
