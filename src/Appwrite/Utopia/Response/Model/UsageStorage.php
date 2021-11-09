290f6a5625cc340622ebee982cc02c8d898b7460
Damodar Lohani <dlohani48@gmail.com>
2021-11-09 10:55:06 +0545

    update storage usage model

--- src/Appwrite/Utopia/Response/Model/UsageStorage.php
+++ src/Appwrite/Utopia/Response/Model/UsageStorage.php
@@ -23,9 +23,44 @@ class UsageStorage extends Model
                 'example' => new \stdClass,
                 'array' => true 
             ])
-            ->addRule('files', [
+            ->addRule('filesCount', [
                 'type' => Response::MODEL_METRIC_LIST,
                 'description' => 'Aggregated stats for total number of files.',
+                'default' => [],
+                'example' => new \stdClass,
+                'array' => true 
+            ])
+            ->addRule('bucketsCount', [
+                'type' => Response::MODEL_METRIC_LIST,
+                'description' => 'Aggregated stats for total number of buckets.',
+                'default' => [],
+                'example' => new \stdClass,
+                'array' => true 
+            ])
+            ->addRule('bucketsCreate', [
+                'type' => Response::MODEL_METRIC_LIST,
+                'description' => 'Aggregated stats for buckets created.',
+                'default' => [],
+                'example' => new \stdClass,
+                'array' => true 
+            ])
+            ->addRule('bucketsRead', [
+                'type' => Response::MODEL_METRIC_LIST,
+                'description' => 'Aggregated stats for buckets read.',
+                'default' => [],
+                'example' => new \stdClass,
+                'array' => true 
+            ])
+            ->addRule('bucketsUpdate', [
+                'type' => Response::MODEL_METRIC_LIST,
+                'description' => 'Aggregated stats for buckets updated.',
+                'default' => [],
+                'example' => new \stdClass,
+                'array' => true 
+            ])
+            ->addRule('bucketsDelete', [
+                'type' => Response::MODEL_METRIC_LIST,
+                'description' => 'Aggregated stats for buckets deleted.',
                 'default' => [],
                 'example' => new \stdClass,
                 'array' => true 
