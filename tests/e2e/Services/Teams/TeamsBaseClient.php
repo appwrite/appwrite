5c19fcd847fb767fea510baabcd5cb838b06425d
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-11-24 17:08:14 +0100

    fix(tests): adapt default id length

--- tests/e2e/Services/Teams/TeamsBaseClient.php
+++ tests/e2e/Services/Teams/TeamsBaseClient.php
@@ -73,8 +73,8 @@ trait TeamsBaseClient
         $this->assertEquals('Invitation to '.$teamName.' Team at '.$this->getProject()['name'], $lastEmail['subject']);
 
         $secret = substr($lastEmail['text'], strpos($lastEmail['text'], '&secret=', 0) + 8, 256);
-        $membershipUid = substr($lastEmail['text'], strpos($lastEmail['text'], '?membershipId=', 0) + 14, 13);
-        $userUid = substr($lastEmail['text'], strpos($lastEmail['text'], '&userId=', 0) + 8, 13);
+        $membershipUid = substr($lastEmail['text'], strpos($lastEmail['text'], '?membershipId=', 0) + 14, 20);
+        $userUid = substr($lastEmail['text'], strpos($lastEmail['text'], '&userId=', 0) + 8, 20);
 
         /**
          * Test for FAILURE
