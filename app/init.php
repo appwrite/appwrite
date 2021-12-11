76bf24ac9b31236cc53cc171225be6549f0e362b
Eldad Fux <eldad.fux@gmail.com>
2021-12-11 02:17:28 +0200

    Updated Youtube URL

--- app/init.php
+++ app/init.php
@@ -64,7 +64,7 @@ const APP_SOCIAL_DISCORD = 'https://appwrite.io/discord';
 const APP_SOCIAL_DISCORD_CHANNEL = '564160730845151244';
 const APP_SOCIAL_DEV = 'https://dev.to/appwrite';
 const APP_SOCIAL_STACKSHARE = 'https://stackshare.io/appwrite'; 
-const APP_SOCIAL_YOUTUBE = 'https://www.youtube.com/c/appwrite';
+const APP_SOCIAL_YOUTUBE = 'https://www.youtube.com/c/appwrite?sub_confirmation=1';
 
 // Deletion Types
 const DELETE_TYPE_DOCUMENT = 'document';
