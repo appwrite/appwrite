8fbb6d10570d3aaa9022bf69bbc2de7b5806c197
Christy Jacob <christyjacob4@gmail.com>
2021-12-13 14:37:05 +0400

    feat: update user agent name to Appwrite CLI

--- src/Appwrite/Detector/Detector.php
+++ src/Appwrite/Detector/Detector.php
@@ -47,7 +47,7 @@ class Detector
      */
     public function getClient(): array
     {
-        if (strpos($this->userAgent, 'Terminal') !== false) {
+        if (strpos($this->userAgent, 'Appwrite CLI') !== false) {
             $version = explode(' ', $this->userAgent)[0];
             $version = explode('/', $version)[1];
             $client = [
