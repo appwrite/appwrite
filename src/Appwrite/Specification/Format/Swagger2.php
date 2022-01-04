304981da248d5117070657e820713efb27d0681a
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-01-04 11:42:23 +0100

    fix: specs definitions

--- src/Appwrite/Specification/Format/Swagger2.php
+++ src/Appwrite/Specification/Format/Swagger2.php
@@ -392,15 +392,11 @@ class Swagger2 extends Format
 
         foreach ($this->models as $model) {
             foreach ($model->getRules() as $rule) {
-                if (!in_array($rule['type'], ['string', 'integer', 'boolean', 'json', 'float'])) {
-                    if(\is_array($rule['type'])) {
-                        foreach ($rule['type'] as $value) {
-                            $usedModels[] = $value;
-                        }
-                    }
-                    else {
-                        $usedModels[] = $rule['type'];
-                    }
+                if (
+                    in_array($model->getType(), $usedModels)
+                    && !in_array($rule['type'], ['string', 'integer', 'boolean', 'json', 'float'])
+                ) {
+                    $usedModels[] = $rule['type'];
                 }
             }
         }
