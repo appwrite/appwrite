1a6a1498bc80c831e30db5867aa1704d3b9c4b07
Jake Barnby <jakeb994@gmail.com>
2022-01-19 00:45:44 +1300

    Add fulltext index for collections collection `name` column to fix `listCollections` search

--- app/config/collections.php
+++ app/config/collections.php
@@ -102,6 +102,13 @@ $collections = [
                 'lengths' => [1024],
                 'orders' => [Database::ORDER_ASC],
             ],
+            [
+                '$id' => '_fulltext_name',
+                'type' => Database::INDEX_FULLTEXT,
+                'attributes' => ['name'],
+                'lengths' => [256],
+                'orders' => [Database::ORDER_ASC],
+            ],
         ],
     ],
 
