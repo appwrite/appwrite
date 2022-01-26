32a066fb482d109c1859bc32ea5babfadcf87a79
Matej Baco <matejbacocom@gmail.com>
2022-01-26 10:17:55 +0100

    Cleanup

--- tests/e2e/Services/Database/DatabaseCustomClientTest.php
+++ tests/e2e/Services/Database/DatabaseCustomClientTest.php
@@ -6,8 +6,6 @@ use Tests\E2E\Client;
 use Tests\E2E\Scopes\Scope;
 use Tests\E2E\Scopes\ProjectCustom;
 use Tests\E2E\Scopes\SideClient;
-use function array_merge;
-use function sleep;
 
 class DatabaseCustomClientTest extends Scope
 {
