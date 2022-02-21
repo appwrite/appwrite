1cfc6a8a2d27af276b6486664dbb0e4bed8e1d5f
Christy Jacob <christyjacob4@gmail.com>
2022-02-21 15:11:48 +0400

    fix: review comments

--- app/controllers/api/projects.php
+++ app/controllers/api/projects.php
@@ -598,7 +598,7 @@ App::post('/v1/projects/:projectId/webhooks')
             throw new Exception('Project not found', 404);
         }
 
-        $security = ($security === '1' || $security === 'true' || $security === 1 || $security === true);
+        $security = (bool) filter_var($security, FILTER_VALIDATE_BOOLEAN);
 
         $webhook = new Document([
             '$id' => $dbForConsole->getId(),
