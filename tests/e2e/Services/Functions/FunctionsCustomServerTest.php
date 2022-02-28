baba4ac8913502e36ac3f8985ffe912fb1a51faf
Christy Jacob <christyjacob4@gmail.com>
2022-02-28 21:03:52 +0400

    feat: increase timeout

--- tests/e2e/Services/Functions/FunctionsCustomServerTest.php
+++ tests/e2e/Services/Functions/FunctionsCustomServerTest.php
@@ -1035,7 +1035,7 @@ class FunctionsCustomServerTest extends Scope
         $this->assertEquals(201, $deployment['headers']['status-code']);
 
         // Allow build step to run
-        sleep(10);
+        sleep(30);
 
         $execution = $this->client->call(Client::METHOD_POST, '/functions/'.$functionId.'/executions', array_merge([
             'content-type' => 'application/json',
