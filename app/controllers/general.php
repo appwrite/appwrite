4846a2e1dd9ef3a280cb79f3d6967cb855eee50b
Matej Bačo <matejbaco2000@gmail.com>
2022-04-25 09:04:08 +0000

    Add const usage

--- app/controllers/general.php
+++ app/controllers/general.php
@@ -12,6 +12,7 @@ use Appwrite\Extend\Exception;
 use Utopia\Config\Config;
 use Utopia\Domains\Domain;
 use Appwrite\Auth\Auth;
+use Appwrite\Event\Event;
 use Appwrite\Network\Validator\Origin;
 use Appwrite\Utopia\Response\Filters\V11 as ResponseV11;
 use Appwrite\Utopia\Response\Filters\V12 as ResponseV12;
@@ -101,7 +102,7 @@ App::init(function ($utopia, $request, $response, $console, $project, $dbForCons
     
                     Console::info('Issuing a TLS certificate for the main domain (' . $domain->get() . ') in a few seconds...');
     
-                    Resque::enqueue('v1-certificates', 'CertificatesV1', [
+                    Resque::enqueue(Event::CERTIFICATES_QUEUE_NAME, Event::CERTIFICATES_CLASS_NAME, [
                         'document' => $domainDocument,
                         'domain' => $domain->get(),
                         'validateTarget' => false,
