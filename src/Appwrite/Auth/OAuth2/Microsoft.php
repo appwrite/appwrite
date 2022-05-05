7e4e0135b69586477b02592e6ea093ad0eb1712a
Christy Jacob <christyjacob4@gmail.com>
2022-05-05 15:09:43 +0400

    feat: added check for Microsoft OAuth

--- src/Appwrite/Auth/OAuth2/Microsoft.php
+++ src/Appwrite/Auth/OAuth2/Microsoft.php
@@ -140,13 +140,17 @@ class Microsoft extends OAuth2
     /**
      * Check if the OAuth email is verified
      * 
+     * If present, the email is verified. This was verfied through a manual Microsoft sign up process
+     * 
      * @param $accessToken
      * 
      * @return bool
      */
     public function isEmailVerified(string $accessToken): bool
     {
-        return false;
+        $email = $this->getUserEmail($accessToken);
+
+        return !empty($email);
     }
 
     /**
