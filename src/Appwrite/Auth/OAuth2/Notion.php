cd31cf22d5efbe7276363397d3992f83bb4fc8e8
Christy Jacob <christyjacob4@gmail.com>
2022-05-05 14:59:32 +0400

    feat: added check for Notion OAuth

--- src/Appwrite/Auth/OAuth2/Notion.php
+++ src/Appwrite/Auth/OAuth2/Notion.php
@@ -137,13 +137,17 @@ class Notion extends OAuth2
     /**
      * Check if the OAuth email is verified
      * 
+     * If present, the email is verified. This was verfied through a manual Notion sign up process
+     * 
      * @param $accessToken
      * 
      * @return bool
      */
     public function isEmailVerified(string $accessToken): bool
     {
-        return false;
+        $email = $this->getUserEmail($accessToken);
+
+        return !empty($email);
     }
 
     /**
