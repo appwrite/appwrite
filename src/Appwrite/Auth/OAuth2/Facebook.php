0a13706f0fd007fd41a655b96ad853e9bcf8823a
Christy Jacob <christyjacob4@gmail.com>
2022-05-06 00:41:35 +0400

    feat: added check for Facebook OAuth

--- src/Appwrite/Auth/OAuth2/Facebook.php
+++ src/Appwrite/Auth/OAuth2/Facebook.php
@@ -132,13 +132,17 @@ class Facebook extends OAuth2
     /**
      * Check if the OAuth email is verified
      * 
+     * If present, the email is verified. This was verfied through a manual Facebook sign up process
+     * 
      * @param $accessToken
      * 
      * @return bool
      */
     public function isEmailVerified(string $accessToken): bool
     {
-        return false;
+        $email = $this->getUserEmail($accessToken);
+
+        return !empty($email);
     }
 
     /**
