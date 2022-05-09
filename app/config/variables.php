128748e62640a559761b96d8d6aabaf0034e6781
Shimon Newman <shimonewman@gmail.com>
2022-05-09 09:04:38 +0300

    Update app/config/variables.php
    Co-authored-by: Damodar Lohani <lohanidamodar@users.noreply.github.com>

--- app/config/variables.php
+++ app/config/variables.php
@@ -456,7 +456,7 @@ return [
             ],
             [
                 'name' => '_APP_STORAGE_S3_SECRET',
-                'description' => 'AWS S3 storage secret key. Required when the storage adapter is set to S3. You can get your secret key from your AWS console (this applies also to S3 S3 compatible storage like DigitalOcean).',
+                'description' => 'AWS S3 storage secret key. Required when the storage adapter is set to S3. You can get your secret key from your AWS console (this applies also to S3 compatible storage like DigitalOcean).',
                 'introduction' => '0.13.0',
                 'default' => '',
                 'required' => false,
