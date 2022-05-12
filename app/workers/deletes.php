f9da67ec3ea2d23c6b52f10b6dd14db5f58b328b
Everly Precia Suresh <everlyprecia@gmail.com>
2022-05-12 22:33:08 +0000

    format code

--- app/workers/deletes.php
+++ app/workers/deletes.php
@@ -545,7 +545,7 @@ class DeletesV1 extends Worker
         $dbForProject = $this->getProjectDB($projectId);
         $dbForProject->deleteCollection('bucket_' . $document->getInternalId());
 
-        $device=$this->getDevice(APP_STORAGE_UPLOADS.'/app-'.$projectId);
+        $device = $this->getDevice(APP_STORAGE_UPLOADS.'/app-'.$projectId);
         
         $device->deletePath($document->getId());
     }
