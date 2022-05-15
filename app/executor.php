b466ad7fa6a3aca8e7c557d70121fd7c1f481dce
Damodar Lohani <lohanidamodar@users.noreply.github.com>
2022-05-15 13:22:21 +0545

    Update app/executor.php
    Co-authored-by: Matej Bačo <matejbacocom@gmail.com>

--- app/executor.php
+++ app/executor.php
@@ -584,7 +584,7 @@ $http->on('start', function ($http) {
     /** 
      * Warmup: make sure images are ready to run fast 🚀
      */
-    $runtimes = new Runtimes(App::getEnv('_APP_EXECUTOR_VERSION'));
+    $runtimes = new Runtimes(App::getEnv('_APP_EXECUTOR_VERSION', 'v1'));
     $allowList = empty(App::getEnv('_APP_FUNCTIONS_RUNTIMES')) ? [] : \explode(',', App::getEnv('_APP_FUNCTIONS_RUNTIMES'));
     $runtimes = $runtimes->getAll(true, $allowList);
     foreach ($runtimes as $runtime) {
