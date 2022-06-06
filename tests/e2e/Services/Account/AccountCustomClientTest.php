742b49723f7c2536ab84cddc2424f4a01bd0da08
Jake Barnby <jakeb994@gmail.com>
2022-06-07 09:53:09 +1200

    Check for ratelimit remaining in create jwt test

--- tests/e2e/Services/Account/AccountCustomClientTest.php
+++ tests/e2e/Services/Account/AccountCustomClientTest.php
@@ -270,6 +270,7 @@ class AccountCustomClientTest extends Scope
         ]));
 
         $this->assertEquals($response['headers']['status-code'], 201);
+        $this->assertEquals($response['headers']['X-RateLimit-Remaining'], 99);
         $this->assertNotEmpty($response['body']['jwt']);
         $this->assertIsString($response['body']['jwt']);
 
