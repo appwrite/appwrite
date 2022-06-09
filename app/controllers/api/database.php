d96b077d222e3a10cf5564ab401517531cb688b8
gepd <guillermoepd@hotmail.com>
2022-06-09 15:32:34 -0400

    remove null as param is already nullable
    Co-authored-by: Everly Precia Suresh <77877486+everly-gif@users.noreply.github.com>

--- app/controllers/api/database.php
+++ app/controllers/api/database.php
@@ -1868,7 +1868,7 @@ App::patch('/v1/database/collections/:collectionId/documents/:documentId')
     ->inject('usage')
     ->inject('events')
     ->inject('mode')
-    ->action(function (string $collectionId, string $documentId, string|array|null $data, ?array $read, ?array $write, Response $response, Database $dbForProject, EventAudit $audits, Stats $usage, Event $events, string $mode) {
+    ->action(function (string $collectionId, string $documentId, string|array $data, ?array $read, ?array $write, Response $response, Database $dbForProject, EventAudit $audits, Stats $usage, Event $events, string $mode) {
 
         /**
          * Skip Authorization to get the collection. Needed in case of empty permissions for document level permissions.
