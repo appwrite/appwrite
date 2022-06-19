0e32cf02cb80c08f73e808307c493483f926cebe
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-06-19 11:34:03 +0200

    chore: run linter

--- app/config/storage/logos.php
+++ app/config/storage/logos.php
@@ -6,15 +6,15 @@ return [ // Based on this list @see http://stackoverflow.com/a/4212908/2299554
 
     // Video Files
 
-    'video/mp4' => __DIR__.'/logos/video.png',
-    'video/x-flv' => __DIR__.'/logos/video.png',
-    'video/webm' => __DIR__.'/logos/video.png',
-    'application/x-mpegURL' => __DIR__.'/logos/video.png',
-    'video/MP2T' => __DIR__.'/logos/video.png',
-    'video/3gpp' => __DIR__.'/logos/video.png',
-    'video/quicktime' => __DIR__.'/logos/video.png',
-    'video/x-msvideo' => __DIR__.'/logos/video.png',
-    'video/x-ms-wmv' => __DIR__.'/logos/video.png',
+    'video/mp4' => __DIR__ . '/logos/video.png',
+    'video/x-flv' => __DIR__ . '/logos/video.png',
+    'video/webm' => __DIR__ . '/logos/video.png',
+    'application/x-mpegURL' => __DIR__ . '/logos/video.png',
+    'video/MP2T' => __DIR__ . '/logos/video.png',
+    'video/3gpp' => __DIR__ . '/logos/video.png',
+    'video/quicktime' => __DIR__ . '/logos/video.png',
+    'video/x-msvideo' => __DIR__ . '/logos/video.png',
+    'video/x-ms-wmv' => __DIR__ . '/logos/video.png',
 
 
 
