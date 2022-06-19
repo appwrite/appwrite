87d85d14d4ffdf0451be8ade5b7f07922d534acb
Matej Bačo <matejbaco2000@gmail.com>
2022-06-19 08:17:10 +0000

    Fix swagger2 docs with array model-to-model refference

--- src/Appwrite/Specification/Format/Swagger2.php
+++ src/Appwrite/Specification/Format/Swagger2.php
@@ -401,11 +401,20 @@ class Swagger2 extends Format
 
         foreach ($this->models as $model) {
             foreach ($model->getRules() as $rule) {
-                if (
-                    in_array($model->getType(), $usedModels)
-                    && !in_array($rule['type'], ['string', 'integer', 'boolean', 'json', 'float'])
-                ) {
-                    $usedModels[] = $rule['type'];
+                if (!in_array($model->getType(), $usedModels)) {
+                    continue;
+                }
+
+                if (\is_array($rule['type'])) {
+                    foreach ($rule['type'] as $ruleType) {
+                        if (!in_array($ruleType, ['string', 'integer', 'boolean', 'json', 'float']) ) {
+                            $usedModels[] = $ruleType;
+                        }
+                    }
+                } else {
+                    if (!in_array($rule['type'], ['string', 'integer', 'boolean', 'json', 'float']) ) {
+                        $usedModels[] = $rule['type'];
+                    }
                 }
             }
         }
