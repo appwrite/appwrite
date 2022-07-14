c154ea3fc6aaff17297e3337a9eee3cde5c14681
Jake Barnby <jakeb994@gmail.com>
2022-07-14 13:47:32 +1200

    Add benchmark scripts

---
+++ tests/benchmarks/graphql.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+export const options = {
+    vus: 20,
+    duration: '60s',
+};
+export default function () {
+    http.post('http://localhost/v1/graphql', JSON.stringify({
+        query: 'query { localeGetCountries { total countries { code name } } }',
+    }), {
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Appwrite-Project': 'test',
+        }
+    })
+}
---
+++ tests/benchmarks/rest.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+export const options = {
+    vus: 20,
+    duration: '60s',
+};
+export default function () {
+    http.get('http://localhost/v1/locale/countries', {
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Appwrite-Project': 'test',
+        }
+    })
+}
