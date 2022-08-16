34fd6763e11d99841d2b83b870797d9bf5d19830
Damodar Lohani <lohanidamodar@users.noreply.github.com>
2022-08-16 11:14:55 +0545

    Update app/controllers/api/account.php
    Co-authored-by: Eldad A. Fux <eldad.fux@gmail.com>

--- app/controllers/api/account.php
+++ app/controllers/api/account.php
@@ -12,7 +12,6 @@ use Appwrite\Network\Validator\Email;
 use Appwrite\Network\Validator\Host;
 use Appwrite\Network\Validator\URL;
 use Appwrite\OpenSSL\OpenSSL;
-use Appwrite\Usage\Stats;
 use Appwrite\Template\Template;
 use Appwrite\URL\URL as URLParser;
 use Appwrite\Utopia\Request;
