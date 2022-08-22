dd00c58b08b789dec6e7806e3db563f31c889ec3
Christy Jacob <christyjacob4@gmail.com>
2022-08-22 14:14:40 +0000

    feat: remove queries

---
+++ src/Appwrite/Utopia/Database/Validator/Queries/Users.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Appwrite\Utopia\Database\Validator\Queries;
+
+use Appwrite\Utopia\Database\Validator\Queries\Collection;
+
+class Users extends Collection
+{
+    public const ALLOWED_ATTRIBUTES = [
+        '$id',
+        '$createdAt',
+        '$updatedAt',
+        'name',
+        'email',
+        'phone',
+        'status',
+        'passwordUpdate',
+        'registration',
+        'emailVerification',
+        'phoneVerification',
+    ];
+
+    /**
+     * Expression constructor
+     *
+     */
+    public function __construct()
+    {
+        parent::__construct('users', self::ALLOWED_ATTRIBUTES);
+    }
+}
