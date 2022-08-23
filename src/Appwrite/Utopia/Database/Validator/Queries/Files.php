d1f6cb31d6d4fce27210d85f548388affc6b4f8a
Matej Bačo <matejbaco2000@gmail.com>
2022-08-23 08:49:39 +0000

    Upgrade listFiles queries

---
+++ src/Appwrite/Utopia/Database/Validator/Queries/Files.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Appwrite\Utopia\Database\Validator\Queries;
+
+use Appwrite\Utopia\Database\Validator\Queries\Collection;
+
+class Files extends Collection
+{
+    public const ALLOWED_ATTRIBUTES = [
+        '$id',
+        '$createdAt',
+        '$updatedAt',
+        
+        'name',
+        'signature',
+        'mimeType',
+        'sizeOriginal',
+        'chunksTotal',
+        'chunksUploaded'
+    ];
+
+    /**
+     * Expression constructor
+     *
+     */
+    public function __construct()
+    {
+        parent::__construct('files', self::ALLOWED_ATTRIBUTES);
+    }
+}
