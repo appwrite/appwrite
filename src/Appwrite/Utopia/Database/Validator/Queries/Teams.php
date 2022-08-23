6c971f6eceaec9c1630f44444b70a2cbf20bd2f1
Matej Bačo <matejbaco2000@gmail.com>
2022-08-23 08:56:28 +0000

    Upgrade listTeams queries

---
+++ src/Appwrite/Utopia/Database/Validator/Queries/Teams.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Appwrite\Utopia\Database\Validator\Queries;
+
+use Appwrite\Utopia\Database\Validator\Queries\Collection;
+
+class Teams extends Collection
+{
+    public const ALLOWED_ATTRIBUTES = [
+        '$id',
+        '$createdAt',
+        '$updatedAt',
+        
+        'name',
+        'total'
+    ];
+
+    /**
+     * Expression constructor
+     *
+     */
+    public function __construct()
+    {
+        parent::__construct('teams', self::ALLOWED_ATTRIBUTES);
+    }
+}
