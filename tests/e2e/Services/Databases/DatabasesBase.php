0389be522e05b77304a4c69f1a55037b72bf1f36
Jake Barnby <jakeb994@gmail.com>
2022-08-27 20:29:32 +1200

    Only give current user create access in create collection test

--- tests/e2e/Services/Databases/DatabasesBase.php
+++ tests/e2e/Services/Databases/DatabasesBase.php
@@ -50,10 +50,7 @@ trait DatabasesBase
             'name' => 'Movies',
             'documentSecurity' => true,
             'permissions' => [
-                Permission::read(Role::any()),
-                Permission::create(Role::any()),
-                Permission::update(Role::any()),
-                Permission::delete(Role::any()),
+                Permission::create(Role::user($this->getUser()['$id'])),
             ],
         ]);
 
@@ -2289,13 +2286,20 @@ trait DatabasesBase
         $this->assertCount(0, $document['body']['$permissions']);
         $this->assertEquals([], $document['body']['$permissions']);
 
-        // Check user can still read document due to collection permissions of read("any")
+        // Check client side can no longer read the document.
         $document = $this->client->call(Client::METHOD_GET, '/databases/' . $databaseId . '/collections/' . $data['moviesId'] . '/documents/' . $id, array_merge([
             'content-type' => 'application/json',
             'x-appwrite-project' => $this->getProject()['$id'],
         ], $this->getHeaders()));
 
-        $this->assertEquals(200, $document['headers']['status-code']);
+        switch ($this->getSide()) {
+            case 'client':
+                $this->assertEquals(404, $document['headers']['status-code']);
+                break;
+            case 'server':
+                $this->assertEquals(200, $document['headers']['status-code']);
+                break;
+        }
 
         return $data;
     }
