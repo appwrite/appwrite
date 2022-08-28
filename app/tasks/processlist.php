533139a0b719fd20d3c426095eec84c7f60e3a0a
fogelito <fogelshmuel@gmail.com>
2022-08-28 18:50:07 +0300

    first try cli processlist.php

--- app/tasks/processlist.php
+++ app/tasks/processlist.php
@@ -3,9 +3,7 @@
 global $cli, $register;
 
 use Appwrite\Extend\PDO;
-use Appwrite\Migration\Migration;
 use Utopia\Cache\Adapter\None;
-use Utopia\Cache\Adapter\Redis as RedisCache;
 use Utopia\CLI\Console;
 use Utopia\App;
 use Utopia\Cache\Cache;
@@ -19,6 +17,8 @@ $cli
     ->task('process')
     ->param('version', APP_VERSION_STABLE, new Text(8), 'Version to migrate to.', false)
     ->action(function ($version) use ($register) {
+
+        //php app/cli.php process
         Authorization::disable();
         $app = new App('UTC');
 
@@ -26,36 +26,28 @@ $cli
 
        // $db = $register->get('db', true);
 
-        $dbHost = 'mariadb';
+        $dbHost = '127.0.0.1';
         $dbPort = '3306';
         $dbUser = 'user';
         $dbPass = 'password';
         $dbScheme = 'appwrite';
 
-        $db = new PDO("mysql:host={$dbHost};port={$dbPort};dbname={$dbScheme};charset=utf8mb4", $dbUser, $dbPass, array(
-            PDO::ATTR_TIMEOUT => 3, // Seconds
-            PDO::ATTR_PERSISTENT => true,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_EMULATE_PREPARES => true,
-            PDO::ATTR_STRINGIFY_FETCHES => true,
-        ));
+        $db = new PDO("mysql:host={$dbHost};port={$dbPort};dbname={$dbScheme};charset=utf8mb4", $dbUser, $dbPass, MariaDB::getPdoAttributes());
         Console::error('Hello 2');
 
-        $redis = $register->get('cache', true);
-        $redis->flushAll();
-        $cache = new Cache(new RedisCache($redis));
+        $cache = new Cache(new None());
 
-        $projectDB = new Database(new MariaDB($db), $cache);
-        $projectDB->setDefaultDatabase(App::getEnv('_APP_DB_SCHEMA', 'appwrite'));
+//        $projectDB = new Database(new MariaDB($db), $cache);
+//        $projectDB->setDefaultDatabase(App::getEnv('_APP_DB_SCHEMA', 'appwrite'));
 
         $consoleDB = new Database(new MariaDB($db), $cache);
         $consoleDB->setDefaultDatabase(App::getEnv('_APP_DB_SCHEMA', 'appwrite'));
-        $consoleDB->setNamespace('_project_console');
-        $console = $app->getResource('console');
+        $consoleDB->setNamespace('_console');
 
         $totalProjects = $consoleDB->count('projects') + 1;
-        Swoole\Event::wait(); // Wait for Coroutines to finish
+        $x = $consoleDB->getAdapter('projects') + 1;
+
+        //Swoole\Event::wait(); // Wait for Coroutines to finish
         Console::success('Finish!');
 
     });
