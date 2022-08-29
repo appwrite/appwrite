8864654fa388bc1ecd9d7ffc6bc133070cac71dd
fogelito <fogelshmuel@gmail.com>
2022-08-29 12:48:06 +0300

    loop processlist.php

--- app/tasks/processlist.php
+++ app/tasks/processlist.php
@@ -5,26 +5,16 @@ global $cli, $register;
 use Appwrite\Extend\PDO;
 use Utopia\Cache\Adapter\None;
 use Utopia\CLI\Console;
-use Utopia\App;
 use Utopia\Cache\Cache;
 use Utopia\Database\Adapter\MariaDB;
 use Utopia\Database\Database;
-use Utopia\Database\Query;
-use Utopia\Database\Validator\Authorization;
-use Utopia\Validator\Text;
 
 $cli
     ->task('process')
-    ->param('version', APP_VERSION_STABLE, new Text(8), 'Version to migrate to.', false)
-    ->action(function ($version) use ($register) {
+    ->action(function () use ($register) {
 
-        //php app/cli.php process
-        Authorization::disable();
-        $app = new App('UTC');
-
-        Console::error('Hello 1');
-
-       // $db = $register->get('db', true);
+        // php app/cli.php process
+        // $db = $register->get('db', true);
 
         $dbHost = '127.0.0.1';
         $dbPort = '3306';
@@ -33,21 +23,14 @@ $cli
         $dbScheme = 'appwrite';
 
         $db = new PDO("mysql:host={$dbHost};port={$dbPort};dbname={$dbScheme};charset=utf8mb4", $dbUser, $dbPass, MariaDB::getPdoAttributes());
-        Console::error('Hello 2');
 
         $cache = new Cache(new None());
-
-//        $projectDB = new Database(new MariaDB($db), $cache);
-//        $projectDB->setDefaultDatabase(App::getEnv('_APP_DB_SCHEMA', 'appwrite'));
-
         $consoleDB = new Database(new MariaDB($db), $cache);
-        $consoleDB->setDefaultDatabase(App::getEnv('_APP_DB_SCHEMA', 'appwrite'));
-        $consoleDB->setNamespace('_console');
-
-        $totalProjects = $consoleDB->count('projects') + 1;
-        $x = $consoleDB->getAdapter('projects') + 1;
+        $processes = $consoleDB->getProcessList();
+        foreach ($processes as $proc) {
+            var_dump($proc);
+        }
 
         //Swoole\Event::wait(); // Wait for Coroutines to finish
-        Console::success('Finish!');
-
+        Console::success('Finish ! 2');
     });
