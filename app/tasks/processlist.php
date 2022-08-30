5daa706fb4f8a27475b8766371d584096bb2c0f1
fogelito <fogelshmuel@gmail.com>
2022-08-30 20:23:47 +0300

    process document

--- app/tasks/processlist.php
+++ app/tasks/processlist.php
@@ -2,12 +2,18 @@
 
 global $cli, $register;
 
+use Appwrite\Auth\Auth;
 use Appwrite\Extend\PDO;
 use Utopia\Cache\Adapter\None;
 use Utopia\CLI\Console;
 use Utopia\Cache\Cache;
 use Utopia\Database\Adapter\MariaDB;
 use Utopia\Database\Database;
+use Utopia\Database\DateTime;
+use Utopia\Database\Document;
+use Utopia\Database\Permission;
+use Utopia\Database\Role;
+use Utopia\Database\Validator\Authorization;
 use Utopia\Validator\WhiteList;
 
 $cli
@@ -15,6 +21,9 @@ $cli
     ->param('name', null, new WhiteList(['MariaDB'], true), 'Adapter List', false)
     ->action(function ($name) use ($register) {
         $consoleDB = null;
+
+        $timeInSecondToTerminate = 10;
+
         switch ($name) {
             case 'MariaDB':
                 echo "i equals 0";
@@ -34,8 +43,28 @@ $cli
         }
 
         $processes = $consoleDB->getProcessList();
-        foreach ($processes as $proc){
-            var_dump($processes);
+        foreach ($processes as $proc) {
+            if ((int)$proc['Time'] > $timeInSecondToTerminate) {
+
+                $document = Authorization::skip(fn() => $consoleDB->createDocument('processes', new Document([
+                    'thread' => $proc['Id'],
+                    'seconds' => $proc['Time'],
+                    'user' => $proc['User'],
+                    'host' => $proc['Host'],
+                    'db' => $proc['Command'],
+                    'state' => $proc['State'],
+                    'info' => $proc['Info'],
+                    'progress' => $proc['Progress'],
+                    'json' => [],
+                    '$permissions' => [
+                        Permission::read(Role::any()),
+                        Permission::update(Role::any()),
+                        Permission::delete(Role::any()),
+                    ],
+                ])));
+
+                var_dump($proc);
+            }
         }
         //Swoole\Event::wait(); // Wait for Coroutines to finish
         Console::success('Finish ! 2');
