cee77ef5a1d447bbe9d29c212ded8aa1905a6a89
shimon <shimon@appwrite.io>
2022-08-30 15:52:26 +0300

    timestamp -> datetime on file cache

--- app/controllers/shared/api.php
+++ app/controllers/shared/api.php
@@ -381,7 +381,6 @@ App::shutdown()
                     'accessedAt' => $now,
                     'signature' => $signature,
                     ])));
-
                 } elseif (date('Y/m/d', \time()) > date('Y/m/d', strtotime($cacheLog->getAttribute('accessedAt')))) {
                     var_dump('update');
                     var_dump($now);
