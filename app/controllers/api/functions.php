2534e9ad8f7d0704f4f0fc8921db3f86dd8c2841
Matej Bačo <matejbaco2000@gmail.com>
2022-08-30 08:20:54 +0000

    Remove unnessessary internal queries

--- app/controllers/api/functions.php
+++ app/controllers/api/functions.php
@@ -1379,9 +1379,6 @@ App::get('/v1/functions/:functionId/variables')
             $queries[] = Query::search('search', $search);
         }
 
-        // Set default limit
-        $queries[] = Query::limit(25);
-
         // Get cursor document if there was a cursor query
         $cursor = Query::getByType($queries, Query::TYPE_CURSORAFTER, Query::TYPE_CURSORBEFORE)[0] ?? null;
         if ($cursor !== null) {
