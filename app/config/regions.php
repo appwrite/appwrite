0848815485c04b08f3bd81dd1a7a3eeb65caca4e
Damodar Lohani <dlohani48@gmail.com>
2022-09-09 01:00:51 +0000

    remove other regions

--- app/config/regions.php
+++ app/config/regions.php
@@ -5,15 +5,5 @@ return [
         'name' => 'Default',
         'default' => true,
         'disabled' => false,
-    ],
-    'fra001' => [
-        'name' => 'Frankfurt',
-        'default' => false,
-        'disabled' => false,
-    ],
-    'nyc001' => [
-        'name' => 'New York',
-        'default' => false,
-        'disabled' => true,
-    ],
+    ]
 ];
