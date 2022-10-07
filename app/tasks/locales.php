c6792c1034fe10f3b0044f115255a4f6ce742df9
Matej Bačo <matejbaco2000@gmail.com>
2022-10-07 07:56:01 +0000

    Code formatting

--- app/tasks/locales.php
+++ app/tasks/locales.php
@@ -18,7 +18,7 @@ $cli
 
         $mainLocale = new Locale($mainLanguage);
         foreach ($mainLocale->getTranslations() as $key => $value) {
-            if(!empty($value)) {
+            if (!empty($value)) {
                 $mainTranslations[] = $key;
             }
         }
@@ -30,31 +30,30 @@ $cli
             $translations = $locale->getTranslations();
             $missing = [];
             foreach ($mainTranslations as $translation) {
-                if(empty($translations[$translation])) {
+                if (empty($translations[$translation])) {
                     $missing[] = $translation;
                 }
             }
 
-            if(\count($missing) <= 0) {
+            if (\count($missing) <= 0) {
                 $type !== 'json' && Console::success('Locale "' . $language . '" is fully translated.');
             } else {
                 $type !== 'json' && Console::error('Locale "' . $language . '" is missing ' . \count($missing) . ' translations.');
 
-                if($type === 'debug') {
+                if ($type === 'debug') {
                     foreach ($missing as $key) {
                         Console::log('"' . $key . '" missing in "' . $language . '".');
                     }
-                } else if ($type === 'json') {
+                } elseif ($type === 'json') {
                     $results[$language] = $missing;
                 }
             }
         }
 
         $type !== 'json'  && Console::info('To get detailed information about missing translations, run the command with --type=debug parameter. Use --type=json to get JSON formatted results.');
-        
-        if($type === 'json') {
+
+        if ($type === 'json') {
             \var_dump($results);
             Console::log(\json_encode($results));
         }
-    
     });
