4250234231c2754bf60fc18e40ac308b3dfb48e8
fogelito <fogelshmuel@gmail.com>
2022-10-11 13:34:42 +0300

    Model MODEL_METRIC changes

--- src/Appwrite/Utopia/Response/Model/UsageCollection.php
+++ src/Appwrite/Utopia/Response/Model/UsageCollection.php
@@ -17,38 +17,38 @@ class UsageCollection extends Model
                 'example' => '30d',
             ])
             ->addRule('documentsCount', [
-                'type' => Response::MODEL_METRIC_LIST,
+                'type' => Response::MODEL_METRIC,
                 'description' => 'Aggregated stats for total number of documents.',
                 'default' => [],
-                'example' => new \stdClass(),
+                'example' => [],
                 'array' => true
             ])
             ->addRule('documentsCreate', [
-                'type' => Response::MODEL_METRIC_LIST,
+                'type' => Response::MODEL_METRIC,
                 'description' => 'Aggregated stats for documents created.',
                 'default' => [],
-                'example' => new \stdClass(),
+                'example' => [],
                 'array' => true
             ])
             ->addRule('documentsRead', [
-                'type' => Response::MODEL_METRIC_LIST,
+                'type' => Response::MODEL_METRIC,
                 'description' => 'Aggregated stats for documents read.',
                 'default' => [],
-                'example' => new \stdClass(),
+                'example' => [],
                 'array' => true
             ])
             ->addRule('documentsUpdate', [
-                'type' => Response::MODEL_METRIC_LIST,
+                'type' => Response::MODEL_METRIC,
                 'description' => 'Aggregated stats for documents updated.',
                 'default' => [],
-                'example' => new \stdClass(),
+                'example' => [],
                 'array' => true
             ])
             ->addRule('documentsDelete', [
-                'type' => Response::MODEL_METRIC_LIST,
+                'type' => Response::MODEL_METRIC,
                 'description' => 'Aggregated stats for documents deleted.',
                 'default' => [],
-                'example' => new \stdClass(),
+                'example' => [],
                 'array' => true
             ])
         ;
