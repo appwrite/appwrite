7433457feec7905bd28f82896f00d47d0fd663ea
Matej Bačo <matejbaco2000@gmail.com>
2022-10-21 08:40:59 +0000

    Mark execution processing in async=false

--- app/controllers/api/functions.php
+++ app/controllers/api/functions.php
@@ -1126,6 +1126,9 @@ App::post('/v1/functions/:functionId/executions')
                 ->dynamic($execution, Response::MODEL_EXECUTION);
         }
 
+        $execution->setAttribute('status', 'processing');
+        $execution = $dbForProject->updateDocument('executions', $executionId, $execution);
+
         $vars = array_reduce($function['vars'] ?? [], function (array $carry, Document $var) {
             $carry[$var->getAttribute('key')] = $var->getAttribute('value') ?? '';
             return $carry;
