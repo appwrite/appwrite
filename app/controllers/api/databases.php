bf064fa5c96f5d0dc260ee50ff9c32622696f47d
Jake Barnby <jakeb994@gmail.com>
2022-10-25 15:44:19 +1300

    Review revert unrelated change

--- app/controllers/api/databases.php
+++ app/controllers/api/databases.php
@@ -1752,14 +1752,15 @@ App::get('/v1/databases/:databaseId/collections/:collectionId/indexes/:key')
         $indexes = $collection->getAttribute('indexes');
 
         // Search for index
-        $indexIndex = array_search($key, array_map(fn($idx) => $idx['key'], $indexes));
+        $indexIndex = array_search($key, array_column($indexes, 'key'));
 
         if ($indexIndex === false) {
             throw new Exception(Exception::INDEX_NOT_FOUND);
         }
 
-        $index = $indexes[$indexIndex];
-        $index->setAttribute('collectionId', $database->getInternalId() . '_' . $collectionId);
+        $index = new Document([\array_merge($indexes[$indexIndex], [
+            'collectionId' => $database->getInternalId() . '_' . $collectionId,
+        ])]);
 
         $response->dynamic($index, Response::MODEL_INDEX);
     });
