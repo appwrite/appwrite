0049a7656537269c6c7e6c8a34b4960c856bb684
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-10-31 09:53:36 +0100

    feat: submodule

--- app/controllers/web/console.php
+++ app/controllers/web/console.php
@@ -3,50 +3,17 @@
 use Appwrite\Utopia\Response;
 use Utopia\App;
 
-$fallbackRoute = function (Response $response) {
-    $fallback = file_get_contents(__DIR__ . '/../../../console/index.html');
-    $response->html($fallback);
-};
-
 App::get('/')
+    ->alias('/console/*')
+    ->alias('/invite')
+    ->alias('/login')
+    ->alias('/recover')
+    ->alias('/register')
     ->groups(['web'])
     ->label('permission', 'public')
     ->label('scope', 'home')
     ->inject('response')
-    ->action($fallbackRoute);
-
-App::get('/console/*')
-    ->groups(['web'])
-    ->label('permission', 'public')
-    ->label('scope', 'home')
-    ->inject('response')
-    ->action($fallbackRoute);
-
-App::get('/invite')
-    ->groups(['web'])
-    ->label('permission', 'public')
-    ->label('scope', 'home')
-    ->inject('response')
-    ->action($fallbackRoute);
-
-App::get('/login')
-    ->groups(['web'])
-    ->label('permission', 'public')
-    ->label('scope', 'home')
-    ->inject('response')
-    ->action($fallbackRoute);
-
-App::get('/recover')
-    ->groups(['web'])
-    ->label('permission', 'public')
-    ->label('scope', 'home')
-    ->inject('response')
-    ->action($fallbackRoute);
-
-App::get('/register')
-    ->groups(['web'])
-    ->label('permission', 'public')
-    ->label('scope', 'home')
-    ->inject('response')
-    ->action($fallbackRoute);
-
+    ->action(function (Response $response) {
+        $fallback = file_get_contents(__DIR__ . '/../../../console/index.html');
+        $response->html($fallback);
+    });
