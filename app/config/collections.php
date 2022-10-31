7989518eecb033ad36b217c0668f62378bdc139e
Damodar Lohani <dlohani48@gmail.com>
2022-10-31 09:07:06 +0000

    buildTime attribute to deployment

--- app/config/collections.php
+++ app/config/collections.php
@@ -2328,6 +2328,17 @@ $collections = [
                 'filters' => [],
             ],
             [
+                '$id' => ID::custom('buildTime'),
+                'type' => Database::VAR_INTEGER,
+                'format' => '',
+                'size' => 0,
+                'signed' => true,
+                'required' => false,
+                'default' => null,
+                'array' => false,
+                'filters' => [],
+            ],
+            [
                 'array' => false,
                 '$id' => ID::custom('entrypoint'),
                 'type' => Database::VAR_STRING,
