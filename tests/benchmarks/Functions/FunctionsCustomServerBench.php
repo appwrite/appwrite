8448e928940687e40c56c61d8d5cffe8f0130661
Jake Barnby <jakeb994@gmail.com>
2022-11-04 20:32:47 +1300

    One rev/iter for deployment benchmark

--- tests/benchmarks/Functions/FunctionsCustomServerBench.php
+++ tests/benchmarks/Functions/FunctionsCustomServerBench.php
@@ -3,12 +3,16 @@
 namespace Tests\Benchmarks\Functions;
 
 use PhpBench\Attributes\BeforeMethods;
+use PhpBench\Attributes\Iterations;
+use PhpBench\Attributes\Revs;
 use Tests\E2E\Scopes\SideServer;
 
 class FunctionsCustomServerBench extends Base
 {
     use SideServer;
 
+    #[Revs(1)]
+    #[Iterations(1)]
     #[BeforeMethods(['createFunction', 'prepareDeployment'])]
     public function benchDeploymentCreate()
     {
