682790272fa75604d25a764256c2758caaa9da52
Christy Jacob <christyjacob4@gmail.com>
2022-11-07 18:10:56 +0530

    feat: update volume sync

--- app/tasks/volume-sync.php
+++ app/tasks/volume-sync.php
@@ -41,6 +41,5 @@ $cli
             Console::execute("rsync -av $source $destination", $stdin, $stdout, $stderr);
             Console::success($stdout);
             Console::error($stderr);
-
         }, $interval);
     });
