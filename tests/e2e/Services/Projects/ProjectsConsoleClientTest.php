60a63d9eb9ed7c5344c913f344401e77e6f5acc7
Bradley Schofield <bradley@appwrite.io>
2022-11-14 11:43:28 +0000

    Update ProjectsConsoleClientTest.php

--- tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
+++ tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
@@ -471,12 +471,11 @@ class ProjectsConsoleClientTest extends Scope
         $this->assertEquals(401, $response['headers']['status-code']);
 
         // Return project back to normal
-        $response = $this->client->call(Client::METHOD_PATCH, '/projects/' . $id, array_merge([
+        $response = $this->client->call(Client::METHOD_PATCH, '/projects/' . $id . '/auth/authDuration', array_merge([
             'content-type' => 'application/json',
             'x-appwrite-project' => $this->getProject()['$id'],
         ], $this->getHeaders()), [
-            'projectId' => ID::unique(),
-            'name' => 'Project Test 2'
+            'duration' => 525600,
         ]);
 
         $this->assertEquals(200, $response['headers']['status-code']);
