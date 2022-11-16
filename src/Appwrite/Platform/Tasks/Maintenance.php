6a79bea5df243b206fccf8bfe996c7d3818758e7
shimon <shimon@appwrite.io>
2022-11-16 16:56:47 +0200

    Deleted functions schedules clean up

--- src/Appwrite/Platform/Tasks/Maintenance.php
+++ src/Appwrite/Platform/Tasks/Maintenance.php
@@ -135,7 +135,7 @@ class Maintenance extends Action
         $usageStatsRetention30m = (int) App::getEnv('_APP_MAINTENANCE_RETENTION_USAGE_30M', '129600'); //36 hours
         $usageStatsRetention1d = (int) App::getEnv('_APP_MAINTENANCE_RETENTION_USAGE_1D', '8640000'); // 100 days
         $cacheRetention = (int) App::getEnv('_APP_MAINTENANCE_RETENTION_CACHE', '2592000'); // 30 days
-        $schedulesDeletionRetention = (int) App::getEnv('_APP_MAINTENANCE_RETENTION_SCHEDULES', '86400'); //Todo One day gap???
+        $schedulesDeletionRetention = (int) App::getEnv('_APP_MAINTENANCE_RETENTION_SCHEDULES', '360'); // 1 hour
 
         Console::loop(function () use ($interval, $executionLogsRetention, $abuseLogsRetention, $auditLogRetention, $usageStatsRetention30m, $usageStatsRetention1d, $cacheRetention, $schedulesDeletionRetention, $dbForConsole) {
             $time = DateTime::now();
