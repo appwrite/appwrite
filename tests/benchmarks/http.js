df203d438cbc1892fdc72d8efcd244fe2d99b67c
wess <wcope@me.com>
2022-11-17 12:38:54 -0500

    Fixes spacing and linter nag in tests/benchmarks/http.js

--- tests/benchmarks/http.js
+++ tests/benchmarks/http.js
@@ -10,29 +10,25 @@ export const requests = new Counter('http_reqs');
 
 export const options = {
     stages: [
-      { target: 150, duration: '1m' },
-      { target: 150, duration: '1m' },
-      { target: 0, duration: '1m' },
-    ]
-    // thresholds: {
-    //     requests: ['count < 100'],
-    // },
+    { target: 50, duration: '1m' },
+    // { target: 15, duration: '1m' },
+    // { target: 0, duration: '1m' },
+    ],
+    thresholds: {
+        requests: ['count < 100'],
+    },
 };
-
-// testing (63766a35b47c0dae6b5a) is any/all
 
 export default function () {
     const config = {
         headers: {
-            // 'X-Appwrite-Key': '62115c397d1f1644730ff5df69f7fec6daa898c11e41f314fd6e210cac536a2fbcefa96ca9e5c7b4e2fcb7a535f1e8fc16c27829c1ccab3d6a1726e31d561cdaf8dc64c63e0e9ad0f406a8f9703edfbbf0f9ac6a9271d573b2ea08805220dbede08db1c8e6cf792582ef8452b82bcd96e9890fb79109d05b6fda1b1673d35292',
-            'X-Appwrite-Project': '637666b7f1733a4b00a8'
-        }
-      }
+            'X-Appwrite-Key': '24356eb021863f81eb7dd77c7750304d0464e141cad6e9a8befa1f7d2b066fde190df3dab1e8d2639dbb82ee848da30501424923f4cd80d887ee40ad77ded62763ee489448523f6e39667f290f9a54b2ab8fad131a0bc985e6c0f760015f7f3411e40626c75646bb19d2bb2f7bf2f63130918220a206758cbc48845fd725a695',
+            'X-Appwrite-Project': '60479fe35d95d'
+        }}
 
-    const resDb = http.get('http://localhost:9501/v1/databases/benchmarking/collections/63766a35b47c0dae6b5a/documents', config);
+    const resDb = http.get('http://localhost:9501/', config);
 
     check(resDb, {
         'status is 200': (r) => r.status === 200,
-        'total': (r) => JSON.parse(r.body).documents.length === 15
     });
 }
