17b7af9199f84363e8631f2969577ba0bb19aff5
Christy Jacob <christyjacob4@gmail.com>
2022-11-23 02:44:38 +0530

    fix: add headers to console

--- app/controllers/web/console.php
+++ app/controllers/web/console.php
@@ -1,7 +1,25 @@
 <?php
 
+use Appwrite\Utopia\Request;
 use Appwrite\Utopia\Response;
 use Utopia\App;
+
+App::init()
+    ->groups(['web'])
+    ->inject('request')
+    ->inject('response')
+    ->action(function (Request $request, Response $response) {
+        $time = (60 * 60 * 24 * 45); // 45 days cache
+
+        $response
+            ->addHeader('Cache-Control', 'public, max-age=' . $time)
+            ->addHeader('Expires', \date('D, d M Y H:i:s', \time() + $time) . ' GMT') // 45 days cache
+            ->addHeader('X-Frame-Options', 'SAMEORIGIN') // Avoid console and homepage from showing in iframes
+            ->addHeader('X-XSS-Protection', '1; mode=block; report=/v1/xss?url=' . \urlencode($request->getURI()))
+            ->addHeader('X-UA-Compatible', 'IE=Edge') // Deny IE browsers from going into quirks mode
+        ;
+
+    });
 
 App::get('/console')
     ->alias('/')
