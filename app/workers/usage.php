9c19544c61de01fa219a7a9576c58595b7c54934
shimon <shimon@appwrite.io>
2022-12-05 12:43:31 +0200

    updates

--- app/workers/usage.php
+++ app/workers/usage.php
@@ -24,6 +24,9 @@ $server
     ->workerStart()
     ->action(function () use (&$stack) {
         Timer::tick(30000, function () use (&$stack) {
+            $hourly = date('d-m-Y H:00', time());
+            $daily  = date('d-m-Y 00:00', time());
+
             $chunk = array_slice($stack, 0, count($stack));
             array_splice($stack, 0, count($stack));
             var_dump($chunk);
