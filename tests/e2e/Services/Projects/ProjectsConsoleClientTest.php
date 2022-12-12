ca39ed37d87d851e25d379e8ae866a4aed94f988
Damodar Lohani <dlohani48@gmail.com>
2022-12-12 05:02:48 +0000

    reset limit on test

--- tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
+++ tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
@@ -979,6 +979,16 @@ class ProjectsConsoleClientTest extends Scope
         $this->assertEquals(1, count($sessions));
         $this->assertEquals($sessionId2, $sessions[0]['$id']);
 
+        /**
+         * Reset Limit
+         */
+        $response = $this->client->call(Client::METHOD_PATCH, '/projects/' . $id . '/auth/max-sessions', array_merge([
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+        ], $this->getHeaders()), [
+            'limit' => 10,
+        ]);
+
         return $data;
     }
 
