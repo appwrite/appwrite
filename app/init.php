a8595eaca2c205cd5aa237d3ce3c297156a83a61
Bradley Schofield <bradley@appwrite.io>
2022-12-17 20:45:58 +0000

    Port Messages worker to Utopia queue system

--- app/init.php
+++ app/init.php
@@ -853,7 +853,10 @@ App::setResource('audits', fn() => new Audit());
 App::setResource('mails', fn() => new Mail());
 App::setResource('deletes', fn() => new Delete());
 App::setResource('database', fn() => new EventDatabase());
-App::setResource('messaging', fn() => new Phone());
+App::setResource('messaging', function (Connection $queue) {
+    return new Phone($queue);
+}, ['queue']);
+
 App::setResource('queue', function (Group $pools) {
     return $pools->get('queue')->pop()->getResource();
 }, ['pools']);
