15f52b933b060a6f78de55a7038c61ec121fb385
Damodar Lohani <dlohani48@gmail.com>
2022-12-18 08:34:57 +0000

    fix excaping

--- app/controllers/api/projects.php
+++ app/controllers/api/projects.php
@@ -587,7 +587,7 @@ App::patch('/v1/projects/:projectId/auth/password-history')
     ->label('sdk.response.type', Response::CONTENT_TYPE_JSON)
     ->label('sdk.response.model', Response::MODEL_PROJECT)
     ->param('projectId', '', new UID(), 'Project unique ID.')
-    ->param('limit', false, new Range(0, APP_LIMIT_USER_SESSIONS_MAX), 'Set the max number of password to store in user history. User can't choose a new password that is already stored in the password history list.  Max number of password allowed in history is' . APP_LIMIT_USER_PASSWORD_HISTORY . '. Default value is 0')
+    ->param('limit', false, new Range(0, APP_LIMIT_USER_SESSIONS_MAX), 'Set the max number of password to store in user history. User can\'t choose a new password that is already stored in the password history list.  Max number of password allowed in history is' . APP_LIMIT_USER_PASSWORD_HISTORY . '. Default value is 0')
     ->inject('response')
     ->inject('dbForConsole')
     ->action(function (string $projectId, int $limit, Response $response, Database $dbForConsole) {
