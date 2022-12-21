772481451c49365399620c939f01e4a257e78989
Damodar Lohani <dlohani48@gmail.com>
2022-12-21 06:42:27 +0000

    test to set password history limit above 20

--- tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
+++ tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
@@ -1001,6 +1001,19 @@ class ProjectsConsoleClientTest extends Scope
         $id = $data['projectId'] ?? '';
 
         /**
+         * Test for Failure
+         */
+        $response = $this->client->call(Client::METHOD_PATCH, '/projects/' . $id . '/auth/password-history', array_merge([
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+        ], $this->getHeaders()), [
+            'limit' => 25,
+        ]);
+
+        $this->assertEquals(400, $response['headers']['status-code']);
+
+
+        /**
          * Test for Success
          */
         $response = $this->client->call(Client::METHOD_PATCH, '/projects/' . $id . '/auth/password-history', array_merge([
