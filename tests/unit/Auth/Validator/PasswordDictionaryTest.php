96cf2e6330250c50ceabbac1f4da9e5e33c32a9b
Damodar Lohani <dlohani48@gmail.com>
2022-12-26 10:27:04 +0000

    password dictionary test

---
+++ tests/unit/Auth/Validator/PasswordDictionaryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit\Auth\Validator;
+
+use Appwrite\Auth\Validator\PasswordDictionary;
+use PHPUnit\Framework\TestCase;
+use Utopia\Database\Document;
+
+class PasswordDictionaryTest extends TestCase
+{
+    protected ?PasswordDictionary $object = null;
+
+    public function setUp(): void
+    {
+        $this->object = new PasswordDictionary(
+            ['password' => true, '123456' => true],
+            new Document([
+                'auths' => [
+                    'passwordDictionary' => true
+                ]
+                ])
+        );
+    }
+
+    public function testValues(): void
+    {
+        $this->assertEquals($this->object->isValid('1'), false); // to check parent is being called
+        $this->assertEquals($this->object->isValid('123456'), false);
+        $this->assertEquals($this->object->isValid('password'), false);
+        $this->assertEquals($this->object->isValid('myPasswordIsRight'), true);
+    }
+}
