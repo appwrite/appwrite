7910149053ac878412e7c55dbb6074988844c877
Bradley Schofield <bradley@appwrite.io>
2023-01-13 15:42:29 +0000

    Remove tests and update other references to old validators

--- src/Appwrite/GraphQL/Types/Mapper.php
+++ src/Appwrite/GraphQL/Types/Mapper.php
@@ -232,14 +232,14 @@ class Mapper
             case 'Appwrite\Network\Validator\Email':
             case 'Appwrite\Event\Validator\Event':
             case 'Utopia\Validator\HexColor':
-            case 'Appwrite\Network\Validator\Host':
-            case 'Appwrite\Network\Validator\IP':
+            case 'Utopia\Validator\Host':
+            case 'Utopia\Validator\IP':
             case 'Utopia\Database\Validator\Key':
-            case 'Appwrite\Network\Validator\Origin':
+            case 'Utopia\Validator\Origin':
             case 'Appwrite\Auth\Validator\Password':
             case 'Utopia\Validator\Text':
             case 'Utopia\Database\Validator\UID':
-            case 'Appwrite\Network\Validator\URL':
+            case 'Utopia\Validator\URL':
             case 'Utopia\Validator\WhiteList':
             default:
                 $type = Type::string();
