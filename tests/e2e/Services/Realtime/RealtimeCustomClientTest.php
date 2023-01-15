5ab955c2ba3c348d21228c631b56829634f74ab2
shimon <shimon@appwrite.io>
2023-01-15 14:02:53 +0200

    usage test

--- tests/e2e/Services/Realtime/RealtimeCustomClientTest.php
+++ tests/e2e/Services/Realtime/RealtimeCustomClientTest.php
@@ -1248,7 +1248,7 @@ class RealtimeCustomClientTest extends Scope
         $this->assertNotEmpty($deployment['body']['$id']);
 
         // Wait for deployment to be built.
-        sleep(5);
+        sleep(10);
 
         $response = $this->client->call(Client::METHOD_PATCH, '/functions/' . $functionId . '/deployments/' . $deploymentId, array_merge([
             'content-type' => 'application/json',
