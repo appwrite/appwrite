7f3a2dcce72df7bd76cb9c4c03e18fd71c74ac8a
Damodar Lohani <dlohani48@gmail.com>
2023-01-16 09:46:53 +0000

    fix model

--- src/Appwrite/Utopia/Response/Model/Project.php
+++ src/Appwrite/Utopia/Response/Model/Project.php
@@ -245,7 +245,7 @@ class Project extends Model
 
         $document->setAttribute('authLimit', $authValues['limit'] ?? 0);
         $document->setAttribute('authDuration', $authValues['duration'] ?? Auth::TOKEN_EXPIRATION_LOGIN_LONG);
-        $document->setAttribute('authSessionLimit', $authValues['maxSessions'] ?? APP_LIMIT_USER_SESSIONS_DEFAULT);
+        $document->setAttribute('authSessionsLimit', $authValues['maxSessions'] ?? APP_LIMIT_USER_SESSIONS_DEFAULT);
         $document->setAttribute('authPasswordHistory', $authValues['passwordHistory'] ?? 0);
 
         foreach ($auth as $index => $method) {
