0253b308009e00a8ca5d29ecd52520a0aaf64585
Damodar Lohani <lohanidamodar@users.noreply.github.com>
2023-01-20 12:24:22 +0545

    fix executor vendor require

--- app/executor.php
+++ app/executor.php
@@ -1,6 +1,7 @@
 <?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
+if (\file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
 
 use Appwrite\Runtimes\Runtimes;
 use Swoole\ConnectionPool;
