c66aa38fe68d24ccec03bd4b073afe2d5ca1e69c
Vedant Borkar <vedantborkar1234@gmail.com>
2023-02-17 01:07:36 +0530

    Update errors.php

--- app/config/errors.php
+++ app/config/errors.php
@@ -531,7 +531,7 @@ return [
     ],
     Exception::DOMAIN_ALREADY_EXISTS => [
         'name' => Exception::DOMAIN_ALREADY_EXISTS,
-        'description' => 'A Domain with the requested ID already exists.',
+        'description' => 'A Domain with same name already exists.',
         'code' => 409,
     ],
     Exception::VARIABLE_NOT_FOUND => [
