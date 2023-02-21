0b07fb2060d7c4773f28b3f34268f019c8b8f34b
Damodar Lohani <dlohani48@gmail.com>
2023-02-21 05:18:41 +0000

    fix error

--- tests/e2e/Services/Teams/TeamsBaseClient.php
+++ tests/e2e/Services/Teams/TeamsBaseClient.php
@@ -272,7 +272,7 @@ trait TeamsBaseClient
         $this->assertNotEmpty($response['body']['teamId']);
         $this->assertNotEmpty($response['body']['teamName']);
         $this->assertCount(2, $response['body']['roles']);
-        $this->assertEquals(false, DateTime::isValid($response['body']['joined'])); // is null in DB
+        $this->assertEquals(false, (new DateTimeValidator())->isValid($response['body']['joined'])); // is null in DB
         $this->assertEquals(false, $response['body']['confirm']);
 
         $lastEmail = $this->getLastEmail();
