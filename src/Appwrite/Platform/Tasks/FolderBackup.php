d9b55b4b04d5d20a94f3ca3feffb94b4b8b29d8f
Shimon Newman <shimonewman@gmail.com>
2023-02-27 08:36:00 +0200

    Update src/Appwrite/Platform/Tasks/FolderBackup.php
    Co-authored-by: Eldad A. Fux <eldad.fux@gmail.com>

--- src/Appwrite/Platform/Tasks/FolderBackup.php
+++ src/Appwrite/Platform/Tasks/FolderBackup.php
@@ -43,7 +43,7 @@ class FolderBackup extends Action
                 $sleep  = $next->getTimestamp() - $now->getTimestamp();
             }
 
-            console::log('[' . $now->format("Y-m-d H:i:s.v") . '] Sleeping for ' . $sleep . ' seconds next run will be at [' . $next->format("Y-m-d H:i:s.v") . ']');
+            Console::log('[' . $now->format("Y-m-d H:i:s.v") . '] Sleeping for ' . $sleep . ' seconds next run will be at [' . $next->format("Y-m-d H:i:s.v") . ']');
 
             sleep($sleep);
 
