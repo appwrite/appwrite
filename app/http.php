28af0669f60cfd83e776203463c040d464fcd429
fogelito <fogelshmuel@gmail.com>
2023-03-16 11:11:04 +0200

    docker-compose.yml temp mount

--- app/http.php
+++ app/http.php
@@ -296,7 +296,7 @@ $http->on('request', function (SwooleRequest $swooleRequest, SwooleResponse $swo
             $log->addExtra('line', $th->getLine());
             $log->addExtra('trace', $th->getTraceAsString());
             $log->addExtra('detailedTrace', $th->getTrace());
-            $log->addExtra('roles', Authorization::$roles);
+            $log->addExtra('roles', Authorization::getRoles());
 
             $action = $route->getLabel("sdk.namespace", "UNKNOWN_NAMESPACE") . '.' . $route->getLabel("sdk.method", "UNKNOWN_METHOD");
             $log->setAction($action);
