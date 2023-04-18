118ddb910162899ff1f00ac6e1da97602232bd9b
Damodar Lohani <dlohani48@gmail.com>
2023-04-18 07:14:03 +0000

    format fix

--- app/init.php
+++ app/init.php
@@ -666,12 +666,12 @@ $locales = Config::getParam('locale-codes', []);
 
 foreach ($locales as $locale) {
     $code = $locale['code'];
-    
+
     $path = __DIR__ . '/config/locale/translations/' . $code . '.json';
 
     if (!\file_exists($path)) {
         $path = __DIR__ . '/config/locale/translations/' . \substr($code, 0, 2) . '.json'; // if `ar-ae` doesn't exist, look for `ar`
-        if(!\file_exists($path)) {
+        if (!\file_exists($path)) {
             $path = __DIR__ . '/config/locale/translations/en.json'; // if none translation exists, use default from `en.json`
         }
     }
