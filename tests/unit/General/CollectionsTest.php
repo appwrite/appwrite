361906f42bcec4ef8b9a793f99130d50a0d802d1
fogelito <fogelshmuel@gmail.com>
2023-05-18 11:05:10 +0300

    testValidateCollectionIndexes

--- tests/unit/General/CollectionsTest.php
+++ tests/unit/General/CollectionsTest.php
@@ -3,9 +3,13 @@
 namespace Tests\Unit\General;
 
 use PHPUnit\Framework\TestCase;
+use Utopia\Database\Document;
+use Utopia\Database\Helpers\ID;
+use Utopia\Database\Validator\Index;
 
 class CollectionsTest extends TestCase
 {
+    /** @var array<mixed> $collections */
     protected array $collections;
 
     public function setUp(): void
@@ -27,6 +31,25 @@ class CollectionsTest extends TestCase
                     $this->assertEquals(1, $occurrences);
                 }
             }
+        }
+    }
+
+    public function testValidateCollectionIndexes(): void
+    {
+        $validator = new Index();
+
+        foreach ($this->collections as $collection) {
+            $document = new Document();
+
+            foreach ($collection['attributes'] as $attribute) {
+                $document->setAttribute('attributes', new Document($attribute), Document::SET_TYPE_APPEND);
+            }
+
+            foreach ($collection['indexes'] as $index) {
+                $document->setAttribute('indexes', new Document($index), Document::SET_TYPE_APPEND);
+            }
+
+            $this->assertTrue($validator->isValid($document));
         }
     }
 }
