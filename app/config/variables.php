3172682b63ca92a645ba8b7444e8205b4d78308f
Neutron <120790871+Shiva953@users.noreply.github.com>
2023-05-24 09:34:40 +0530

    Update app/config/variables.php
    Co-authored-by: Vincent (Wen Yu) Ge <wenyu1999@hotmail.com>

--- app/config/variables.php
+++ app/config/variables.php
@@ -755,7 +755,7 @@ return [
             ],
             [
                 'name' => '_APP_FUNCTIONS_INACTIVE_THRESHOLD',
-                'description' => 'The minimum time a function must be inactive before it can be shut down and cleaned up. This feature is intended to clean up unused containers. Containers may remain active for longer than the interval before being shut down, as Appwrite only cleans up unused containers every hour. If no value is provided, the default is 60 seconds. Note: The minimum inactive time is constrained by the MAINTENANCE_INTERVAL. If the maintenance interval is set to a value greater than the inactive threshold, functions may remain active for longer than the specified interval before being shut down.',
+                'description' => 'The minimum time a function must be inactive before it can be shut down and cleaned up. This feature is intended to clean up unused containers. Containers may remain active for longer than the interval before being shut down, as Appwrite only cleans up unused containers every hour. If no value is provided, the default is 60 seconds. The minimum inactive time is constrained by the `MAINTENANCE_INTERVAL`. If the maintenance interval is set to a value greater than the inactive threshold, functions may remain active for longer than the specified interval before being shut down.',
                 'introduction' => '0.13.0',
                 'default' => '60',
                 'required' => false,
