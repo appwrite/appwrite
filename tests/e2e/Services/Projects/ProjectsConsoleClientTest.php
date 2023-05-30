0661f1f8899d8dfed18c832d3332959b175b4f6e
Jake Barnby <jakeb994@gmail.com>
2023-05-30 18:26:17 +1200

    Fix tests

--- tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
+++ tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
@@ -814,11 +814,11 @@ class ProjectsConsoleClientTest extends Scope
 
         $this->assertEquals($response['headers']['status-code'], 501);
 
-        $response = $this->client->call(Client::METHOD_POST, '/account/anonymous', array_merge([
+        $response = $this->client->call(Client::METHOD_POST, '/account/sessions/anonymous', array_merge([
             'origin' => 'http://localhost',
             'content-type' => 'application/json',
             'x-appwrite-project' => $id,
-        ]), []);
+        ]));
 
         $this->assertEquals($response['headers']['status-code'], 501);
 
@@ -874,6 +874,19 @@ class ProjectsConsoleClientTest extends Scope
             'name' => $name,
         ]);
 
+        $email = uniqid() . 'user@localhost.test';
+
+        $response = $this->client->call(Client::METHOD_POST, '/account', array_merge([
+            'origin' => 'http://localhost',
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $id,
+        ]), [
+            'userId' => ID::unique(),
+            'email' => $email,
+            'password' => $password,
+            'name' => $name,
+        ]);
+
         $this->assertEquals($response['headers']['status-code'], 501);
 
         /**
@@ -888,6 +901,8 @@ class ProjectsConsoleClientTest extends Scope
 
         $this->assertEquals(200, $response['headers']['status-code']);
         $this->assertNotEmpty($response['body']['$id']);
+
+        $email = uniqid() . 'user@localhost.test';
 
         $response = $this->client->call(Client::METHOD_POST, '/account', array_merge([
             'origin' => 'http://localhost',
