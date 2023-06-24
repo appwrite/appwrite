fecde6bdba0af4f9e85b1b65b1490ba827610db9
vaibhavagarwal220 <vaibhavagarwal220@gmail.com>
2023-06-24 13:57:02 +0530

    fixed null $name error for anonymous user

--- app/controllers/api/account.php
+++ app/controllers/api/account.php
@@ -2306,7 +2306,7 @@ App::post('/v1/account/verification')
             ->setBody($body)
             ->setFrom($from)
             ->setRecipient($user->getAttribute('email'))
-            ->setName($user->getAttribute('name'))
+            ->setName($user->getAttribute('name') ?? '')
             ->trigger()
         ;
 
