c7492ee35ed1da7cb7d4aded63f95877c0428541
Steven <1477010+stnguyen90@users.noreply.github.com>
2023-07-19 01:32:11 +0000

    Fix a bug where the delete worker wasn't iterating over schedules

--- app/workers/deletes.php
+++ app/workers/deletes.php
@@ -679,15 +679,25 @@ class DeletesV1 extends Worker
         $limit = 50;
         $results = [];
         $sum = $limit;
+        $cursor = null;
 
         $executionStart = \microtime(true);
 
         while ($sum === $limit) {
             $chunk++;
 
-            $results = $database->find($collection, \array_merge([Query::limit($limit)], $queries));
+            $mergedQueries = \array_merge([Query::limit($limit)], $queries);
+            if ($cursor instanceof Document) {
+                $mergedQueries[] = Query::cursorAfter($cursor);
+            }
+
+            $results = $database->find($collection, $mergedQueries);
 
             $sum = count($results);
+
+            if ($sum > 0) {
+                $cursor = $results[$sum - 1];
+            }
 
             foreach ($results as $document) {
                 if (is_callable($callback)) {
