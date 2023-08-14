cb6760a957f94ff9780412b3543ce30ab78c8c12
prateek banga <prateekbanga12@gmail.com>
2023-08-14 22:02:54 +0530

    removes unnecessary use statement

--- tests/e2e/Services/Databases/DatabasesCustomClientTest.php
+++ tests/e2e/Services/Databases/DatabasesCustomClientTest.php
@@ -6,7 +6,6 @@ use Tests\E2E\Client;
 use Tests\E2E\Scopes\Scope;
 use Tests\E2E\Scopes\ProjectCustom;
 use Tests\E2E\Scopes\SideClient;
-use Tests\E2E\Services\Storage\StoragePermissionsScope;
 use Utopia\Database\Helpers\ID;
 use Utopia\Database\Helpers\Permission;
 use Utopia\Database\Helpers\Role;
