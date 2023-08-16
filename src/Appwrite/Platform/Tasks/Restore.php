9ebbc70b7db175c5f3238d62755c973c165d6068
fogelito <fogelshmuel@gmail.com>
2023-08-16 12:26:50 +0300

    composer.lock

--- src/Appwrite/Platform/Tasks/Restore.php
+++ src/Appwrite/Platform/Tasks/Restore.php
@@ -25,7 +25,7 @@ class Restore extends Action
     {
         $this
             ->desc('Restore a DB')
-            ->param('id', '', new Text(20), 'The backup identification')
+            ->param('id', '', new Text(19), 'The backup identification')
             ->param('cloud', null, new Boolean(true), 'Download backup from cloud or use local directory')
             ->param('database', null, new Text(10), 'The Database name for example db_fra1_01')
             ->callback(fn ($id, $cloud, $project) => $this->action($id, $cloud, $project));
