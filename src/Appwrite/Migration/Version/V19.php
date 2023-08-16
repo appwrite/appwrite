ce64fe5674ffcf63a8860ab38f3b5baa445a7585
Steven Nguyen <stnguyen90@users.noreply.github.com>
2023-08-15 17:36:10 -0700

    Update 1.4 migration

--- src/Appwrite/Migration/Version/V19.php
+++ src/Appwrite/Migration/Version/V19.php
@@ -2,8 +2,6 @@
 
 namespace Appwrite\Migration\Version;
 
-use Appwrite\Auth\Auth;
-use Utopia\Config\Config;
 use Appwrite\Migration\Migration;
 use Utopia\CLI\Console;
 use Utopia\Database\Database;
@@ -84,16 +82,207 @@ class V19 extends Migration
             $this->projectDB->setNamespace("_{$this->project->getInternalId()}");
 
             switch ($id) {
+                case '_metadata':
+                    if ($this->project->getInternalId() === 'console') {
+                        $this->createCollection('schedules');
+                    }
+
+                    $this->createCollection('statsLogger');
+                    break;
+                case 'attributes':
+                case 'indexes':
+                    try {
+                        $this->projectDB->updateAttribute($id, 'databaseInternalId', required: true);
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'databaseInternalId' from {$id}: {$th->getMessage()}");
+                    }
+
+                    try {
+                        $this->projectDB->updateAttribute($id, 'collectionInternalId', required: true);
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'collectionInternalId' from {$id}: {$th->getMessage()}");
+                    }
+                    try {
+                        $this->createAttributeFromCollection($this->projectDB, $id, 'error');
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'error' from {$id}: {$th->getMessage()}");
+                    }
+                    break;
+                case 'builds':
+                    try {
+                        $this->createAttributeFromCollection($this->projectDB, $id, 'deploymentInternalId');
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'deploymentInternalId' from {$id}: {$th->getMessage()}");
+                    }
+                    break;
+                case 'collections':
+                    try {
+                        $this->createAttributeFromCollection($this->projectDB, $id, 'enabled');
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'enabled' from {$id}: {$th->getMessage()}");
+                    }
+                    break;
+                case 'databases':
+                    try {
+                        $this->createAttributeFromCollection($this->projectDB, $id, 'enabled');
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'enabled' from {$id}: {$th->getMessage()}");
+                    }
+                    break;
+                case 'deployments':
+                    try {
+                        $this->createAttributeFromCollection($this->projectDB, $id, 'resourceInternalId');
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'resourceInternalId' from {$id}: {$th->getMessage()}");
+                    }
+
+                    try {
+                        $this->createAttributeFromCollection($this->projectDB, $id, 'buildInternalId');
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'buildInternalId' from {$id}: {$th->getMessage()}");
+                    }
+                    break;
+                case 'executions':
+                    try {
+                        $this->createAttributeFromCollection($this->projectDB, $id, 'functionInternalId');
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'functionInternalId' from {$id}: {$th->getMessage()}");
+                    }
+
+                    try {
+                        $this->createAttributeFromCollection($this->projectDB, $id, 'deploymentInternalId');
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'deploymentInternalId' from {$id}: {$th->getMessage()}");
+                    }
+                    break;
+                case 'functions':
+                    try {
+                        $this->createAttributeFromCollection($this->projectDB, $id, 'deploymentInternalId');
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'deploymentInternalId' from {$id}: {$th->getMessage()}");
+                    }
+                    break;
+                case 'memberships':
+                    try {
+                        $this->projectDB->updateAttribute($id, 'teamInternalId', required: true);
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'teamInternalId' from {$id}: {$th->getMessage()}");
+                    }
+                    // Intentional fall through to update memberships.userInternalId
+                case 'sessions':
+                case 'tokens':
+                    try {
+                        $this->projectDB->updateAttribute($id, 'userInternalId', required: true);
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'userInternalId' from {$id}: {$th->getMessage()}");
+                    }
+                    break;
+                case 'domains':
+                case 'keys':
+                case 'platforms':
+                case 'webhooks':
+                    try {
+                        $this->projectDB->updateAttribute($id, 'projectInternalId', required: true);
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'projectInternalId' from {$id}: {$th->getMessage()}");
+                    }
+                    break;
                 case 'projects':
                     try {
-                        /**
-                         * Create 'passwordHistory' attribute
-                         */
                         $this->createAttributeFromCollection($this->projectDB, $id, 'smtp');
                         $this->createAttributeFromCollection($this->projectDB, $id, 'templates');
                         $this->projectDB->deleteCachedCollection($id);
                     } catch (\Throwable $th) {
                         Console::warning("'SMTP and Templates' from {$id}: {$th->getMessage()}");
+                    }
+                    break;
+                case 'schedules':
+                    try {
+                        $this->createAttributeFromCollection($this->projectDB, $id, 'resourceInternalId');
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'resourceInternalId' from {$id}: {$th->getMessage()}");
+                    }
+                    break;
+                case 'stats':
+                    try {
+                        $this->projectDB->updateAttribute($id, 'value', signed: true);
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'teamInternalId' from {$id}: {$th->getMessage()}");
+                    }
+
+                    try {
+                        $this->projectDB->deleteAttribute($id, 'type');
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'teamInternalId' from {$id}: {$th->getMessage()}");
+                    }
+
+                    try {
+                        $this->projectDB->deleteIndex($id, '_key_metric_period_time');
+                        $this->createIndexFromCollection($this->projectDB, $id, '_key_metric_period_time');
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'teamInternalId' from {$id}: {$th->getMessage()}");
+                    }
+                    break;
+                case 'teams':
+                    try {
+                        $this->projectDB->updateAttribute($id, 'teamInternalId', required: true);
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'teamInternalId' from {$id}: {$th->getMessage()}");
+                    }
+                    break;
+                case 'users':
+                    try {
+                        $this->createAttributeFromCollection($this->projectDB, $id, 'labels');
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'labels' from {$id}: {$th->getMessage()}");
+                    }
+
+                    try {
+                        $this->createAttributeFromCollection($this->projectDB, $id, 'accessedAt');
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'accessedAt' from {$id}: {$th->getMessage()}");
+                    }
+
+                    try {
+                        $this->projectDB->updateAttribute($id, 'search', filters: ['userSearch']);
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'search' from {$id}: {$th->getMessage()}");
+                    }
+
+                    try {
+                        $this->createIndexFromCollection($this->projectDB, $id, '_key_accessedAt');
+                    } catch (\Throwable $th) {
+                        Console::warning("'_key_accessedAt' from {$id}: {$th->getMessage()}");
+                    }
+                    break;
+                case 'variables':
+                    try {
+                        $this->projectDB->updateAttribute($id, 'functionInternalId', required: true);
+                        $this->projectDB->deleteCachedCollection($id);
+                    } catch (\Throwable $th) {
+                        Console::warning("'search' from {$id}: {$th->getMessage()}");
                     }
                     break;
                 default:
@@ -117,6 +306,45 @@ class V19 extends Migration
     protected function fixDocument(Document $document): Document
     {
         switch ($document->getCollection()) {
+            case '_metadata':
+                // TODO: function schedules
+
+                // TODO: migrate statsLogger?
+                break;
+            case 'builds':
+                $deploymentId = $document->getAttribute('deploymentId');
+                $deployment = $this->projectDB->getDocument('deployments', $deploymentId);
+                $document->setAttribute('deploymentInternalId', $deployment->getInternalId());
+                break;
+            case 'collections':
+            case 'databases':
+                $document->setAttribute('enabled', true);
+                break;
+            case 'deployments':
+                $resourceId = $document->getAttribute('resourceId');
+                $function = $this->projectDB->getDocument('functions', $resourceId);
+                $document->setAttribute('resourceInternalId', $function->getInternalId());
+
+                $buildId = $document->getAttribute('buildId');
+                $build = $this->projectDB->getDocument('builds', $buildId);
+                $document->setAttribute('buildInternalId', $build->getInternalId());
+                break;
+            case 'executions':
+                $functionId = $document->getAttribute('functionId');
+                $function = $this->projectDB->getDocument('functions', $functionId);
+                $document->setAttribute('functionInternalId', $function->getInternalId());
+
+                $deploymentId = $document->getAttribute('deploymentId');
+                $deployment = $this->projectDB->getDocument('deployments', $deploymentId);
+                $document->setAttribute('deploymentInternalId', $deployment->getInternalId());
+                break;
+            case 'functions':
+                $deploymentId = $document->getAttribute('deploymentId');
+                $deployment = $this->projectDB->getDocument('deployments', $deploymentId);
+                $document->setAttribute('deploymentInternalId', $deployment->getInternalId());
+
+                // TODO: function schedule?
+                break;
             case 'projects':
                 /**
                  * Bump version number.
@@ -126,6 +354,8 @@ class V19 extends Migration
                 $document->setAttribute('smtp', []);
                 $document->setAttribute('templates', []);
 
+                break;
+            default:
                 break;
         }
 
@@ -173,6 +403,13 @@ class V19 extends Migration
             Console::log("Migrating Bucket {$id} {$bucket->getId()} ({$bucket->getAttribute('name')})");
             $this->alterPermissionIndex($id);
             $this->alterUidType($id);
+
+            try {
+                $this->createAttributeFromCollection($this->projectDB, $id, 'bucketInternalId');
+                $this->projectDB->deleteCachedCollection($id);
+            } catch (\Throwable $th) {
+                Console::warning("'bucketInternalId' from {$id}: {$th->getMessage()}");
+            }
         }
     }
 }
