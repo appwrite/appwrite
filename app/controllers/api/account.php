55deaec8b22c931fe8d86a79b3c9823bd3fd3f10
Jake Barnby <jakeb994@gmail.com>
2023-08-23 17:53:24 -0400

    Fix missing env

--- app/controllers/api/account.php
+++ app/controllers/api/account.php
@@ -85,13 +85,16 @@ App::post('/v1/account/invite')
 
         $email = \strtolower($email);
 
-        $whitelistCodes = (!empty(App::getEnv('_APP_CONSOLE_WHITELIST_CODES', null))) ? \explode(',', App::getEnv('_APP_CONSOLE_WHITELIST_CODES', null)) : [];
+        $codes = App::getEnv('_APP_CONSOLE_WHITELIST_CODES');
+        $whitelistCodes = !empty($codes)
+            ? \explode(',', $codes)
+            : [];
 
         if (empty($whitelistCodes)) {
             throw new Exception(Exception::GENERAL_CODES_DISABLED);
         }
 
-        if (!empty($whitelistCodes) && !\in_array($code, $whitelistCodes)) {
+        if (!\in_array($code, $whitelistCodes)) {
             throw new Exception(Exception::USER_INVALID_CODE);
         }
 
