ece93a14e57dc9d3581de616844e6e82236cf7f8
Jake Barnby <jakeb994@gmail.com>
2023-08-28 18:09:37 -0400

    Fix install task

--- src/Appwrite/Platform/Tasks/Install.php
+++ src/Appwrite/Platform/Tasks/Install.php
@@ -6,7 +6,9 @@ use Appwrite\Auth\Auth;
 use Appwrite\Docker\Compose;
 use Appwrite\Docker\Env;
 use Appwrite\Utopia\View;
-use Utopia\Analytics\GoogleAnalytics;
+use Utopia\Analytics\Adapter;
+use Utopia\Analytics\Adapter\GoogleAnalytics;
+use Utopia\Analytics\Event;
 use Utopia\CLI\Console;
 use Utopia\Config\Config;
 use Utopia\Validator\Text;
@@ -33,22 +35,6 @@ class Install extends Action
 
     public function action(string $httpPort, string $httpsPort, string $organization, string $image, string $interactive): void
     {
-        /**
-         * 1. Start - DONE
-         * 2. Check for older setup and get older version - DONE
-         *  2.1 If older version is equal or bigger(?) than current version, **stop setup**
-         *  2.2. Get ENV vars - DONE
-         *   2.2.1 Fetch from older docker-compose.yml file
-         *   2.2.2 Fetch from older .env file (manually parse)
-         *  2.3 Use old ENV vars as default values
-         *  2.4 Ask for all required vars not given as CLI args and if in interactive mode
-         *      Otherwise, just use default vars. - DONE
-         * 3. Ask user to backup important volumes, env vars, and SQL tables
-         *      In th future we can try and automate this for smaller/medium size setups
-         * 4. Drop new docker-compose.yml setup (located inside the container, no network dependencies with appwrite.io) - DONE
-         * 5. Run docker compose up -d - DONE
-         * 6. Run data migration
-         */
         $config = Config::getParam('variables');
         $path = '/usr/src/code/appwrite';
         $defaultHTTPPort = '80';
@@ -70,7 +56,7 @@ class Install extends Action
         Console::success('Starting Appwrite installation...');
 
         // Create directory with write permissions
-        if (null !== $path && !\file_exists(\dirname($path))) {
+        if (!\file_exists(\dirname($path))) {
             if (!@\mkdir(\dirname($path), 0755, true)) {
                 Console::error('Can\'t create directory ' . \dirname($path));
                 Console::exit(1);
@@ -198,31 +184,28 @@ class Install extends Action
             }
         }
 
-        $templateForCompose = new View(__DIR__ . '/../views/install/compose.phtml');
-        $templateForEnv = new View(__DIR__ . '/../views/install/env.phtml');
+        $templateForCompose = new View(__DIR__ . '/../../../../app/views/install/compose.phtml');
+        $templateForEnv = new View(__DIR__ . '/../../../../app/views/install/env.phtml');
 
         $templateForCompose
             ->setParam('httpPort', $httpPort)
             ->setParam('httpsPort', $httpsPort)
             ->setParam('version', APP_VERSION_STABLE)
             ->setParam('organization', $organization)
-            ->setParam('image', $image)
-        ;
+            ->setParam('image', $image);
 
-        $templateForEnv
-            ->setParam('vars', $input)
-        ;
+        $templateForEnv->setParam('vars', $input);
 
         if (!file_put_contents($path . '/docker-compose.yml', $templateForCompose->render(false))) {
             $message = 'Failed to save Docker Compose file';
-            $analytics->createEvent('install/server', 'install', APP_VERSION_STABLE . ' - ' . $message);
+            $this->sendEvent($analytics, $message);
             Console::error($message);
             Console::exit(1);
         }
 
         if (!file_put_contents($path . '/.env', $templateForEnv->render(false))) {
             $message = 'Failed to save environment variables file';
-            $analytics->createEvent('install/server', 'install', APP_VERSION_STABLE . ' - ' . $message);
+            $this->sendEvent($analytics, $message);
             Console::error($message);
             Console::exit(1);
         }
@@ -243,14 +226,29 @@ class Install extends Action
 
         if ($exit !== 0) {
             $message = 'Failed to install Appwrite dockers';
-            $analytics->createEvent('install/server', 'install', APP_VERSION_STABLE . ' - ' . $message);
+            $this->sendEvent($analytics, $message);
             Console::error($message);
             Console::error($stderr);
             Console::exit($exit);
         } else {
             $message = 'Appwrite installed successfully';
-            $analytics->createEvent('install/server', 'install', APP_VERSION_STABLE . ' - ' . $message);
+            $this->sendEvent($analytics, $message);
             Console::success($message);
         }
+    }
+
+    private function sendEvent(Adapter $analytics, string $message): void
+    {
+        $event = new Event();
+        $event->setName(APP_VERSION_STABLE);
+        $event->setValue($message);
+        $event->setUrl('http://localhost/');
+        $event->setProps([
+            'category' => 'install/server',
+            'action' => 'install',
+        ]);
+        $event->setType('install/server');
+
+        $analytics->createEvent($event);
     }
 }
