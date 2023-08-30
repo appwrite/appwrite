e018c29b712a6595e26305091935a645d072d59a
Jake Barnby <jakeb994@gmail.com>
2023-08-30 15:48:01 -0400

    Bump .NET SDK version

--- app/config/platforms.php
+++ app/config/platforms.php
@@ -357,7 +357,7 @@ return [
             [
                 'key' => 'dotnet',
                 'name' => '.NET',
-                'version' => '0.4.2',
+                'version' => '0.5.0',
                 'url' => 'https://github.com/appwrite/sdk-for-dotnet',
                 'package' => 'https://www.nuget.org/packages/Appwrite',
                 'enabled' => true,
