b7119a2718a48512312451c84d867f5bdbd71dd6
Prateek Banga <prateekbanga@Prateeks-MacBook-Air.local>
2023-09-12 21:03:44 +0530

    review changes

--- app/init.php
+++ app/init.php
@@ -543,6 +543,45 @@ Database::addFilter(
             ]));
     }
 );
+
+Database::addFilter(
+    'subQueryProvider',
+    function (mixed $value) {
+        return null;
+    },
+    function (mixed $value, Document $document, Database $database) {
+        $provider = Authorization::skip(fn () => $database
+            ->getDocument(
+                'providers',
+                $document->getAttribute('providerId'),
+            ));
+        if (!$provider->isEmpty()) {
+            return $provider;
+        }
+        return null;
+    }
+);
+
+Database::addFilter(
+    'subQueryTopicTargets',
+    function (mixed $value) {
+        return null;
+    },
+    function (mixed $value, Document $document, Database $database) {
+        $targetIds = Authorization::skip(fn () => \array_map(
+            fn ($document) => $document->getAttribute('targetId'),
+            $database
+            ->find('subscribers', [
+                Query::equal('topicInternalId', [$document->getInternalId()]),
+                Query::limit(APP_LIMIT_SUBQUERY),
+            ])
+        ));
+        if (\count($targetIds) > 0) {
+            return $database->find('targets', [Query::equal('$id', $targetIds)]);
+        }
+        return [];
+    }
+);
 /**
  * DB Formats
  */
