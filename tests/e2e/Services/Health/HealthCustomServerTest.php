2d578dddacec82305f0d210620ef80afb4f60e5c
Matej Bačo <matejbaco2000@gmail.com>
2023-09-24 14:36:47 +0200

    Remove double test

--- tests/e2e/Services/Health/HealthCustomServerTest.php
+++ tests/e2e/Services/Health/HealthCustomServerTest.php
@@ -183,16 +183,6 @@ class HealthCustomServerTest extends Scope
         $this->assertIsInt($response['body']['size']);
         $this->assertLessThan(100, $response['body']['size']);
 
-
-        $response = $this->client->call(Client::METHOD_GET, '/health/queue/logs', array_merge([
-            'content-type' => 'application/json',
-            'x-appwrite-project' => $this->getProject()['$id'],
-        ], $this->getHeaders()), []);
-
-        $this->assertEquals(200, $response['headers']['status-code']);
-        $this->assertIsInt($response['body']['size']);
-        $this->assertLessThan(100, $response['body']['size']);
-
         /**
          * Test for FAILURE
          */
