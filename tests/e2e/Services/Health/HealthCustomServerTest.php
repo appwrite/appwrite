6cbcb610f820a508d67c813f3e18fc9d530f256c
Matej Bačo <matejbaco2000@gmail.com>
2023-10-02 11:12:09 +0200

    Rename logs to audits

--- tests/e2e/Services/Health/HealthCustomServerTest.php
+++ tests/e2e/Services/Health/HealthCustomServerTest.php
@@ -141,12 +141,12 @@ class HealthCustomServerTest extends Scope
         return [];
     }
 
-    public function testLogsSuccess(): array
+    public function testAuditsSuccess(): array
     {
         /**
          * Test for SUCCESS
          */
-        $response = $this->client->call(Client::METHOD_GET, '/health/queue/logs', array_merge([
+        $response = $this->client->call(Client::METHOD_GET, '/health/queue/audits', array_merge([
             'content-type' => 'application/json',
             'x-appwrite-project' => $this->getProject()['$id'],
         ], $this->getHeaders()), []);
