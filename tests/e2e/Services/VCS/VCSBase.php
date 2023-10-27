9164ed1045a5e1098d7c83bd5ae4ea77758f6dc0
Khushboo Verma <43381712+vermakhushboo@users.noreply.github.com>
2023-10-27 19:43:07 +0530

    Fixed linter errors

--- tests/e2e/Services/VCS/VCSBase.php
+++ tests/e2e/Services/VCS/VCSBase.php
@@ -6,12 +6,12 @@ use Utopia\App;
 
 trait VCSBase
 {
-    protected function setUp(): void 
-    { 
-        parent::setUp(); 
-    
-        if (App::getEnv('_APP_VCS_GITHUB_PRIVATE_KEY') === 'disabled') { 
-            $this->markTestSkipped('VCS is not enabled.'); 
-        } 
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (App::getEnv('_APP_VCS_GITHUB_PRIVATE_KEY') === 'disabled') {
+            $this->markTestSkipped('VCS is not enabled.');
+        }
     }
 }
