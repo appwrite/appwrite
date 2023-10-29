d0857856aa034eb148788f2ce083fe0e38429084
Gordian <68512769+Gordiancyber@users.noreply.github.com>
2023-10-29 04:27:07 +0100

    Adding code comments for each line Update Appwrite.php
    adding comments to explain the purpose of each line in more detail, from top to bottom for appwrite.php

--- src/Appwrite/Platform/Appwrite.php
+++ src/Appwrite/Platform/Appwrite.php
@@ -1,16 +1,28 @@
 <?php
 
+// Define the namespace for the Appwrite\Platform class
 namespace Appwrite\Platform;
 
+// Import the 'Tasks' and 'Workers' services
 use Appwrite\Platform\Services\Tasks;
 use Appwrite\Platform\Services\Workers;
+
+// Import the 'Platform' class from the Utopia namespace
 use Utopia\Platform\Platform;
 
+// Define the 'Appwrite' class that extends the 'Platform' class
 class Appwrite extends Platform
 {
+    // Constructor for the 'Appwrite' class
     public function __construct()
     {
+        // Call the constructor of the parent class 'Platform'
+        parent::__construct();
+
+        // Add the 'tasks' service to the 'Appwrite' platform
         $this->addService('tasks', new Tasks());
+
+        // Add the 'workers' service to the 'Appwrite' platform
         $this->addService('workers', new Workers());
     }
 }
