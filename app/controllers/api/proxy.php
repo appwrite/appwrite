8b24c44fb9fcad2d120e5b5b65a27c5bb8b206cc
loks0n <22452787+loks0n@users.noreply.github.com>
2023-11-06 14:44:00 +0000

    fix: proxy create rule 500

--- app/controllers/api/proxy.php
+++ app/controllers/api/proxy.php
@@ -87,8 +87,12 @@ App::post('/v1/proxy/rules')
             $resourceInternalId = $function->getInternalId();
         }
 
-        $domain = new Domain($domain);
-
+        try {
+            $domain = new Domain($domain);
+        } catch (Exception) {
+            throw new Exception(Exception::GENERAL_ARGUMENT_INVALID, 'Invalid domain. Domain may not start with http:// or https://.');
+        }
+        
         $ruleId = ID::unique();
         $rule = new Document([
             '$id' => $ruleId,
