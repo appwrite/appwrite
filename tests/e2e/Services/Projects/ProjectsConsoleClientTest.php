d8d9976aa13d4bf84c601052fc18adf1b7093c08
Eldad Fux <eldad.fux@gmail.com>
2023-12-19 22:39:40 +0100

    Updated tests

--- tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
+++ tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
@@ -584,14 +584,20 @@ class ProjectsConsoleClientTest extends Scope
     public function testCreateProjectSMTPTests($data): array
     {
         $id = $data['projectId'];
+        $email = 'mailer@appwrite.io';
         $response = $this->client->call(Client::METHOD_PATCH, '/projects/' . $id . '/smtp/tests', array_merge([
             'content-type' => 'application/json',
             'x-appwrite-project' => $this->getProject()['$id'],
         ], $this->getHeaders()), [
-            'email' => 'mailer@appwrite.io',
+            'email' => $email,
         ]);
 
         $this->assertEquals(204, $response['headers']['status-code']);
+
+        $lastEmail = $this->getLastEmail();
+
+        $this->assertEquals($email, $lastEmail['to'][0]['address']);
+        $this->assertEquals('SMTP test email from Appwrite', $lastEmail['subject']);
 
         return $data;
     }
