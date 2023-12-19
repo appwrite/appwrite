cac0b0b5e4cf9f6c4095d45745de84aa01f9ec0b
Eldad Fux <eldad.fux@gmail.com>
2023-12-19 23:31:45 +0100

    Fixed test

--- tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
+++ tests/e2e/Services/Projects/ProjectsConsoleClientTest.php
@@ -585,7 +585,7 @@ class ProjectsConsoleClientTest extends Scope
     {
         $id = $data['projectId'];
         $email = 'mailer@appwrite.io';
-        $response = $this->client->call(Client::METHOD_PATCH, '/projects/' . $id . '/smtp/tests', array_merge([
+        $response = $this->client->call(Client::METHOD_POST, '/projects/' . $id . '/smtp/tests', array_merge([
             'content-type' => 'application/json',
             'x-appwrite-project' => $this->getProject()['$id'],
         ], $this->getHeaders()), [
