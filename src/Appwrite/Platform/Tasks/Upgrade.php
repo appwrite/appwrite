864703fb4ee5711af4a05d6927111a9a70b38210
Steven Nguyen <stnguyen90@users.noreply.github.com>
2024-01-08 20:56:51 +0000

    Update the install/upgrade arguments to be kebab instead of camel case
    Kebab case like --http-port is much more common in CLIs than --httpPort.

--- src/Appwrite/Platform/Tasks/Upgrade.php
+++ src/Appwrite/Platform/Tasks/Upgrade.php
@@ -17,12 +17,12 @@ class Upgrade extends Install
     {
         $this
             ->desc('Upgrade Appwrite')
-            ->param('httpPort', '', new Text(4), 'Server HTTP port', true)
-            ->param('httpsPort', '', new Text(4), 'Server HTTPS port', true)
+            ->param('http-port', '', new Text(4), 'Server HTTP port', true)
+            ->param('https-port', '', new Text(4), 'Server HTTPS port', true)
             ->param('organization', 'appwrite', new Text(0), 'Docker Registry organization', true)
             ->param('image', 'appwrite', new Text(0), 'Main appwrite docker image', true)
             ->param('interactive', 'Y', new Text(1), 'Run an interactive session', true)
-            ->param('noStart', false, new Boolean(true), 'Run an interactive session', true)
+            ->param('no-start', false, new Boolean(true), 'Run an interactive session', true)
             ->callback(fn ($httpPort, $httpsPort, $organization, $image, $interactive, $noStart) => $this->action($httpPort, $httpsPort, $organization, $image, $interactive, $noStart));
     }
 
