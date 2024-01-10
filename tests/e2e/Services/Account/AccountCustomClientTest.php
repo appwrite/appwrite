de977fc84c092ca2de8f446be90705db77f3dbda
loks0n <22452787+loks0n@users.noreply.github.com>
2024-01-10 10:27:38 +0000

    test: fix account tests

--- tests/e2e/Services/Account/AccountCustomClientTest.php
+++ tests/e2e/Services/Account/AccountCustomClientTest.php
@@ -2347,9 +2347,9 @@ class AccountCustomClientTest extends Scope
 
         $lastEmail = $this->getLastEmail();
         $this->assertEquals($email, $lastEmail['to'][0]['address']);
-        $this->assertEquals('Login', $lastEmail['subject']);
+        $$this->assertEquals($this->getProject()['name'] . ' Login', $lastEmail['subject']);
 
-        $token = substr($lastEmail['text'], strpos($lastEmail['text'], '&secret=', 0) + 8, 256);
+        $token = substr($lastEmail['text'], strpos($lastEmail['text'], '&secret=', 0) + 8, 64);
 
         $expireTime = strpos($lastEmail['text'], 'expire=' . urlencode($response['body']['expire']), 0);
 
