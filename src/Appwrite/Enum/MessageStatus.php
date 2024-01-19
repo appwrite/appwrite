e597def3f302f7c977513d8d52d1aa7a95448493
Steven Nguyen <stnguyen90@users.noreply.github.com>
2024-01-19 01:53:56 +0000

    Remove commented out MessageStatus enum

--- src/Appwrite/Enum/MessageStatus.php
+++ src/Appwrite/Enum/MessageStatus.php
@@ -2,31 +2,6 @@
 
 namespace Appwrite\Enum;
 
-// TODO: Convert to backed enum after upgrading to PHP 8.1
-// enum MessageStatus: string
-// {
-//     /**
-//      * Message that is not ready to be sent
-//      */
-//     case Draft = 'draft';
-//     /**
-//      * Scheduled to be sent for a later time
-//      */
-//     case Scheduled = 'scheduled';
-//     /**
-//      * Picked up by the worker and starting to send
-//      */
-//     case Processing = 'processing';
-//     /**
-//      * Sent without errors
-//      */
-//     case Sent = 'sent';
-//     /**
-//      * Sent with some errors
-//      */
-//     case Failed = 'failed';
-// }
-
 class MessageStatus
 {
     /**
