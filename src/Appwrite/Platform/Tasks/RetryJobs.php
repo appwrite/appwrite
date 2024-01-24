ab2d830cc0357b1ec6ae8ba00f1d04aba19f2e70
Bradley Schofield <ionicisere@gmail.com>
2024-01-24 17:44:29 +0000

    Address comments

--- src/Appwrite/Platform/Tasks/RetryJobs.php
+++ src/Appwrite/Platform/Tasks/RetryJobs.php
@@ -2,11 +2,14 @@
 
 namespace Appwrite\Platform\Tasks;
 
+use Appwrite\Event\Event;
+use Twig\Node\Expression\Test\EvenTest;
 use Utopia\CLI\Console;
 use Utopia\Platform\Action;
 use Utopia\Queue\Client;
 use Utopia\Queue\Connection;
 use Utopia\Validator\Text;
+use Utopia\Validator\WhiteList;
 
 class RetryJobs extends Action
 {
@@ -20,7 +23,20 @@ class RetryJobs extends Action
     {
         $this
             ->desc('Retry failed jobs from a specific queue identified by the name parameter')
-            ->param('name', '', new Text(128), 'Queue name')
+            ->param('name', '', new WhiteList([
+                Event::DATABASE_QUEUE_NAME,
+                Event::DELETE_QUEUE_NAME,
+                Event::AUDITS_QUEUE_NAME,
+                Event::MAILS_QUEUE_NAME,
+                Event::FUNCTIONS_QUEUE_NAME,
+                Event::USAGE_QUEUE_NAME,
+                Event::WEBHOOK_CLASS_NAME,
+                Event::CERTIFICATES_QUEUE_NAME,
+                Event::BUILDS_QUEUE_NAME,
+                Event::MESSAGING_QUEUE_NAME,
+                Event::MIGRATIONS_QUEUE_NAME,
+                Event::HAMSTER_CLASS_NAME
+            ]), 'Queue name')
             ->inject('queue')
             ->callback(fn ($name, $queue) => $this->action($name, $queue));
     }
@@ -42,6 +58,8 @@ class RetryJobs extends Action
             Console::error('No failed jobs found.');
             return;
         }
+
+        Console::log('Retrying failed jobs...');
 
         $queueClient->retry();
     }
