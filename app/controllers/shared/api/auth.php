d40a51b50a0db3c48d904f397a36bed6e9389ada
Khushboo Verma <43381712+vermakhushboo@users.noreply.github.com>
2024-02-02 20:01:54 +0530

    Only disallow console requests

--- app/controllers/shared/api/auth.php
+++ app/controllers/shared/api/auth.php
@@ -15,8 +15,8 @@ App::init()
     ->inject('project')
     ->inject('geodb')
     ->action(function (App $utopia, Request $request, Document $project, Reader $geodb) {
-        $denylist = App::getEnv('_APP_COUNTRIES_DENYLIST', '');
-        if (!empty($denylist)) {
+        $denylist = App::getEnv('_APP_CONSOLE_COUNTRIES_DENYLIST', '');
+        if (!empty($denylist && $project->getId() === 'console')) {
             $countries = explode(',', $denylist);
             $record = $geodb->get($request->getIP()) ?? [];
             $country = $record['country']['iso_code'] ?? '';
