d4afe2239662927b06844bdffdcb2118facb48b1
Matej Bačo <matejbaco2000@gmail.com>
2024-02-06 12:07:47 +0100

    Add ZDT migration support to project creation

--- app/controllers/api/projects.php
+++ app/controllers/api/projects.php
@@ -4,6 +4,7 @@ use Appwrite\Auth\Auth;
 use Appwrite\Event\Delete;
 use Appwrite\Event\Validator\Event;
 use Appwrite\Extend\Exception;
+use Appwrite\Hooks\Hooks;
 use Appwrite\Network\Validator\Email;
 use Appwrite\Network\Validator\Origin;
 use Appwrite\Template\Template;
@@ -75,7 +76,8 @@ App::post('/v1/projects')
     ->inject('dbForConsole')
     ->inject('cache')
     ->inject('pools')
-    ->action(function (string $projectId, string $name, string $teamId, string $region, string $description, string $logo, string $url, string $legalName, string $legalCountry, string $legalState, string $legalCity, string $legalAddress, string $legalTaxId, Response $response, Database $dbForConsole, Cache $cache, Group $pools) {
+    ->inject('hooks')
+    ->action(function (string $projectId, string $name, string $teamId, string $region, string $description, string $logo, string $url, string $legalName, string $legalCountry, string $legalState, string $legalCity, string $legalAddress, string $legalTaxId, Response $response, Database $dbForConsole, Cache $cache, Group $pools, Hooks $hooks) {
 
 
         $team = $dbForConsole->getDocument('teams', $teamId);
@@ -182,6 +184,12 @@ App::post('/v1/projects')
 
         /** @var array $collections */
         $collections = Config::getParam('collections', [])['projects'] ?? [];
+
+        // Allow Cloud overrides (useful for ZDT migration)
+        $collectiosOverride = $hooks->trigger('getProjectCollections');
+        if(!empty($collectiosOverride)) {
+            $collections = $collectiosOverride;
+        }
 
         foreach ($collections as $key => $collection) {
             if (($collection['$collection'] ?? '') !== Database::METADATA) {
