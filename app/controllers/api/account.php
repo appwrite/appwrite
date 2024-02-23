83c5477c794242869683616bb99ceecde0b9921f
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-02-23 01:46:13 +0100

    fix: missing team variable and added mfa templates to config

--- app/controllers/api/account.php
+++ app/controllers/api/account.php
@@ -1192,14 +1192,16 @@ App::post('/v1/account/tokens/magic-url')
 
         $emailVariables = [
             'direction' => $locale->getText('settings.direction'),
-            /* {{user}}, {{redirect}} and {{project}} are required in default and custom templates */
+            // {{user}}, {{redirect}} and {{project}} are required in default and custom templates
             'user' => $user->getAttribute('name'),
             'project' => $project->getAttribute('name'),
             'redirect' => $url,
             'agentDevice' => $agentDevice['deviceBrand'] ?? $agentDevice['deviceBrand'] ?? 'UNKNOWN',
             'agentClient' => $agentClient['clientName'] ?? 'UNKNOWN',
             'agentOs' => $agentOs['osName'] ?? 'UNKNOWN',
-            'phrase' => !empty($phrase) ? $phrase : ''
+            'phrase' => !empty($phrase) ? $phrase : '',
+            // TODO: remove unnecessary team variable from this email
+            'team' => '',
         ];
 
         $queueForMails
@@ -1419,14 +1421,16 @@ App::post('/v1/account/tokens/email')
 
         $emailVariables = [
             'direction' => $locale->getText('settings.direction'),
-            /* {{user}}, {{project}} and {{otp}} are required in the templates */
+            // {{user}}, {{project}} and {{otp}} are required in the templates
             'user' => $user->getAttribute('name'),
             'project' => $project->getAttribute('name'),
             'otp' => $tokenSecret,
             'agentDevice' => $agentDevice['deviceBrand'] ?? $agentDevice['deviceBrand'] ?? 'UNKNOWN',
             'agentClient' => $agentClient['clientName'] ?? 'UNKNOWN',
             'agentOs' => $agentOs['osName'] ?? 'UNKNOWN',
-            'phrase' => !empty($phrase) ? $phrase : ''
+            'phrase' => !empty($phrase) ? $phrase : '',
+            // TODO: remove unnecessary team variable from this email
+            'team' => '',
         ];
 
         $queueForMails
@@ -2884,10 +2888,12 @@ App::post('/v1/account/recovery')
 
         $emailVariables = [
             'direction' => $locale->getText('settings.direction'),
-            /* {{user}}, {{redirect}} and {{project}} are required in default and custom templates */
+            // {{user}}, {{redirect}} and {{project}} are required in default and custom templates
             'user' => $profile->getAttribute('name'),
             'redirect' => $url,
-            'project' => $projectName
+            'project' => $projectName,
+            // TODO: remove unnecessary team variable from this email
+            'team' => ''
         ];
 
         $queueForMails
@@ -3130,10 +3136,12 @@ App::post('/v1/account/verification')
 
         $emailVariables = [
             'direction' => $locale->getText('settings.direction'),
-            /* {{user}}, {{redirect}} and {{project}} are required in default and custom templates */
+            // {{user}}, {{redirect}} and {{project}} are required in default and custom templates
             'user' => $user->getAttribute('name'),
             'redirect' => $url,
-            'project' => $projectName
+            'project' => $projectName,
+            // TODO: remove unnecessary team variable from this email
+            'team' => '',
         ];
 
         $queueForMails
@@ -3778,7 +3786,7 @@ App::post('/v1/account/mfa/challenge')
 
                 $emailVariables = [
                     'direction' => $locale->getText('settings.direction'),
-                    /* {{user}}, {{project}} and {{otp}} are required in the templates */
+                    // {{user}}, {{project}} and {{otp}} are required in the templates
                     'user' => $user->getAttribute('name'),
                     'project' => $project->getAttribute('name'),
                     'otp' => $code,
