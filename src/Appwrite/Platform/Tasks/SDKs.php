82efd571469b03f1257906ba784bdafec61d8e38
Jake Barnby <jakeb994@gmail.com>
2024-02-23 18:32:15 +1300

    Fix hidden files not copied when generating SDKs

--- src/Appwrite/Platform/Tasks/SDKs.php
+++ src/Appwrite/Platform/Tasks/SDKs.php
@@ -260,7 +260,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
                         git fetch origin ' . $gitBranch . ' && \
                         git pull origin ' . $gitBranch . ' && \
                         rm -rf ' . $target . '/* && \
-                        cp -r ' . $result . '/* ' . $target . '/ && \
+                        cp -r ' . $result . '/. ' . $target . '/ && \
                         git add . && \
                         git commit -m "' . $message . '" && \
                         git push -u origin ' . $gitBranch . '
