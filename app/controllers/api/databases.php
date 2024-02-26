6a3cc4a5abd23fee71d47ccd069ca00d2f01cf05
fogelito <fogelshmuel@gmail.com>
2024-02-26 14:22:28 +0200

    Duplicate message

--- app/controllers/api/databases.php
+++ app/controllers/api/databases.php
@@ -3501,7 +3501,6 @@ App::patch('/v1/databases/:databaseId/collections/:collectionId/documents/:docum
             $message .= !is_null($exception->getDocumentId()) ? ' Id: ' . $exception->getDocumentId() : '';
             $message .= !is_null($exception->getRelatedCollectionId()) ? ' Related collection: ' . $exception->getRelatedCollectionId() : '';
             $message .= !is_null($exception->getRelatedDocumentId()) ? ' Related Id: ' . $exception->getRelatedDocumentId() : '';
-            $message .= 'Try again with a different ID or use ID.unique() to generate a unique ID.';
             throw new Exception(Exception::DOCUMENT_ALREADY_EXISTS, $message);
         } catch (StructureException $exception) {
             throw new Exception(Exception::DOCUMENT_INVALID_STRUCTURE, $exception->getMessage());
