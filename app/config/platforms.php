0cb1fed43a61b5acd0090e1eb3775a217227320d
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-03-04 10:04:51 +0100

    fix: release console sdk

--- app/config/platforms.php
+++ app/config/platforms.php
@@ -185,7 +185,7 @@ return [
             [
                 'key' => 'web',
                 'name' => 'Console',
-                'version' => '0.6.0-rc.14',
+                'version' => '0.6.0-rc.15',
                 'url' => 'https://github.com/appwrite/sdk-for-console',
                 'package' => '',
                 'enabled' => true,
