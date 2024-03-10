b409efc492cb68efae0d372f7c375e70e5a7b543
Damodar Lohani <dlohani48@gmail.com>
2024-03-10 08:01:10 +0000

    fix path

--- tests/e2e/Services/Databases/DatabasesBase.php
+++ tests/e2e/Services/Databases/DatabasesBase.php
@@ -4266,7 +4266,7 @@ trait DatabasesBase
             ], $this->getHeaders()), [
                 'documentId' => ID::unique(),
                 'data' => [
-                    'longtext' => file_get_contents(__DIR__ . '/../../../extensionsresources/longtext.txt'),
+                    'longtext' => file_get_contents(__DIR__ . '/../../../resources/longtext.txt'),
                 ],
                 'permissions' => [
                     Permission::read(Role::user($this->getUser()['$id'])),
