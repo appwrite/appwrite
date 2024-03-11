4daa5b9c93ac5597bc1843518ca9f96ca27530b8
Khushboo Verma <43381712+vermakhushboo@users.noreply.github.com>
2024-03-11 15:55:54 +0100

    Remove setResource startTime

--- app/init.php
+++ app/init.php
@@ -875,10 +875,6 @@ App::setResource('hooks', function ($register) {
     return $register->get('hooks');
 }, ['register']);
 
-App::setResource('startTime', function() {
-    return \microtime(true);
-});
-
 App::setResource('register', fn() => $register);
 App::setResource('locale', fn() => new Locale(App::getEnv('_APP_LOCALE', 'en')));
 
