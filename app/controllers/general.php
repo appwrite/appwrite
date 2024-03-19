1983304ee87f4e8e1b0c1564ed7424872596e9cf
Bradley Schofield <ionicisere@gmail.com>
2024-03-19 09:07:32 +0000

    Rollback experimental logging

--- app/controllers/general.php
+++ app/controllers/general.php
@@ -858,23 +858,6 @@ App::error()
             $publish = $error->getCode() === 0 || $error->getCode() >= 500;
         }
 
-        if ($error->getCode() >= 400 && $error->getCode() < 500) {
-            // Register error logger
-            $providerName = App::getEnv('_APP_EXPERIMENT_LOGGING_PROVIDER', '');
-            $providerConfig = App::getEnv('_APP_EXPERIMENT_LOGGING_CONFIG', '');
-
-            if (!(empty($providerName) || empty($providerConfig))) {
-                if (!Logger::hasProvider($providerName)) {
-                    throw new Exception("Logging provider not supported. Logging is disabled");
-                }
-
-                $classname = '\\Utopia\\Logger\\Adapter\\' . \ucfirst($providerName);
-                $adapter = new $classname($providerConfig);
-                $logger = new Logger($adapter);
-                $publish = true;
-            }
-        }
-
         if ($logger && $publish) {
             try {
                 /** @var Utopia\Database\Document $user */
