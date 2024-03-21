fd6b1df93a7eebe58d3e16b9ead99f7dd8a51f98
Damodar Lohani <dlohani48@gmail.com>
2024-03-21 07:54:06 +0000

    fetch domains

--- app/http.php
+++ app/http.php
@@ -20,11 +20,17 @@ use Utopia\Database\Database;
 use Utopia\Database\Document;
 use Utopia\Swoole\Files;
 use Appwrite\Utopia\Request;
+use Swoole\Timer;
+use Utopia\Database\DateTime;
+use Utopia\Database\Query;
 use Utopia\Logger\Log;
 use Utopia\Logger\Log\User;
 use Utopia\Pools\Group;
 
+const DOMAIN_SYNC_TIMER = 30; // 30 seconds
+
 $domains = [];
+$lastSyncUpdate = null;
 
 $http = new Server("0.0.0.0", App::getEnv('PORT', 80), SWOOLE_PROCESS);
 
@@ -44,39 +50,41 @@ $http
         'buffer_output_size' => $payloadSize,
     ]);
 
-function dispatch(Server $server, $fd, $type, $data = null) {
+function dispatch(Server $server, $fd, $type, $data = null)
+{
     global $workerNumber, $domains;
-    
+
     $safeThreadsPercent = intval(App::getEnv('_APP_SAFE_THREADS_PERCENT', 80)) / 100;
 
     $safeThreads = (int) floor($workerNumber * $safeThreadsPercent);
 
     $risky = false;
-    if($data && str_contains($data, 'POST') && str_contains($data, '/executions')) {
+    if ($data && str_contains($data, 'POST') && str_contains($data, '/executions')) {
         $risky = true;
-    } else if ($data && str_contains($data, 'GET')){
+    } elseif ($data && str_contains($data, '.appwrite.global')) {
+        $risky = true;
+    } elseif ($data) {
         $lines = explode($data, '\n');
-        if(count($lines) > 2) {
+        if (count($lines) > 2) {
             $host = trim(explode('Host: ', $lines[1])[1]);
-            if(array_key_exists($host, $domains)) {
+            if (array_key_exists($host, $domains)) {
                 $risky = true;
             }
         }
     }
 
-    if($risky) {
+    if ($risky) {
         var_dump('execution request sending to unsafe thread');
-        for($j = $safeThreads; $j < $workerNumber; $j++) {
-            if($server->getWorkerStatus($j) === 2) {
+        for ($j = $safeThreads; $j < $workerNumber; $j++) {
+            if ($server->getWorkerStatus($j) === 2) {
                 return $j;
             }
         }
         return rand($safeThreads, $workerNumber - 1);
-
     }
 
-    for($i = 0; $i < $workerNumber; $i++) {
-        if($server->getWorkerStatus($i) === 2) {
+    for ($i = 0; $i < $workerNumber; $i++) {
+        if ($server->getWorkerStatus($i) === 2) {
             return $i;
         }
     }
@@ -99,10 +107,10 @@ Files::load(__DIR__ . '/../console');
 
 include __DIR__ . '/controllers/general.php';
 
-$http->on('start', function (Server $http) use ($payloadSize, $register) {
+$http->on('start', function (Server $http) use ($payloadSize, $register, &$domains, &$lastSyncUpdate) {
     $app = new App('UTC');
 
-    go(function () use ($register, $app) {
+    go(function () use ($register, $app, &$domains, &$lastSyncUpdate) {
         $pools = $register->get('pools');
         /** @var Group $pools */
         App::setResource('pools', fn () => $pools);
@@ -249,6 +257,8 @@ $http->on('start', function (Server $http) use ($payloadSize, $register) {
             $dbForConsole->createCollection('bucket_' . $bucket->getInternalId(), $attributes, $indexes);
         }
 
+        getDomains($dbForConsole, $lastSyncUpdate, $domains, $pools);
+
         $pools->reclaim();
 
         Console::success('[Setup] - Server database init completed...');
@@ -264,7 +274,7 @@ $http->on('start', function (Server $http) use ($payloadSize, $register) {
     });
 });
 
-$http->on('request', function (SwooleRequest $swooleRequest, SwooleResponse $swooleResponse) use ($register, $http) {
+$http->on('request', function (SwooleRequest $swooleRequest, SwooleResponse $swooleResponse) use ($register) {
     App::setResource('swooleRequest', fn () => $swooleRequest);
     App::setResource('swooleResponse', fn () => $swooleResponse);
     $request = new Request($swooleRequest);
@@ -367,5 +377,51 @@ $http->on('request', function (SwooleRequest $swooleRequest, SwooleResponse $swo
         $pools->reclaim();
     }
 });
+
+function getDomains(Database $dbForConsole, &$lastSyncUpdate, &$domains, $pools)
+{
+    go(function () use ($dbForConsole, &$lastSyncUpdate, &$domains, $pools) {
+        Timer::tick(DOMAIN_SYNC_TIMER * 1000, function () use ($dbForConsole, &$domains, &$lastSyncUpdate, $pools) {
+            $time = DateTime::now();
+            $timerStart = \microtime(true);
+            var_dump($domains);
+            $limit = 1000;
+            $sum = $limit;
+            $total = 0;
+            $latestDocument = null;
+
+            while ($sum === $limit) {
+                $queries = [Query::limit($limit)];
+                if ($latestDocument !== null) {
+                    $queries[] =  Query::cursorAfter($latestDocument);
+                }
+                if ($lastSyncUpdate != null) {
+                    $queries[] = Query::greaterThanEqual('$updatedAt', $lastSyncUpdate);
+                }
+                $queries[] = Query::equal('resourceType', ['function']);
+
+                $results = Authorization::skip(fn () =>  $dbForConsole->find('rules', $queries));
+
+                $sum = count($results);
+                $total = $total + $sum;
+                foreach ($results as $document) {
+                    $domain = $document->getAttribute('domain');
+                    if (str_contains($domain, '.appwrite.global')) {
+                        continue;
+                    }
+                    $domains[$domain] = true;
+                }
+                $latestDocument = !empty(array_key_last($results)) ? $results[array_key_last($results)] : null;
+            }
+
+            $lastSyncUpdate = $time;
+            $timerEnd = \microtime(true);
+
+            $pools->reclaim();
+
+            Console::log("Sync domains tick: {$total} domains were updated in " . ($timerEnd - $timerStart) . " seconds");
+        });
+    });
+}
 
 $http->start();
