f4d3d1dda03fd4ff3e1272f4b969c3e8e55f54fc
Damodar Lohani <dlohani48@gmail.com>
2024-04-25 01:55:20 +0000

    update flutter version

--- app/config/platforms.php
+++ app/config/platforms.php
@@ -63,7 +63,7 @@ return [
             [
                 'key' => 'flutter',
                 'name' => 'Flutter',
-                'version' => '12.0.1',
+                'version' => '12.0.2',
                 'url' => 'https://github.com/appwrite/sdk-for-flutter',
                 'package' => 'https://pub.dev/packages/appwrite',
                 'enabled' => true,
