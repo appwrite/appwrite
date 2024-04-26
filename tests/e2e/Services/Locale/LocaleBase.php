37097b09148105b34976fd0c9c6662659d84567b
bhc <behicsakar@gmail.com>
2024-04-27 01:18:11 +0300

    Update total count of countries in LocaleBase tests for testGetCountriesPhones

--- tests/e2e/Services/Locale/LocaleBase.php
+++ tests/e2e/Services/Locale/LocaleBase.php
@@ -120,7 +120,7 @@ trait LocaleBase
 
         $this->assertEquals($response['headers']['status-code'], 200);
         $this->assertIsArray($response['body']);
-        $this->assertEquals(195, $response['body']['total']);
+        $this->assertEquals(196, $response['body']['total']);
         $this->assertIsArray($response['body']['phones']);
         $this->assertEquals($response['body']['phones'][0]['code'], '+1');
         $this->assertEquals($response['body']['phones'][0]['countryName'], 'Canada');
