9a7c424e85b40d08ee9007e4efb900dc358597c6
Jake Barnby <jakeb994@gmail.com>
2024-04-29 16:52:46 +1200

    Update app/controllers/general.php

--- app/controllers/general.php
+++ app/controllers/general.php
@@ -364,7 +364,7 @@ App::init()
     ->action(function (Document $project, Appwrite\Utopia\Request $request) {
         if ($project->getId() === 'console') {
             $message = empty($request->getHeader('x-appwrite-project')) ?
-                'No Appwrite project was specified. Please specify your Appwrite Project ID in the "x-appwrite-project" header or when you initialize your Appwrite SDK.' :
+                'No Appwrite project was specified. Please specify your project ID when initializing your Appwrite SDK.' :
                 'This endpoint is not available for the console project. The Appwrite Console is a reserved project ID and cannot be used with the Appwrite SDKs and APIs. Please check if your project ID is correct.';
             throw new AppwriteException(AppwriteException::GENERAL_ACCESS_FORBIDDEN, $message);
         }
