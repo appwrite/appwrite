885e7019f81fcfac27936a1c394267f2a98d2e0c
Khushboo Verma <43381712+vermakhushboo@users.noreply.github.com>
2024-05-01 23:37:18 +0530

    Update chunk size to 10 MB

--- app/http.php
+++ app/http.php
@@ -32,7 +32,7 @@ $http = new Server(
     mode: SWOOLE_PROCESS,
 );
 
-$payloadSize = 6 * (1024 * 1024); // 6MB
+$payloadSize = 10 * (1024 * 1024); // 10MB
 $workerNumber = swoole_cpu_num() * intval(System::getEnv('_APP_WORKER_PER_CORE', 6));
 
 $http
