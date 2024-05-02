d4254b34e22c8324b2b1785096ae62abbc8bb2df
Jake Barnby <jakeb994@gmail.com>
2024-05-02 18:28:03 +1200

    Update db name

--- app/init.php
+++ app/init.php
@@ -144,7 +144,7 @@ const APP_SOCIAL_YOUTUBE = 'https://www.youtube.com/c/appwrite?sub_confirmation=
 const APP_HOSTNAME_INTERNAL = 'appwrite';
 
 // Databases
-const DATABASE_SHARED_TABLES = 'database_db_self_hosted_shared_tables';
+const DATABASE_SHARED_TABLES = 'database_db_fra1_self_hosted_16_0';
 
 // Database Reconnect
 const DATABASE_RECONNECT_SLEEP = 2;
