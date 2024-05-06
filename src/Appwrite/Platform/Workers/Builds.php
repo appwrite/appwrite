7d42db82a0a219aba7ee4186798189ee295387f0
Matej Bačo <matejbaco2000@gmail.com>
2024-05-06 15:05:13 +0000

    Fix python failing logs

--- src/Appwrite/Platform/Workers/Builds.php
+++ src/Appwrite/Platform/Workers/Builds.php
@@ -433,6 +433,8 @@ class Builds extends Action
                                         throw new \Exception('Build not found', 404);
                                     }
 
+                                    $logs = \mb_substr($logs, 0, \mb_strlen($logs, 'UTF-8') - 1, 'UTF-8'); // Get only valid UTF8 part - removes leftover half-multibytes causing SQL errors
+
                                     $build = $build->setAttribute('logs', $build->getAttribute('logs', '') . $logs);
                                     $build = $dbForProject->updateDocument('builds', $build->getId(), $build);
 
