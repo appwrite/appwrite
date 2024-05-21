8b85454ba172915ffc88edbac2f3a5e0a506be27
fogelito <fogelshmuel@gmail.com>
2024-05-21 15:49:42 +0300

    push

--- app/controllers/general.php
+++ app/controllers/general.php
@@ -376,7 +376,7 @@ App::init()
 */
 
 App::init()
-    ->groups(['database', 'functions', 'storage', 'messaging', 'vcs'])
+    ->groups(['database', 'functions', 'storage', 'messaging', 'vcs', 'vcs'])
     ->inject('project')
     ->inject('request')
     ->action(function (Document $project, Request $request) {
