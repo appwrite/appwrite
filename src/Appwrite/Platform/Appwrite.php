131de2ea7a7fd12098ab747a609d478c229f6c25
Damodar Lohani <dlohani48@gmail.com>
2024-06-06 08:35:50 +0000

    refactor module

--- src/Appwrite/Platform/Appwrite.php
+++ src/Appwrite/Platform/Appwrite.php
@@ -2,8 +2,6 @@
 
 namespace Appwrite\Platform;
 
-use Appwrite\Platform\Services\Tasks;
-use Appwrite\Platform\Services\Workers;
 use Appwrite\Platform\Modules\Core;
 use Utopia\Platform\Platform;
 
@@ -12,7 +10,5 @@ class Appwrite extends Platform
     public function __construct()
     {
         parent::__construct(new Core());
-        $this->addService('tasks', new Tasks());
-        $this->addService('workers', new Workers());
     }
 }
--- src/Appwrite/Platform/Modules/Core.php
+++ src/Appwrite/Platform/Modules/Core.php
@@ -2,8 +2,16 @@
 
 namespace Appwrite\Platform\Modules;
 
+use Appwrite\Platform\Services\Tasks;
+use Appwrite\Platform\Services\Workers;
 use Utopia\Platform\Module;
 
 class Core extends Module
 {
+    public function __construct()
+    {
+        $this->addService('tasks', new Tasks());
+        $this->addService('workers', new Workers());
+    }
+
 }
