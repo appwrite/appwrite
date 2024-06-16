4c8d5d2c89165b9040e69024c8d8b8ab13b7e4a8
Damodar Lohani <dlohani48@gmail.com>
2024-06-16 09:49:19 +0000

    increase file size

--- tests/e2e/Services/Storage/StorageBase.php
+++ tests/e2e/Services/Storage/StorageBase.php
@@ -225,7 +225,7 @@ trait StorageBase
             'bucketId' => ID::unique(),
             'name' => 'Test Bucket 2',
             'fileSecurity' => true,
-            'maximumFileSize' => 200000000, //200MB
+            'maximumFileSize' => 6000000000, //200MB
             'allowedFileExtensions' => ["jpg", "png"],
             'permissions' => [
                 Permission::read(Role::any()),
