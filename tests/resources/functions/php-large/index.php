c7ccc387687f8730ded682a8027dea2312ef43ae
Matej Bačo <matejbaco2000@gmail.com>
2024-07-02 19:08:31 +0000

    Fix tests

--- tests/resources/functions/php-large/index.php
+++ tests/resources/functions/php-large/index.php
@@ -8,7 +8,6 @@ return function ($context) {
         'APPWRITE_FUNCTION_TRIGGER' => $context->req->headers['x-appwrite-trigger'] ?? '',
         'APPWRITE_FUNCTION_RUNTIME_NAME' => \getenv('APPWRITE_FUNCTION_RUNTIME_NAME') ?: '',
         'APPWRITE_FUNCTION_RUNTIME_VERSION' => \getenv('APPWRITE_FUNCTION_RUNTIME_VERSION') ?: '',
-        'APPWRITE_VERSION' => \getenv('APPWRITE_VERSION') ?: '',
         'UNICODE_TEST' => "êä"
     ]);
 };
