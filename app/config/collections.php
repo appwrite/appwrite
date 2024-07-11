6a717f22728d7cda0fd82d0e0eba382026854bde
fogelito <fogelshmuel@gmail.com>
2024-07-11 18:47:03 +0300

    Add resourceId to migration collections.php

--- app/config/collections.php
+++ app/config/collections.php
@@ -4074,6 +4074,17 @@ $projectCollections = array_merge([
                 'filters' => [],
             ],
             [
+                '$id' => ID::custom('resourceId'),
+                'type' => Database::VAR_STRING,
+                'format' => '',
+                'size' => Database::LENGTH_KEY,
+                'signed' => true,
+                'required' => false,
+                'default' => null,
+                'array' => false,
+                'filters' => [],
+            ],
+            [
                 '$id' => ID::custom('statusCounters'),
                 'type' => Database::VAR_STRING,
                 'format' => '',
