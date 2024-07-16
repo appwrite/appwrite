740da0342fa5fd706814757ba4859afa4b399b4a
Bradley Schofield <ionicisere@gmail.com>
2024-07-16 19:41:47 +0900

    Update app/config/runtimes/enum.php
    Co-authored-by: Christy Jacob <christyjacob4@gmail.com>

--- app/config/runtimes/enum.php
+++ app/config/runtimes/enum.php
@@ -4,6 +4,6 @@ enum Sizes: string
 {
     case S_1VCPU_512MB = 's-1vcpu-512mb';
     case S_1VCPU_1GB = 's-1vcpu-1gb';
-    case S_2VCPU_1GB = 's-2vcpu-1gb';
+    case S_2VCPU_2GB = 's-2vcpu-2gb';
     case S_2VCPU_4GB = 's-2vcpu-4gb';
 }
