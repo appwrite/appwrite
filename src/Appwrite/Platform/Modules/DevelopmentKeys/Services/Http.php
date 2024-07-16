af3dc25e2c41afa5519f9287142726096e5c7f60
Damodar Lohani <dlohani48@gmail.com>
2024-07-16 07:48:21 +0000

    add new endpoints to module

--- src/Appwrite/Platform/Modules/DevelopmentKeys/Services/Http.php
+++ src/Appwrite/Platform/Modules/DevelopmentKeys/Services/Http.php
@@ -3,6 +3,10 @@
 namespace Appwrite\Platform\Modules\DevelopmentKeys\Services;
 
 use Appwrite\Platform\Modules\DevelopmentKeys\Http\Create;
+use Appwrite\Platform\Modules\DevelopmentKeys\Http\Update;
+use Appwrite\Platform\Modules\DevelopmentKeys\Http\Get;
+use Appwrite\Platform\Modules\DevelopmentKeys\Http\XList;
+use Appwrite\Platform\Modules\DevelopmentKeys\Http\Delete;
 use Utopia\Platform\Service;
 
 class Http extends Service
@@ -11,5 +15,9 @@ class Http extends Service
     {
         $this->type = Service::TYPE_HTTP;
         $this->addAction(Create::getName(), new Create());
+        $this->addAction(Update::getName(), new Update());
+        $this->addAction(Get::getName(), new Get());
+        $this->addAction(XList::getName(), new XList());
+        $this->addAction(Delete::getName(), new Delete());
     }
 }
