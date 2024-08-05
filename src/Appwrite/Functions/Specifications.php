641553336572ba4b044fcbdcc70f36b50c8486b2
Bradley Schofield <ionicisere@gmail.com>
2024-08-05 18:01:28 +0900

    Update Specifications.php

--- src/Appwrite/Functions/Specifications.php
+++ src/Appwrite/Functions/Specifications.php
@@ -2,9 +2,10 @@
 
 namespace Appwrite\Functions;
 
-class Specifications {
-    static $S_1VCPU_512MB = 's-1vcpu-512mb';
-    static $S_1VCPU_1GB = 's-1vcpu-1gb';
-    static $S_2VCPU_2GB = 's-2vcpu-2gb';
-    static $S_2VCPU_4GB = 's-2vcpu-4gb';
+class Specifications
+{
+    public static $S_1VCPU_512MB = 's-1vcpu-512mb';
+    public static $S_1VCPU_1GB = 's-1vcpu-1gb';
+    public static $S_2VCPU_2GB = 's-2vcpu-2gb';
+    public static $S_2VCPU_4GB = 's-2vcpu-4gb';
 }
