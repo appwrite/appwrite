47316a250b375b04ffee29243d906820e8fef177
Matej Bačo <matejbaco2000@gmail.com>
2024-08-08 09:15:53 +0000

    Edge case for test case

--- src/Appwrite/Utopia/Response/Filters/V18.php
+++ src/Appwrite/Utopia/Response/Filters/V18.php
@@ -24,8 +24,10 @@ class V18 extends Filter
 
     protected function parseExecution(array $content)
     {
-        if($content['status'] === 'completed' && $content['statusCode'] >= 400 && $content['statusCode'] < 500) {
-            $content['status'] = 'failed';
+        if(!empty($content['status']) && !empty($content['statusCode'])) {
+            if($content['status'] === 'completed' && $content['statusCode'] >= 400 && $content['statusCode'] < 500) {
+                $content['status'] = 'failed';
+            }
         }
 
         unset($content['scheduledAt']);
