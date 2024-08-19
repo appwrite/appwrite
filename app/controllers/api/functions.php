12b6351060337573d6f1006333cfc0d1821a47e7
Matej Bačo <matejbaco2000@gmail.com>
2024-08-19 11:06:58 +0000

    Separate deployment and build sizes

--- app/controllers/api/functions.php
+++ app/controllers/api/functions.php
@@ -1361,7 +1361,8 @@ App::get('/v1/functions/:functionId/deployments')
             $result->setAttribute('status', $build->getAttribute('status', 'processing'));
             $result->setAttribute('buildLogs', $build->getAttribute('logs', ''));
             $result->setAttribute('buildTime', $build->getAttribute('duration', 0));
-            $result->setAttribute('size', $result->getAttribute('size', 0) + $build->getAttribute('size', 0));
+            $result->setAttribute('buildSize', $build->getAttribute('size', 0));
+            $result->setAttribute('size', $result->getAttribute('size', 0));
         }
 
         $response->dynamic(new Document([
@@ -1407,7 +1408,8 @@ App::get('/v1/functions/:functionId/deployments/:deploymentId')
         $deployment->setAttribute('status', $build->getAttribute('status', 'waiting'));
         $deployment->setAttribute('buildLogs', $build->getAttribute('logs', ''));
         $deployment->setAttribute('buildTime', $build->getAttribute('duration', 0));
-        $deployment->setAttribute('size', $deployment->getAttribute('size', 0) + $build->getAttribute('size', 0));
+        $deployment->setAttribute('buildSize', $build->getAttribute('size', 0));
+        $deployment->setAttribute('size', $deployment->getAttribute('size', 0));
 
         $response->dynamic($deployment, Response::MODEL_DEPLOYMENT);
     });
