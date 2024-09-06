f20ed6c66edeee05ff32428ee4e8da3b15902dc1
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2024-09-07 01:10:23 +0300

    feat: add `populatePayload` method to the `Service` class in the sdk-console

--- public/sdk-console/service.ts
+++ public/sdk-console/service.ts
@@ -27,4 +27,40 @@ export class Service {
 
         return output;
     }
+
+    /**
+     * Populates a payload object with key-value pairs
+     * 
+     * Use this function to populate a payload object with key-value pairs from another object.
+     * This function performs a shallow copy of all defined key-value pairs from the `values` object into the `payload`.
+     * This function directly modifies the original `payload` object, making it more efficient for large-scale applications.
+     * 
+     * @param {Payload} payload - The payload object to populate
+     * @param {Record<string, any>} values - The key-value pairs to populate the payload with
+     * 
+     * @example
+     * const payload: Payload = {};
+     * const values = { key1: 'value1', key2: 'value2' };
+     * 
+     * Service.populatePayload(payload, values);
+     * console.log(payload); // { key1: 'value1', key2: 'value2' }
+     * 
+     * @throws {TypeError} - If the payload is not an object
+     * @throws {TypeError} - If the values are not an object
+     */
+    static populatePayload(payload: Payload, values: Record<string, any>): void {
+        if (typeof payload !== 'object') {
+            throw new TypeError('Payload must be an object');
+        }
+
+        if (typeof values !== 'object') {
+            throw new TypeError('Values must be an object');
+        }
+
+        for (const key in values) {
+            if(typeof values[key] !== 'undefined') {
+                payload[key] = values[key];
+            }
+        }
+    }
 }
