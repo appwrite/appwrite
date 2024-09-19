d6d7cf1d9fbaec70274b2dbcb6b143cbd7061730
fogelito <fogelshmuel@gmail.com>
2024-09-19 18:27:02 +0300

    Pull 1.6.x

--- src/Appwrite/Platform/Tasks/ScheduleBase.php
+++ src/Appwrite/Platform/Tasks/ScheduleBase.php
@@ -151,7 +151,7 @@ abstract class ScheduleBase extends Action
                     $total = $total + $sum;
 
                     foreach ($results as $document) {
-                        $localDocument = $schedules[$document['resourceId']] ?? null;
+                        $localDocument = $this->schedules[$document['resourceId']] ?? null;
 
                         // Check if resource has been updated since last sync
                         $org = $localDocument !== null ? \strtotime($localDocument['resourceUpdatedAt']) : null;
