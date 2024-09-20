df9e80a9629c638a276f96717096e406b71f98c2
loks0n <22452787+loks0n@users.noreply.github.com>
2024-09-20 17:02:23 +0100

    feat: handle bools

--- tests/e2e/Client.php
+++ tests/e2e/Client.php
@@ -303,6 +303,8 @@ class Client
 
             if (is_array($value)) {
                 $output += $this->flatten($value, $finalKey); // @todo: handle name collision here if needed
+            } elseif (is_bool($value)) {
+                $output[$finalKey] = $value ? 'true' : 'false';
             } else {
                 $output[$finalKey] = $value;
             }
