8f4594babb3e6381cf03a54544246be89df2d83b
loks0n <22452787+loks0n@users.noreply.github.com>
2024-10-09 14:12:46 +0100

    debug: ci docker logs

--- tests/e2e/Services/Functions/FunctionsCustomClientTest.php
+++ tests/e2e/Services/Functions/FunctionsCustomClientTest.php
@@ -250,28 +250,21 @@ class FunctionsCustomClientTest extends Scope
             'activate' => true
         ]);
 
-        try {
-            $execution = $this->client->call(Client::METHOD_POST, '/functions/' . $functionId . '/executions', array_merge([
-                'content-type' => 'application/json',
-                'x-appwrite-project' => $this->getProject()['$id'],
-            ], $this->getHeaders()), [
-                'x-appwrite-event' => "OVERRIDDEN",
-                'x-appwrite-trigger' => "OVERRIDDEN",
-                'x-appwrite-user-id' => "OVERRIDDEN",
-                'x-appwrite-user-jwt' => "OVERRIDDEN",
-            ]);
-    
-            $output = json_decode($execution['body']['responseBody'], true);
-            $this->assertNotEquals('OVERRIDDEN', $output['APPWRITE_FUNCTION_JWT']);
-            $this->assertNotEquals('OVERRIDDEN', $output['APPWRITE_FUNCTION_EVENT']);
-            $this->assertNotEquals('OVERRIDDEN', $output['APPWRITE_FUNCTION_TRIGGER']);
-            $this->assertNotEquals('OVERRIDDEN', $output['APPWRITE_FUNCTION_USER_ID']);
-        } catch (\Exception $e) {
-            // output docker logs
-            $out = '';
-            Console::execute('docker compose logs appwrite', '', $out, $out);
-            fwrite(STDOUT, print_r($out, true));
-        }
+        $execution = $this->client->call(Client::METHOD_POST, '/functions/' . $functionId . '/executions', array_merge([
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+        ], $this->getHeaders()), [
+            'x-appwrite-event' => "OVERRIDDEN",
+            'x-appwrite-trigger' => "OVERRIDDEN",
+            'x-appwrite-user-id' => "OVERRIDDEN",
+            'x-appwrite-user-jwt' => "OVERRIDDEN",
+        ]);
+
+        $output = json_decode($execution['body']['responseBody'], true);
+        $this->assertNotEquals('OVERRIDDEN', $output['APPWRITE_FUNCTION_JWT']);
+        $this->assertNotEquals('OVERRIDDEN', $output['APPWRITE_FUNCTION_EVENT']);
+        $this->assertNotEquals('OVERRIDDEN', $output['APPWRITE_FUNCTION_TRIGGER']);
+        $this->assertNotEquals('OVERRIDDEN', $output['APPWRITE_FUNCTION_USER_ID']);
 
 
         $this->cleanupFunction($functionId);
