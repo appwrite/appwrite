9793cf4ece402c98ec36341d79f3166b3d3841fa
Khushboo Verma <43381712+vermakhushboo@users.noreply.github.com>
2024-10-23 17:07:28 +0200

    Add all deployment endpoints for sites

---
+++ src/Appwrite/Platform/Modules/Sites/Http/Deployments/RebuildDeployment.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Appwrite\Platform\Modules\Sites\Http\Deployments;
+
+use Appwrite\Event\Build;
+use Appwrite\Event\Event;
+use Appwrite\Extend\Exception;
+use Appwrite\Utopia\Response;
+use Utopia\Database\Database;
+use Utopia\Database\Helpers\ID;
+use Utopia\Database\Validator\UID;
+use Utopia\Platform\Action;
+use Utopia\Platform\Scope\HTTP;
+use Utopia\Storage\Device;
+
+class RebuildDeployment extends Action
+{
+    use HTTP;
+
+    public static function getName()
+    {
+        return 'rebuildDeployment';
+    }
+
+    public function __construct()
+    {
+        $this
+            ->setHttpMethod(Action::HTTP_REQUEST_METHOD_POST)
+            ->setHttpPath('/v1/sites/:siteId/deployments/:deploymentId/build')
+            ->desc('Rebuild deployment')
+            ->groups(['api', 'sites'])
+            ->label('scope', 'functions.write') //TODO: Update the scope to sites later
+            ->label('event', 'sites.[siteId].deployments.[deploymentId].update')
+            ->label('audits.event', 'deployment.update')
+            ->label('audits.resource', 'site/{request.siteId}')
+            ->label('sdk.auth', [APP_AUTH_TYPE_KEY])
+            ->label('sdk.namespace', 'sites')
+            ->label('sdk.method', 'createBuild')
+            ->label('sdk.response.code', Response::STATUS_CODE_NOCONTENT)
+            ->label('sdk.response.model', Response::MODEL_NONE)
+            ->param('siteId', '', new UID(), 'Site ID.')
+            ->param('deploymentId', '', new UID(), 'Deployment ID.')
+            ->inject('response')
+            ->inject('dbForProject')
+            ->inject('queueForEvents')
+            ->inject('queueForBuilds')
+            ->inject('deviceForSites')
+            ->inject('deviceForFunctions') //TODO: remove it later
+            ->callback([$this, 'action']);
+    }
+
+    public function action(string $siteId, string $deploymentId, Response $response, Database $dbForProject, Event $queueForEvents, Build $queueForBuilds, Device $deviceForSites, Device $deviceForFunctions)
+    {
+        $site = $dbForProject->getDocument('sites', $siteId);
+
+        if ($site->isEmpty()) {
+            throw new Exception(Exception::SITE_NOT_FOUND);
+        }
+        $deployment = $dbForProject->getDocument('deployments', $deploymentId);
+
+        if ($deployment->isEmpty()) {
+            throw new Exception(Exception::DEPLOYMENT_NOT_FOUND);
+        }
+
+        $path = $deployment->getAttribute('path');
+        if (empty($path) || !$deviceForFunctions->exists($path)) {
+            throw new Exception(Exception::DEPLOYMENT_NOT_FOUND);
+        }
+
+        $deploymentId = ID::unique();
+
+        $destination = $deviceForFunctions->getPath($deploymentId . '.' . \pathinfo('code.tar.gz', PATHINFO_EXTENSION));
+        $deviceForFunctions->transfer($path, $destination, $deviceForFunctions);
+
+        $deployment->removeAttribute('$internalId');
+        $deployment = $dbForProject->createDocument('deployments', $deployment->setAttributes([
+            '$internalId' => '',
+            '$id' => $deploymentId,
+            'buildId' => '',
+            'buildInternalId' => '',
+            'path' => $destination,
+            'buildCommand' => $site->getAttribute('buildCommand', ''),
+            'installCommand' => $site->getAttribute('installCommand', ''),
+            'outputDirectory' => $site->getAttribute('outputDirectory', ''),
+            'search' => implode(' ', [$deploymentId]),
+        ]));
+
+        $queueForBuilds
+            ->setType(BUILD_TYPE_DEPLOYMENT)
+            ->setResource($site)
+            ->setDeployment($deployment);
+
+        $queueForEvents
+            ->setParam('siteId', $site->getId())
+            ->setParam('deploymentId', $deployment->getId());
+
+        $response->noContent();
+    }
+}
--- src/Appwrite/Platform/Modules/Sites/Services/Http.php
+++ src/Appwrite/Platform/Modules/Sites/Services/Http.php
@@ -2,7 +2,14 @@
 
 namespace Appwrite\Platform\Modules\Sites\Services;
 
+use Appwrite\Platform\Modules\Sites\Http\Deployments\CancelDeployment;
 use Appwrite\Platform\Modules\Sites\Http\Deployments\CreateDeployment;
+use Appwrite\Platform\Modules\Sites\Http\Deployments\DeleteDeployment;
+use Appwrite\Platform\Modules\Sites\Http\Deployments\DownloadDeployment;
+use Appwrite\Platform\Modules\Sites\Http\Deployments\GetDeployment;
+use Appwrite\Platform\Modules\Sites\Http\Deployments\ListDeployments;
+use Appwrite\Platform\Modules\Sites\Http\Deployments\RebuildDeployment;
+use Appwrite\Platform\Modules\Sites\Http\Deployments\UpdateDeployment;
 use Appwrite\Platform\Modules\Sites\Http\Sites\CreateSite;
 use Appwrite\Platform\Modules\Sites\Http\Sites\GetSite;
 use Appwrite\Platform\Modules\Sites\Http\Sites\ListFrameworks;
@@ -21,5 +28,12 @@ class Http extends Service
         $this->addAction(UpdateSite::getName(), new UpdateSite());
         $this->addAction(ListFrameworks::getName(), new ListFrameworks());
         $this->addAction(CreateDeployment::getName(), new CreateDeployment());
+        $this->addAction(GetDeployment::getName(), new GetDeployment());
+        $this->addAction(ListDeployments::getName(), new ListDeployments());
+        $this->addAction(UpdateDeployment::getName(), new UpdateDeployment());
+        $this->addAction(DeleteDeployment::getName(), new DeleteDeployment());
+        $this->addAction(DownloadDeployment::getName(), new DownloadDeployment());
+        $this->addAction(RebuildDeployment::getName(), new RebuildDeployment());
+        $this->addAction(CancelDeployment::getName(), new CancelDeployment());
     }
 }
