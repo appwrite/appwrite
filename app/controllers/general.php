03d4d4c8fab2c3e0a053680abc154eb01c07a084
Khushboo Verma <43381712+vermakhushboo@users.noreply.github.com>
2024-10-26 12:29:42 +0200

    Add build env vars for sites

--- app/controllers/general.php
+++ app/controllers/general.php
@@ -331,8 +331,7 @@ function router(App $utopia, Database $dbForConsole, callable $getProjectDB, Swo
             };
             $entrypoint = match($type) {
                 'function' => $deployment->getAttribute('entrypoint', ''),
-                //todo: check if null works
-                'site' => 'placeholder' // entrypoint is required in api, but not needed with site
+                'site' => ''
             };
             $runtimeEntrypoint = match ($version) {
                 'v2' => '',
@@ -344,7 +343,6 @@ function router(App $utopia, Database $dbForConsole, callable $getProjectDB, Swo
                 deploymentId: $deployment->getId(),
                 body: \strlen($body) > 0 ? $body : null,
                 variables: $vars,
-                // todo: figure out timeouts for sites
                 timeout: $resource->getAttribute('timeout', 30),
                 image: $runtime['image'],
                 source: $build->getAttribute('path', ''),
