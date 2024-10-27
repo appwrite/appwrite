dd87cdbebfef58710c55cb8cef50713eeb943c55
Khushboo Verma <43381712+vermakhushboo@users.noreply.github.com>
2024-10-27 18:04:40 +0100

    Fix remaining vars

--- src/Appwrite/Platform/Modules/Functions/Workers/Builds.php
+++ src/Appwrite/Platform/Modules/Functions/Workers/Builds.php
@@ -526,8 +526,8 @@ class Builds extends Action
                         'APPWRITE_FUNCTION_PROJECT_ID' => $project->getId(),
                         'APPWRITE_FUNCTION_RUNTIME_NAME' => $runtime['name'] ?? '',
                         'APPWRITE_FUNCTION_RUNTIME_VERSION' => $runtime['version'] ?? '',
-                        'APPWRITE_FUNCTION_CPUS' => $cpus,
-                        'APPWRITE_FUNCTION_MEMORY' => $memory
+                        'APPWRITE_COMPUTE_CPUS' => $cpus,
+                        'APPWRITE_COMPUTE_MEMORY' => $memory
                     ];
                     break;
                 case 'sites':
@@ -539,8 +539,8 @@ class Builds extends Action
                         'APPWRITE_SITE_PROJECT_ID' => $project->getId(),
                         'APPWRITE_SITE_RUNTIME_NAME' => $runtime['name'] ?? '',
                         'APPWRITE_SITE_RUNTIME_VERSION' => $runtime['version'] ?? '',
-                        'APPWRITE_SITE_CPUS' => $cpus,
-                        'APPWRITE_SITE_MEMORY' => $memory
+                        'APPWRITE_COMPUTE_CPUS' => $cpus,
+                        'APPWRITE_COMPUTE_MEMORY' => $memory
                     ];
                     break;
             }
