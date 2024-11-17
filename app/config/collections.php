cb359e40bac65e678fce60ba55715843fba6cd14
Khushboo Verma <43381712+vermakhushboo@users.noreply.github.com>
2024-11-18 00:23:45 +0530

    Add fallbackFile for SPA

--- app/config/collections.php
+++ app/config/collections.php
@@ -3337,6 +3337,17 @@ $projectCollections = array_merge([
                 'filters' => [],
             ],
             [
+                '$id' => ID::custom('fallbackFile'),
+                'type' => Database::VAR_STRING,
+                'format' => '',
+                'size' => Database::LENGTH_KEY,
+                'signed' => true,
+                'required' => false,
+                'default' => null,
+                'array' => false,
+                'filters' => [],
+            ],
+            [
                 '$id' => ID::custom('deploymentInternalId'),
                 'type' => Database::VAR_STRING,
                 'format' => '',
