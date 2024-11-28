104a9b60740b4c8ab155c5ac927d6702eb658290
Bradley Schofield <ionicisere@gmail.com>
2024-11-28 16:29:22 +0900

    Remove realtime support for bulk operations

--- app/controllers/api/databases.php
+++ app/controllers/api/databases.php
@@ -2865,7 +2865,7 @@ App::post('/v1/databases/:databaseId/collections/:collectionId/documents')
     ->param('documentId', '', new CustomId(), 'Document ID. Choose a custom ID or generate a random ID with `ID.unique()`. Valid chars are a-z, A-Z, 0-9, period, hyphen, and underscore. Can\'t start with a special char. Max length is 36 chars.', true)
     ->param('collectionId', '', new UID(), 'Collection ID. You can create a new collection using the Database service [server integration](https://appwrite.io/docs/server/databases#databasesCreateCollection). Make sure to define attributes before creating documents.')
     ->param('data', [], new JSON(), 'Document data as JSON object.', true)
-    ->param('documents', [], new ArrayList(new JSON(), APP_LIMIT_ARRAY_PARAMS_SIZE), 'Array of documents data as JSON object.', true)
+    ->param('documents', [], new ArrayList(new JSON(), APP_LIMIT_ARRAY_DOCUMENTS_SIZE), 'Array of documents data as JSON object.', true)
     ->param('permissions', null, new Permissions(APP_LIMIT_ARRAY_PARAMS_SIZE, [Database::PERMISSION_READ, Database::PERMISSION_UPDATE, Database::PERMISSION_DELETE, Database::PERMISSION_WRITE]), 'An array of permissions strings. By default, only the current user is granted all permissions. [Learn more about permissions](https://appwrite.io/docs/permissions).', true)
     ->inject('response')
     ->inject('dbForProject')
@@ -3072,7 +3072,7 @@ App::post('/v1/databases/:databaseId/collections/:collectionId/documents')
             ->setContext('database', $database);
 
         // Add $collectionId and $databaseId for all documents
-        $processDocument = function (Document $collection, Document &$document) use (&$processDocument, $dbForProject, $database, $queueForEvents) {
+        $processDocument = function (Document $collection, Document &$document) use (&$processDocument, $dbForProject, $database) {
             $document->removeAttribute('$collection');
             $document->setAttribute('$databaseId', $database->getId());
             $document->setAttribute('$collectionId', $collection->getId());
@@ -3107,33 +3107,20 @@ App::post('/v1/databases/:databaseId/collections/:collectionId/documents')
 
         foreach ($documents as $document) {
             $processDocument($collection, $document);
-
-            $queueForEvents
-                ->setProject($project)
-                ->setEvent('databases.[databaseId].collections.[collectionId].documents.[documentId].create')
-                ->setParam('documentId', $document->getId())
-                ->setPayload($response->output($document, Response::MODEL_DOCUMENT))
-                ->trigger();
-
-            if ($project->getId() !== 'console') {
-                $queueForRealtime
-                    ->from($queueForEvents)
-                    ->trigger();
-            }
         }
-
-        // We don't want to double trigger events
-        $queueForEvents->setEvent('');
-        $queueForRealtime->setEvent('');
 
         if ($isBulk) {
             $response
                 ->setStatusCode(Response::STATUS_CODE_CREATED)
                 ->dynamic(new Document([
                     'total' => count($documents),
-                    'documents' => $documents,
+                    'documents' => $documents
                 ]), Response::MODEL_DOCUMENT_LIST);
         } else {
+            $queueForEvents
+                ->setParam('documentId', $document->getId())
+                ->setEvent('databases.[databaseId].collections.[collectionId].documents.[documentId].create');
+
             $response
                 ->setStatusCode(Response::STATUS_CODE_CREATED)
                 ->dynamic($documents[0], Response::MODEL_DOCUMENT);
---
+++ tests/benchmarks/bulk-operations.js
@@ -0,0 +1,32 @@
+import { check } from "k6";
+import http from "k6/http";
+
+const amount = 10000;
+const databaseId = "674818e2000070c94275";
+const collectionId = "674818e3002f22da5fab";
+
+const documents = Array(amount).fill({
+    $id: "unique()",
+    name: "asd",
+});
+
+export default function () {
+    const payload = JSON.stringify({
+        documents,
+    });
+
+    const res = http.post(`http://localhost/v1/databases/${databaseId}/collections/${collectionId}/documents`,
+        payload,
+        {
+            headers: {
+                "X-Appwrite-Key": "standard_fa89c4834660f39e95ca2c2996fe7dd4ff498725e37c09323234c009570c1719f1c10610bf3541cf9ead120c107e41397a4eae1c787c83bdf577857bbc5963341641c77f582cc41e11a0d50eb4c2e4b1fda74418a8b9a253d6e63008e33560ba35310b9dc2fed5f09ca599e646f744cc6308b8ccd27ff04f9e498ec5a5f2c3db",
+                "X-Appwrite-Project": "674818bc0017934d58dd",
+                "Content-Type": "application/json",
+            },
+        }
+    );
+
+    check(res, {
+        "status is 200": (r) => r.status === 201,
+    });
+}
