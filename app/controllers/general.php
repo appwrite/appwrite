8a59a22c2e0a25af066833a830198dda95c1676d
Damodar Lohani <dlohani48@gmail.com>
2024-12-04 05:25:16 +0000

    update suggested changes

--- app/controllers/general.php
+++ app/controllers/general.php
@@ -672,7 +672,7 @@ App::init()
             ->addHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
             ->addHeader('Access-Control-Allow-Headers', 'Origin, Cookie, Set-Cookie, X-Requested-With, Content-Type, Access-Control-Allow-Origin, Access-Control-Request-Headers, Accept, X-Appwrite-Project, X-Appwrite-Key, X-Appwrite-Locale, X-Appwrite-Mode, X-Appwrite-JWT, X-Appwrite-Response-Format, X-Appwrite-Timeout, X-SDK-Version, X-SDK-Name, X-SDK-Language, X-SDK-Platform, X-SDK-GraphQL, X-Appwrite-ID, X-Appwrite-Timestamp, Content-Range, Range, Cache-Control, Expires, Pragma, X-Forwarded-For, X-Forwarded-User-Agent')
             ->addHeader('Access-Control-Expose-Headers', 'X-Appwrite-Session, X-Fallback-Cookies')
-            ->addHeader('Access-Control-Allow-Origin', $refDomain)
+            ->addHeader('Access-Control-Allow-Origin', $devKey->isEmpty() ? $refDomain : "*")
             ->addHeader('Access-Control-Allow-Credentials', 'true');
 
         /*
