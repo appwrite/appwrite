deab7fad55004519f93533c6815c5bd80586f656
Darshan <itznotabug@gmail.com>
2024-12-27 14:55:49 +0530

    update: add `logs` db for consistency.

--- app/init.php
+++ app/init.php
@@ -841,6 +841,12 @@ $register->set('pools', function () {
             'multiple' => true,
             'schemes' => ['mariadb', 'mysql'],
         ],
+        'logs' => [
+            'type' => 'database',
+            'dsns' => System::getEnv('_APP_CONNECTIONS_DB_LOGS', $fallbackForDB),
+            'multiple' => true,
+            'schemes' => ['mariadb', 'mysql'],
+        ],
         'queue' => [
             'type' => 'queue',
             'dsns' => System::getEnv('_APP_CONNECTIONS_QUEUE', $fallbackForRedis),
