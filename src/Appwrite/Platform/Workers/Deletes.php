f52b2d5dcde55de4928740371e6d39b5dce0262a
Christy Jacob <christyjacob4@gmail.com>
2025-01-16 16:59:30 +0530

    chore: review comments

--- src/Appwrite/Platform/Workers/Deletes.php
+++ src/Appwrite/Platform/Workers/Deletes.php
@@ -154,20 +154,15 @@ class Deletes extends Action
                 $this->deleteSessionTargets($project, $getProjectDB, $document);
                 break;
             case DELETE_TYPE_MAINTENANCE:
-                $this->performMaintenance($project, $getProjectDB, $executionRetention, $auditRetention, $hourlyUsageRetentionDatetime);
+                $this->deleteExpiredTargets($project, $getProjectDB);
+                $this->deleteExecutionLogs($project, $getProjectDB, $executionRetention);
+                $this->deleteAuditLogs($project, $getProjectDB, $auditRetention);
+                $this->deleteUsageStats($project, $getProjectDB, $hourlyUsageRetentionDatetime);
+                $this->deleteExpiredSessions($project, $getProjectDB);
                 break;
             default:
                 throw new \Exception('No delete operation for type: ' . \strval($type));
         }
-    }
-
-    private function performMaintenance(Document $project, callable $getProjectDB, string $executionRetention, string $auditRetention, string $hourlyUsageRetentionDatetime): void
-    {
-        $this->deleteExpiredTargets($project, $getProjectDB);
-        $this->deleteExecutionLogs($project, $getProjectDB, $executionRetention);
-        $this->deleteAuditLogs($project, $getProjectDB, $auditRetention);
-        $this->deleteUsageStats($project, $getProjectDB, $hourlyUsageRetentionDatetime);
-        $this->deleteExpiredSessions($project, $getProjectDB);
     }
 
     /**
