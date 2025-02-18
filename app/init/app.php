e5f41958ed20c84fe16a3b345d09c6ef3d61de47
Damodar Lohani <dlohani48@gmail.com>
2025-02-18 02:59:57 +0000

    fix path

--- app/init/app.php
+++ app/init/app.php
@@ -8,8 +8,8 @@
  *
  */
 
-if (\file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    require_once __DIR__ . '/../vendor/autoload.php';
+if (\file_exists(__DIR__ . '/../../vendor/autoload.php')) {
+    require_once __DIR__ . '/../../vendor/autoload.php';
 }
 
 \ini_set('memory_limit', '512M');
