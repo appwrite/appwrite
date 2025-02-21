d6c2dfb9157f845828bc5cc60cccccb3385d4b3a
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2025-02-21 03:27:46 +0000

    chore: updated name

--- app/controllers/api/teams.php
+++ app/controllers/api/teams.php
@@ -715,7 +715,7 @@ App::post('/v1/teams/:teamId/memberships')
                     ->setSubject($subject)
                     ->setBody($body)
                     ->setRecipient($invitee->getAttribute('email'))
-                    ->setName($name)
+                    ->setName($invitee->getAttribute('name', ''))
                     ->setVariables($emailVariables)
                     ->trigger();
 
@@ -781,7 +781,7 @@ App::post('/v1/teams/:teamId/memberships')
             ->dynamic(
                 $membership
                     ->setAttribute('teamName', $team->getAttribute('name'))
-                    ->setAttribute('userName', $name)
+                    ->setAttribute('userName', $invitee->getAttribute('name'))
                     ->setAttribute('userEmail', $invitee->getAttribute('email')),
                 Response::MODEL_MEMBERSHIP
             );
