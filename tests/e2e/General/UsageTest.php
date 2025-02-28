02de0d2432750728768fe6efb0a81cd21eff54d9
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2025-02-28 13:29:32 +0000

    chore: fix stats count

--- tests/e2e/General/UsageTest.php
+++ tests/e2e/General/UsageTest.php
@@ -331,7 +331,7 @@ class UsageTest extends Scope
             ]
         );
 
-        $this->assertEquals(29, count($response['body']));
+        $this->assertEquals(30, count($response['body']));
         $this->assertEquals(1, count($response['body']['requests']));
         $this->assertEquals($requestsTotal, $response['body']['requests'][array_key_last($response['body']['requests'])]['value']);
         $this->validateDates($response['body']['requests']);
@@ -552,7 +552,7 @@ class UsageTest extends Scope
             ]
         );
 
-        $this->assertEquals(29, count($response['body']));
+        $this->assertEquals(30, count($response['body']));
         $this->assertEquals(1, count($response['body']['requests']));
         $this->assertEquals(1, count($response['body']['network']));
         $this->assertEquals($requestsTotal, $response['body']['requests'][array_key_last($response['body']['requests'])]['value']);
