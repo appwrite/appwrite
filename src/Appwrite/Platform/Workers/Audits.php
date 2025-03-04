1838b9c4e879f2eca2fd775dcd32df9821e35db5
shimon <shimon@appwrite.io>
2025-03-04 19:49:10 +0200

    updating audits workers var scope

--- src/Appwrite/Platform/Workers/Audits.php
+++ src/Appwrite/Platform/Workers/Audits.php
@@ -17,9 +17,9 @@ use Utopia\System\System;
 
 class Audits extends Action
 {
-    protected const BATCH_SIZE_DEVELOPMENT = 2; // smaller batch size for development
+    protected const BATCH_SIZE_DEVELOPMENT = 26; // smaller batch size for development
     protected const BATCH_SIZE_PRODUCTION = 5_000;
-    protected const BATCH_AGGREGATION_INTERVAL = 60; // in seconds
+    protected const BATCH_AGGREGATION_INTERVAL = 300; // in seconds
 
     private int $lastTriggeredTime = 0;
 
