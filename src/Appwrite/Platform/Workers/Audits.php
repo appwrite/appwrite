57d7a09f66580d384750a18f94a0820f05317e63
Jake Barnby <jakeb994@gmail.com>
2025-03-25 19:11:35 +1300

    Fix attribute mismatch

--- src/Appwrite/Platform/Workers/Audits.php
+++ src/Appwrite/Platform/Workers/Audits.php
@@ -103,7 +103,7 @@ class Audits extends Action
                 'mode' => $mode,
                 'data' => $auditPayload,
             ],
-            'timestamp' => date("Y-m-d H:i:s", $message->getTimestamp()),
+            'time' => \date("Y-m-d H:i:s", $message->getTimestamp()),
         ]);
 
         if (isset($this->logs[$project->getInternalId()])) {
