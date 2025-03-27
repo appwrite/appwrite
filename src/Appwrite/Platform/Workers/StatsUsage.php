05067302d2d61f49d92095e48e8ed31178e1805a
Damodar Lohani <dlohani48@gmail.com>
2025-03-27 08:25:48 +0000

    Fix: merge the working of StatsUsage and StatsUsageDump

--- src/Appwrite/Platform/Workers/StatsUsage.php
+++ src/Appwrite/Platform/Workers/StatsUsage.php
@@ -2,23 +2,98 @@
 
 namespace Appwrite\Platform\Workers;
 
-use Appwrite\Event\StatsUsageDump;
 use Exception;
+use Throwable;
 use Utopia\CLI\Console;
 use Utopia\Database\DateTime;
 use Utopia\Database\Document;
 use Utopia\Platform\Action;
 use Utopia\Queue\Message;
+use Utopia\Registry\Registry;
 use Utopia\System\System;
 
 class StatsUsage extends Action
 {
+    /**
+     * In memory per project metrics calculation
+     */
     private array $stats = [];
     private int $lastTriggeredTime = 0;
     private int $keys = 0;
     private const INFINITY_PERIOD = '_inf_';
     private const BATCH_SIZE_DEVELOPMENT = 1;
     private const BATCH_SIZE_PRODUCTION = 10_000;
+
+    /**
+    * Stats for batch write separated per project
+    * @var array
+    */
+    private array $projects = [];
+
+    /**
+     * Array of stat documents to batch write to logsDB
+     * @var array
+     */
+    private array $statDocuments = [];
+
+    protected Registry $register;
+
+    /**
+     * Metrics to skip writing to logsDB
+     * As these metrics are calculated separately
+     * by logs DB
+     * @var array
+     */
+    protected array $skipBaseMetrics = [
+        METRIC_DATABASES => true,
+        METRIC_BUCKETS => true,
+        METRIC_USERS => true,
+        METRIC_FUNCTIONS => true,
+        METRIC_TEAMS => true,
+        METRIC_MESSAGES => true,
+        METRIC_MAU => true,
+        METRIC_WEBHOOKS => true,
+        METRIC_PLATFORMS => true,
+        METRIC_PROVIDERS => true,
+        METRIC_TOPICS => true,
+        METRIC_KEYS => true,
+        METRIC_FILES => true,
+        METRIC_FILES_STORAGE => true,
+        METRIC_DEPLOYMENTS_STORAGE => true,
+        METRIC_BUILDS_STORAGE => true,
+        METRIC_DEPLOYMENTS => true,
+        METRIC_BUILDS => true,
+        METRIC_COLLECTIONS => true,
+        METRIC_DOCUMENTS => true,
+        METRIC_DATABASES_STORAGE => true,
+    ];
+
+    /**
+     * Skip metrics associated with parent IDs
+     * these need to be checked individually with `str_ends_with`
+     */
+    protected array $skipParentIdMetrics = [
+        '.files',
+        '.files.storage',
+        '.collections',
+        '.documents',
+        '.deployments',
+        '.deployments.storage',
+        '.builds',
+        '.builds.storage',
+        '.databases.storage'
+    ];
+
+    /**
+     * @var callable
+     */
+    protected mixed $getLogsDB;
+
+    protected array $periods = [
+        '1h' => 'Y-m-d H:00',
+        '1d' => 'Y-m-d 00:00',
+        'inf' => '0000-00-00 00:00'
+    ];
 
     public static function getName(): string
     {
@@ -41,7 +116,8 @@ class StatsUsage extends Action
             ->desc('Stats usage worker')
             ->inject('message')
             ->inject('getProjectDB')
-            ->inject('queueForStatsUsageDump')
+            ->inject('getLogsDB')
+            ->inject('register')
             ->callback([$this, 'action']);
 
         $this->lastTriggeredTime = time();
@@ -50,13 +126,16 @@ class StatsUsage extends Action
     /**
      * @param Message $message
      * @param callable $getProjectDB
-     * @param StatsUsageDump $queueForStatsUsageDump
+     * @param callable $getLogsDB
+     * @param Registry $register
      * @return void
      * @throws \Utopia\Database\Exception
      * @throws Exception
      */
-    public function action(Message $message, callable $getProjectDB, StatsUsageDump $queueForStatsUsageDump): void
+    public function action(Message $message, callable $getProjectDB, callable $getLogsDB, Registry $register): void
     {
+        $this->getLogsDB = $getLogsDB;
+        $this->register = $register;
         $payload = $message->getPayload() ?? [];
         if (empty($payload)) {
             throw new Exception('Missing payload');
@@ -98,9 +177,7 @@ class StatsUsage extends Action
         ) {
             Console::warning('[' . DateTime::now() . '] Aggregated ' . $this->keys . ' keys');
 
-            $queueForStatsUsageDump
-                ->setStats($this->stats)
-                ->trigger();
+            $this->commitToDB($getProjectDB);
 
             $this->stats = [];
             $this->keys = 0;
@@ -249,5 +326,128 @@ class StatsUsage extends Action
         } catch (\Throwable $e) {
             console::error("[reducer] " . " {DateTime::now()} " . " {$project->getInternalId()} " . " {$e->getMessage()}");
         }
+    }
+
+    public function commitToDb(callable $getProjectDB): void
+    {
+
+        foreach ($this->stats as $stats) {
+            $project = new Document($stats['project'] ?? []);
+            $numberOfKeys = !empty($stats['keys']) ? count($stats['keys']) : 0;
+            $receivedAt = $stats['receivedAt'] ?? null;
+            if ($numberOfKeys === 0) {
+                continue;
+            }
+
+            console::log('['.DateTime::now().'] Id: '.$project->getId(). ' InternalId: '.$project->getInternalId(). ' Db: '.$project->getAttribute('database').' ReceivedAt: '.$receivedAt. ' Keys: '.$numberOfKeys);
+
+            try {
+                foreach ($stats['keys'] ?? [] as $key => $value) {
+                    if ($value == 0) {
+                        continue;
+                    }
+
+                    if (str_contains($key, METRIC_DATABASES_STORAGE)) {
+                        // skip database storage calc as it's wrong and we plan to get this from StatsResources
+                        continue;
+                    }
+
+                    foreach ($this->periods as $period => $format) {
+                        $time = null;
+
+                        if ($period !== 'inf') {
+                            $time = !empty($receivedAt) ? (new \DateTime($receivedAt))->format($format) : date($format, time());
+                        }
+                        $id = \md5("{$time}_{$period}_{$key}");
+
+                        $document = new Document([
+                            '$id' => $id,
+                            'period' => $period,
+                            'time' => $time,
+                            'metric' => $key,
+                            'value' => $value,
+                            'region' => System::getEnv('_APP_REGION', 'default'),
+                        ]);
+
+
+                        $this->projects[$project->getInternalId()]['project']  = new Document([
+                            '$id' => $project->getId(),
+                            '$internalId' => $project->getInternalId(),
+                            'database' => $project->getAttribute('database'),
+                        ]);
+                        $this->projects[$project->getInternalId()]['stats'][] = $document;
+
+                        $this->prepareForLogsDB($project, $document);
+                    }
+                }
+            } catch (Exception $e) {
+                console::error('[' . DateTime::now() . '] project [' . $project->getInternalId() . '] database [' . $project['database'] . '] ' . ' ' . $e->getMessage());
+            }
+        }
+
+        foreach ($this->projects as $internalId => $projectStats) {
+            if (empty($internalId)) {
+                continue;
+            }
+            try {
+                /** @var \Utopia\Database\Database $dbForProject */
+                $dbForProject = $getProjectDB($projectStats['project']);
+                Console::log('Processing batch with ' . count($projectStats['stats']) . ' stats');
+                $dbForProject->createOrUpdateDocumentsWithIncrease('stats', 'value', $projectStats['stats']);
+                Console::success('Batch successfully written to DB');
+
+                unset($this->projects[$internalId]);
+            } catch (Throwable $e) {
+                Console::error('Error processing stats: ' . $e->getMessage());
+            }
+        }
+
+        $this->writeToLogsDB();
+
+    }
+
+    protected function prepareForLogsDB(Document $project, Document $stat)
+    {
+        if (System::getEnv('_APP_STATS_USAGE_DUAL_WRITING', 'disabled') === 'disabled') {
+            return;
+        }
+        if (array_key_exists($stat->getAttribute('metric'), $this->skipBaseMetrics)) {
+            return;
+        }
+        foreach ($this->skipParentIdMetrics as $skipMetric) {
+            if (str_ends_with($stat->getAttribute('metric'), $skipMetric)) {
+                return;
+            }
+        }
+        $documentClone = new Document($stat->getArrayCopy());
+        $documentClone->setAttribute('$tenant', (int) $project->getInternalId());
+        $this->statDocuments[] = $documentClone;
+    }
+
+    protected function writeToLogsDB(): void
+    {
+        if (System::getEnv('_APP_STATS_USAGE_DUAL_WRITING', 'disabled') === 'disabled') {
+            Console::log('Dual Writing is disabled. Skipping...');
+            return;
+        }
+
+        /** @var \Utopia\Database\Database $dbForLogs*/
+        $dbForLogs = call_user_func($this->getLogsDB);
+        $dbForLogs
+            ->setTenant(null)
+            ->setTenantPerDocument(true);
+
+        try {
+            Console::log('Processing batch with ' . count($this->statDocuments) . ' stats');
+            $dbForLogs->createOrUpdateDocumentsWithIncrease(
+                'stats',
+                'value',
+                $this->statDocuments
+            );
+            Console::success('Usage logs pushed to Logs DB');
+        } catch (Throwable $th) {
+            Console::error($th->getMessage());
+        }
+        $this->register->get('pools')->get('logs')->reclaim();
     }
 }
