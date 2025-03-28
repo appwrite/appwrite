a9152c0c2021ee6910a267a9e7313fac5e9ab757
Jake Barnby <jakeb994@gmail.com>
2025-03-28 16:51:47 +1300

    Doctor pool use

--- src/Appwrite/Platform/Tasks/Doctor.php
+++ src/Appwrite/Platform/Tasks/Doctor.php
@@ -3,14 +3,17 @@
 namespace Appwrite\Platform\Tasks;
 
 use Appwrite\ClamAV\Network;
-use Appwrite\PubSub\Adapter;
+use Appwrite\PubSub\Adapter as PubSubAdapter;
+use PHPMailer\PHPMailer\PHPMailer;
 use Utopia\App;
 use Utopia\CLI\Console;
 use Utopia\Config\Config;
+use Utopia\Database\Adapter as DatabaseAdapter;
 use Utopia\Domains\Domain;
 use Utopia\DSN\DSN;
 use Utopia\Logger\Logger;
 use Utopia\Platform\Action;
+use Utopia\Pools\Group;
 use Utopia\Registry\Registry;
 use Utopia\Storage\Device\Local;
 use Utopia\Storage\Storage;
@@ -76,9 +79,9 @@ class Doctor extends Action
             Console::log('🟢 Abuse protection is enabled');
         }
 
-        $authWhitelistRoot = System::getEnv('_APP_CONSOLE_WHITELIST_ROOT', null);
-        $authWhitelistEmails = System::getEnv('_APP_CONSOLE_WHITELIST_EMAILS', null);
-        $authWhitelistIPs = System::getEnv('_APP_CONSOLE_WHITELIST_IPS', null);
+        $authWhitelistRoot = System::getEnv('_APP_CONSOLE_WHITELIST_ROOT');
+        $authWhitelistEmails = System::getEnv('_APP_CONSOLE_WHITELIST_EMAILS');
+        $authWhitelistIPs = System::getEnv('_APP_CONSOLE_WHITELIST_IPS');
 
         if (
             empty($authWhitelistRoot)
@@ -114,19 +117,16 @@ class Doctor extends Action
             } else {
                 Console::log('🟢 Logging adapter is enabled (' . $providerName . ')');
             }
-        } catch (\Throwable $th) {
+        } catch (\Throwable) {
             Console::log('🔴 Logging adapter is misconfigured');
         }
 
         \usleep(200 * 1000); // Sleep for 0.2 seconds
 
-        try {
-            Console::log("\n" . '[Connectivity]');
-        } catch (\Throwable $th) {
-            //throw $th;
-        }
+        Console::log("\n" . '[Connectivity]');
 
-        $pools = $register->get('pools'); /** @var \Utopia\Pools\Group $pools */
+        /** @var Group $pools */
+        $pools = $register->get('pools');
 
         $configs = [
             'Console.DB' => Config::getParam('pools-console'),
@@ -136,20 +136,22 @@ class Doctor extends Action
         foreach ($configs as $key => $config) {
             foreach ($config as $database) {
                 try {
-                    $adapter = $pools->get($database)->pop()->getResource();
-
-                    if ($adapter->ping()) {
-                        Console::success('🟢 ' . str_pad("{$key}({$database})", 50, '.') . 'connected');
-                    } else {
-                        Console::error('🔴 ' . str_pad("{$key}({$database})", 47, '.') . 'disconnected');
-                    }
-                } catch (\Throwable $th) {
+                    $pools->get($database)->use(function (DatabaseAdapter $adapter) use ($key, $database) {
+                        if ($adapter->ping()) {
+                            Console::success('🟢 ' . str_pad("{$key}({$database})", 50, '.') . 'connected');
+                        } else {
+                            Console::error('🔴 ' . str_pad("{$key}({$database})", 47, '.') . 'disconnected');
+                        }
+                    });
+                } catch (\Throwable) {
                     Console::error('🔴 ' . str_pad("{$key}.({$database})", 47, '.') . 'disconnected');
                 }
             }
         }
 
-        $pools = $register->get('pools'); /** @var \Utopia\Pools\Group $pools */
+        /** @var Group $pools */
+        $pools = $register->get('pools');
+
         $configs = [
             'Cache' => Config::getParam('pools-cache'),
             'Queue' => Config::getParam('pools-queue'),
@@ -159,15 +161,14 @@ class Doctor extends Action
         foreach ($configs as $key => $config) {
             foreach ($config as $pool) {
                 try {
-                    /** @var Adapter $adapter */
-                    $adapter = $pools->get($pool)->pop()->getResource();
-
-                    if ($adapter->ping()) {
-                        Console::success('🟢 ' . str_pad("{$key}({$pool})", 50, '.') . 'connected');
-                    } else {
-                        Console::error('🔴 ' . str_pad("{$key}({$pool})", 47, '.') . 'disconnected');
-                    }
-                } catch (\Throwable $th) {
+                    $pools->get($pool)->use(function (PubSubAdapter $adapter) use ($key, $pool) {
+                        if ($adapter->ping()) {
+                            Console::success('🟢 ' . str_pad("{$key}({$pool})", 50, '.') . 'connected');
+                        } else {
+                            Console::error('🔴 ' . str_pad("{$key}({$pool})", 47, '.') . 'disconnected');
+                        }
+                    });
+                } catch (\Throwable) {
                     Console::error('🔴 ' . str_pad("{$key}({$pool})", 47, '.') . 'disconnected');
                 }
             }
@@ -185,13 +186,14 @@ class Doctor extends Action
                 } else {
                     Console::error('🔴 ' . str_pad("Antivirus", 47, '.') . 'disconnected');
                 }
-            } catch (\Throwable $th) {
+            } catch (\Throwable) {
                 Console::error('🔴 ' . str_pad("Antivirus", 47, '.') . 'disconnected');
             }
         }
 
         try {
-            $mail = $register->get('smtp'); /* @var $mail \PHPMailer\PHPMailer\PHPMailer */
+            /* @var PHPMailer $mail */
+            $mail = $register->get('smtp');
 
             $mail->addAddress('demo@example.com', 'Example.com');
             $mail->Subject = 'Test SMTP Connection';
@@ -200,7 +202,7 @@ class Doctor extends Action
 
             $mail->send();
             Console::success('🟢 ' . str_pad("SMTP", 50, '.') . 'connected');
-        } catch (\Throwable $th) {
+        } catch (\Throwable) {
             Console::error('🔴 ' . str_pad("SMTP", 47, '.') . 'disconnected');
         }
 
@@ -274,7 +276,7 @@ class Doctor extends Action
                     Console::error('Failed to check for a newer version' . "\n");
                 }
             }
-        } catch (\Throwable $th) {
+        } catch (\Throwable) {
             Console::error('Failed to check for a newer version' . "\n");
         }
     }
