e78a3e1f83b0f0bbb15ae303a3c2c3468ff39517
Khushboo Verma <43381712+vermakhushboo@users.noreply.github.com>
2025-04-10 00:11:17 +0530

    Fix test

--- tests/e2e/Services/Sites/SitesCustomServerTest.php
+++ tests/e2e/Services/Sites/SitesCustomServerTest.php
@@ -2434,7 +2434,7 @@ class SitesCustomServerTest extends Scope
         }, 100000, 500);
 
         $response = $proxyClient->call(Client::METHOD_GET, '/');
-        $this->assertStringContainsString('build_failed', $response['body']);
+        $this->assertStringContainsString('Deployment build failed', $response['body']);
 
         $this->cleanupSite($siteId);
     }
