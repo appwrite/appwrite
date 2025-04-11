72b195cbc465c5ac99b28a4efe97294b64affdb6
loks0n <22452787+loks0n@users.noreply.github.com>
2025-04-11 12:55:27 +0100

    fix: origin validator

--- src/Appwrite/Network/Validator/Origin.php
+++ src/Appwrite/Network/Validator/Origin.php
@@ -18,14 +18,14 @@ class Origin extends Host
     /** @var array<Document> */
     protected array $platforms = [];
 
-    private string $origin = "";
+    private string $origin = '';
 
     /**
-     * @param array<Document> $platforms
+     * @param array<Document>|null $platforms
      */
-    public function __construct(array $platforms)
+    public function __construct(?array $platforms = [])
     {
-        $this->platforms = $platforms;
+        $this->platforms = $platforms ?? [];
         parent::__construct($this->getHostnames());
     }
 
@@ -33,17 +33,17 @@ class Origin extends Host
     {
         $platforms = array_filter(
             $this->platforms,
-            fn ($platform) => in_array($platform["type"], [
+            fn ($platform) => in_array($platform['type'], [
                 Client::TYPE_WEB,
                 Client::TYPE_FLUTTER_WEB,
             ])
         );
         $platforms = array_filter(
             $platforms,
-            fn ($platform) => !empty($platform["hostname"])
+            fn ($platform) => !empty($platform['hostname'])
         );
         $hostnames = array_map(
-            fn ($platform) => $platform["hostname"],
+            fn ($platform) => $platform['hostname'],
             $platforms
         );
 
@@ -54,15 +54,15 @@ class Origin extends Host
     {
         $platforms = array_filter(
             $this->platforms,
-            fn ($platform) => in_array($platform["type"], [
+            fn ($platform) => in_array($platform['type'], [
                 Client::TYPE_CUSTOM_SCHEME,
             ])
         );
         $platforms = array_filter(
             $platforms,
-            fn ($platform) => !empty($platform["key"])
+            fn ($platform) => !empty($platform['key'])
         );
-        $schemes = array_map(fn ($platform) => $platform["key"], $platforms);
+        $schemes = array_map(fn ($platform) => $platform['key'], $platforms);
 
         return array_unique($schemes);
     }
@@ -77,10 +77,10 @@ class Origin extends Host
     public function getDescription(): string
     {
         $parsed = $this->parseUrl($this->origin);
-        $platform = !empty($parsed["scheme"]) ? Client::getName($parsed["scheme"]) : "";
+        $platform = !empty($parsed['scheme']) ? Client::getName($parsed['scheme']) : '';
 
-        if (empty($this->origin) || empty($parsed["scheme"]) || empty($platform)) {
-            return "Unsupported platform";
+        if (empty($this->origin) || empty($parsed['scheme']) || empty($platform)) {
+            return 'Unsupported platform';
         }
 
         return "Invalid Origin. Register your new client ({$this->origin}) as a new {$platform} platform on your project console dashboard";
@@ -96,26 +96,26 @@ class Origin extends Host
      */
     public function isValid($value): bool
     {
-        $this->origin = $value;
+        $this->origin = $value ?? '';
         if (empty($value)) {
             return false;
         }
         $parsed = $this->parseUrl($value);
 
         if (
-            !empty($parsed["scheme"]) &&
-            \in_array($parsed["scheme"], $this->getSchemes())
+            !empty($parsed['scheme']) &&
+            \in_array($parsed['scheme'], $this->getSchemes())
         ) {
             return true;
         }
 
-        if (!in_array($parsed["scheme"], ["http", "https"])) {
+        if (!in_array($parsed['scheme'], ['http', 'https'])) {
             return false;
         }
 
         if (
-            !empty($parsed["host"]) &&
-            \in_array($parsed["host"], $this->getHostnames())
+            !empty($parsed['host']) &&
+            \in_array($parsed['host'], $this->getHostnames())
         ) {
             return parent::isValid($value);
         }
@@ -128,9 +128,9 @@ class Origin extends Host
         $parsed = \parse_url($value);
         $matches = [];
 
-        $parsed["scheme"] =
-            $parsed["scheme"] ??
-            (preg_match("/^([a-zA-Z][a-zA-Z0-9+.-]*):\/\//", $value, $matches)
+        $parsed['scheme'] =
+            $parsed['scheme'] ??
+            (preg_match('/^([a-zA-Z][a-zA-Z0-9+.-]*):\/\//', $value, $matches)
                 ? $matches[1]
                 : null);
 
