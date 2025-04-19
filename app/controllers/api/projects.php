decf874f89c23ebde4d4ff41a8947bb63bb6381e
Huiguan Liu <57612880+kateliuhyd@users.noreply.github.com>
2025-04-19 05:48:16 +0000

    fix: validate SMTP credentials using SMTPAuth

--- app/controllers/api/projects.php
+++ app/controllers/api/projects.php
@@ -1683,6 +1683,7 @@ App::patch('/v1/projects/:projectId/smtp')
         if ($enabled) {
             $mail = new PHPMailer(true);
             $mail->isSMTP();
+            $mail->SMTPAuth = (!empty($username) && !empty($password)) ? true : false;
             $mail->Username = $username;
             $mail->Password = $password;
             $mail->Host = $host;
