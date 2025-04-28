b4130c8e9ef18b5f62989993a452819ced3562f4
Khushboo Verma <khush.vv@gmail.com>
2025-04-28 19:15:43 +0530

    Remove s

--- tests/e2e/Services/Functions/FunctionsCustomServerTest.php
+++ tests/e2e/Services/Functions/FunctionsCustomServerTest.php
@@ -681,7 +681,7 @@ class FunctionsCustomServerTest extends Scope
          */
         $dateValidator = new DatetimeValidator();
 
-        $response = $this->client->call(Client::METHOD_PATCH, '/functions/' . $data['functionId'] . '/deployments', array_merge([
+        $response = $this->client->call(Client::METHOD_PATCH, '/functions/' . $data['functionId'] . '/deployment', array_merge([
             'content-type' => 'application/json',
             'x-appwrite-project' => $this->getProject()['$id'],
         ], $this->getHeaders()), [
