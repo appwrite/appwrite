7493e16bac4b2297eeaeedcb7318d9f2d359b3e5
Luke B. Silver <22452787+loks0n@users.noreply.github.com>
2025-05-08 19:23:04 +0100

    Revert "Merge pull request #9729 from appwrite/update-database-lib" (#9735)
    This reverts commit a35c955e54f0e859e537155c53fa77f2a11fec08, reversing
    changes made to 51f44b02b9ea79755703f47bc7087a8fea2db310.

--- src/Appwrite/Migration/Migration.php
+++ src/Appwrite/Migration/Migration.php
@@ -9,7 +9,6 @@ use Utopia\Config\Config;
 use Utopia\Database\Database;
 use Utopia\Database\Document;
 use Utopia\Database\Helpers\ID;
-use Utopia\Database\PDO;
 use Utopia\Database\Query;
 use Utopia\Database\Validator\Authorization;
 use Utopia\System\System;
@@ -39,9 +38,9 @@ abstract class Migration
     protected Database $consoleDB;
 
     /**
-     * @var PDO
+     * @var \PDO
      */
-    protected PDO $pdo;
+    protected \PDO $pdo;
 
     /**
      * @var array
@@ -147,10 +146,10 @@ abstract class Migration
     /**
      * Set PDO for Migration.
      *
-     * @param PDO $pdo
+     * @param \PDO $pdo
      * @return \Appwrite\Migration\Migration
      */
-    public function setPDO(PDO $pdo): self
+    public function setPDO(\PDO $pdo): self
     {
         $this->pdo = $pdo;
 
