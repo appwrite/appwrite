55236524e336b8108b94e6e8c0d332dce6db9735
Jake Barnby <jakeb994@gmail.com>
2025-05-14 21:42:39 +1200

    Fix merge

--- src/Appwrite/Platform/Tasks/ScheduleExecutions.php
+++ src/Appwrite/Platform/Tasks/ScheduleExecutions.php
@@ -3,6 +3,7 @@
 namespace Appwrite\Platform\Tasks;
 
 use Appwrite\Event\Func;
+use Swoole\Coroutine as Co;
 use Utopia\Database\Database;
 use Utopia\Pools\Group;
 
@@ -28,6 +29,7 @@ class ScheduleExecutions extends ScheduleBase
 
     protected function enqueueResources(Group $pools, Database $dbForPlatform, callable $getProjectDB): void
     {
+        $queueForFunctions = new Func($this->publisher);
         $intervalEnd = (new \DateTime())->modify('+' . self::ENQUEUE_TIMER . ' seconds');
 
         foreach ($this->schedules as $schedule) {
@@ -55,10 +57,8 @@ class ScheduleExecutions extends ScheduleBase
 
             $this->updateProjectAccess($schedule['project'], $dbForPlatform);
 
-            \go(function () use ($schedule, $delay, $data, $pools) {
-                \Co::sleep($delay);
-
-                $queueForFunctions = new Func($this->publisher);
+            \go(function () use ($queueForFunctions, $schedule, $scheduledAt, $delay, $data) {
+                Co::sleep($delay);
 
                 $queueForFunctions->setType('schedule')
                     // Set functionId instead of function as we don't have $dbForProject
