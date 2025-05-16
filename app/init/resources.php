bdd47f86c697dd5020885433cd5b156ca2f65a15
Christy Jacob <christyjacob4@gmail.com>
2025-05-16 19:05:07 +0200

    chore: revert f802fb9 and 651cec6

--- app/init/resources.php
+++ app/init/resources.php
@@ -507,21 +507,21 @@ App::setResource('timelimit', function (\Redis $redis) {
     };
 }, ['redis']);
 
-App::setResource('deviceForLocal', function (Telemetry $telemetry) {
-    return new Device\Telemetry($telemetry, new Local());
-}, ['telemetry']);
+App::setResource('deviceForLocal', function () {
+    return new Local();
+});
 
-App::setResource('deviceForFiles', function ($project, Telemetry $telemetry) {
-    return new Device\Telemetry($telemetry, getDevice(APP_STORAGE_UPLOADS . '/app-' . $project->getId()));
-}, ['project', 'telemetry']);
+App::setResource('deviceForFiles', function ($project) {
+    return getDevice(APP_STORAGE_UPLOADS . '/app-' . $project->getId());
+}, ['project']);
 
-App::setResource('deviceForFunctions', function ($project, Telemetry $telemetry) {
-    return new Device\Telemetry($telemetry, getDevice(APP_STORAGE_FUNCTIONS . '/app-' . $project->getId()));
-}, ['project', 'telemetry']);
+App::setResource('deviceForFunctions', function ($project) {
+    return getDevice(APP_STORAGE_FUNCTIONS . '/app-' . $project->getId());
+}, ['project']);
 
-App::setResource('deviceForBuilds', function ($project, Telemetry $telemetry) {
-    return new Device\Telemetry($telemetry, getDevice(APP_STORAGE_BUILDS . '/app-' . $project->getId()));
-}, ['project', 'telemetry']);
+App::setResource('deviceForBuilds', function ($project) {
+    return getDevice(APP_STORAGE_BUILDS . '/app-' . $project->getId());
+}, ['project']);
 
 function getDevice(string $root, string $connection = ''): Device
 {
