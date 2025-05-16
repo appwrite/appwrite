5d25265e5c92de1330aad70252bae8618803f887
Steven Nguyen <stnguyen90@users.noreply.github.com>
2025-05-16 14:46:24 -0700

    fix(storage): do not preview gif input/output
    Processing a gif file can consume large amounts of memory causing the
    container to crash.
    
    As a safety precaution, don't output to gif either. Any attempt to
    output to gif will fallback to jpg.

--- app/config/storage/inputs.php
+++ app/config/storage/inputs.php
@@ -4,7 +4,6 @@ return [
     // Accepted inputs files
     "jpg" => "image/jpeg",
     "jpeg" => "image/jpeg",
-    "gif" => "image/gif",
     "png" => "image/png",
     "heic" => "image/heic",
     "webp" => "image/webp",
