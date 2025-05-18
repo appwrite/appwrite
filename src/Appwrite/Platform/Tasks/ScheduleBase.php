517bf300a4d273bdaf3fdb82ae52803771dfb936
shimon <shimon@appwrite.io>
2025-05-18 18:37:46 +0300

    Runtime::enableCoroutine();

--- src/Appwrite/Platform/Tasks/ScheduleBase.php
+++ src/Appwrite/Platform/Tasks/ScheduleBase.php
@@ -14,6 +14,7 @@ use Utopia\Database\Validator\Authorization;
 use Utopia\Platform\Action;
 use Utopia\Pools\Group;
 use Utopia\System\System;
+use function Swoole\Coroutine\run;
 
 abstract class ScheduleBase extends Action
 {
