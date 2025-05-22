128dc1d946ab46bdb23127dcc94bc4f77a159139
Fabian Gruber <fabian@appwrite.io>
2025-05-22 09:58:48 +0200

    fix: send deploymentResourceType in rules verification

--- src/Appwrite/Platform/Modules/Proxy/Http/Rules/Function/Create.php
+++ src/Appwrite/Platform/Modules/Proxy/Http/Rules/Function/Create.php
@@ -192,7 +192,7 @@ class Create extends Action
             $queueForCertificates
                 ->setDomain(new Document([
                     'domain' => $rule->getAttribute('domain'),
-                    'domainType' => 'function',
+                    'domainType' => $rule->getAttribute('deploymentResourceType', $rule->getAttribute('type')),
                 ]))
                 ->trigger();
         }
