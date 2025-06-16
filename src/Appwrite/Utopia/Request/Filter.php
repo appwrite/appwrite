e704e96290822db12744bda75b0a003bb2f9e5c3
Jake Barnby <jakeb994@gmail.com>
2025-06-16 11:48:27 -0400

    Revert "Feat: Lazy-load relationships"

--- src/Appwrite/Utopia/Request/Filter.php
+++ src/Appwrite/Utopia/Request/Filter.php
@@ -2,20 +2,8 @@
 
 namespace Appwrite\Utopia\Request;
 
-use Utopia\Database\Database;
-use Utopia\Route;
-
 abstract class Filter
 {
-    private ?Route $route;
-    private ?Database $dbForProject;
-
-    public function __construct(Database $dbForProject = null, Route $route = null)
-    {
-        $this->route = $route;
-        $this->dbForProject = $dbForProject;
-    }
-
     /**
      * Parse params to another format.
      *
@@ -25,33 +13,4 @@ abstract class Filter
      * @return array
      */
     abstract public function parse(array $content, string $model): array;
-
-    /**
-     * Get the database for the current project.
-     *
-     * @return null|Database
-     */
-    public function getDbForProject(): ?Database
-    {
-        return $this->dbForProject;
-    }
-
-    /**
-     * Returns the value of the given route param key, or a default if not found or on error.
-     *
-     * @param string $key
-     * @param mixed $default
-     *
-     * @return mixed
-     */
-    public function getParamValue(string $key, mixed $default = ''): mixed
-    {
-        try {
-            $value = $this->route?->getParamValue($key) ?? $default;
-        } catch (\Exception $e) {
-            $value = $default;
-        }
-
-        return $value;
-    }
 }
