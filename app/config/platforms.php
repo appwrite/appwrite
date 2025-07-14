054b6a995aae0834e710152b116334223f9c6f11
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2025-07-14 08:54:37 +0530

    update dotnet to 0.14.0

--- app/config/platforms.php
+++ app/config/platforms.php
@@ -358,7 +358,7 @@ return [
             [
                 'key' => 'dotnet',
                 'name' => '.NET',
-                'version' => '0.13.0',
+                'version' => '0.14.0',
                 'url' => 'https://github.com/appwrite/sdk-for-dotnet',
                 'package' => 'https://www.nuget.org/packages/Appwrite',
                 'enabled' => true,
