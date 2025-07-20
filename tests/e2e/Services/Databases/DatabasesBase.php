4d670bb47db5fa0d36fce4818e0d8e4998a6a088
fogelito <fogelshmuel@gmail.com>
2025-07-20 12:06:23 +0300

    Remove use

--- tests/e2e/Services/Databases/DatabasesBase.php
+++ tests/e2e/Services/Databases/DatabasesBase.php
@@ -12,7 +12,6 @@ use Utopia\Database\Helpers\Permission;
 use Utopia\Database\Helpers\Role;
 use Utopia\Database\Query;
 use Utopia\Database\Validator\Datetime as DatetimeValidator;
-use Utopia\System\System;
 
 trait DatabasesBase
 {
