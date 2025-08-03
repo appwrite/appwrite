16685dad4e20a0ba4ce3c9126ea2787cf2dac9eb
Khushboo Verma <khush.vv@gmail.com>
2025-08-04 01:49:08 +0530

    Update console tests

--- tests/e2e/Services/Console/ConsoleConsoleClientTest.php
+++ tests/e2e/Services/Console/ConsoleConsoleClientTest.php
@@ -24,10 +24,11 @@ class ConsoleConsoleClientTest extends Scope
         ], $this->getHeaders()));
 
         $this->assertEquals(200, $response['headers']['status-code']);
-        $this->assertCount(13, $response['body']);
+        $this->assertCount(15, $response['body']);
         $this->assertIsString($response['body']['_APP_DOMAIN_TARGET_CNAME']);
         $this->assertIsString($response['body']['_APP_DOMAIN_TARGET_A']);
         $this->assertIsString($response['body']['_APP_DOMAIN_TARGET_AAAA']);
+        $this->assertIsString($response['body']['_APP_DOMAIN_TARGET_CAA']);
         $this->assertIsInt($response['body']['_APP_STORAGE_LIMIT']);
         $this->assertIsInt($response['body']['_APP_COMPUTE_SIZE_LIMIT']);
         $this->assertIsBool($response['body']['_APP_DOMAIN_ENABLED']);
@@ -37,6 +38,7 @@ class ConsoleConsoleClientTest extends Scope
         $this->assertIsString($response['body']['_APP_DOMAIN_FUNCTIONS']);
         $this->assertIsString($response['body']['_APP_OPTIONS_FORCE_HTTPS']);
         $this->assertIsString($response['body']['_APP_DOMAINS_NAMESERVERS']);
+        $this->assertIsString($response['body']['_APP_DOMAINS_DNS']);
         // When adding new keys, dont forget to update count a few lines above
     }
 }
