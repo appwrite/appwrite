f06af411badfb757aa05cc8a18626519c65aa585
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2025-08-05 16:48:05 +0530

    Add 'type' attribute to database creation and update tests

--- tests/e2e/Services/Databases/Legacy/DatabasesBase.php
+++ tests/e2e/Services/Databases/Legacy/DatabasesBase.php
@@ -32,6 +32,34 @@ trait DatabasesBase
         $this->assertNotEmpty($database['body']['$id']);
         $this->assertEquals(201, $database['headers']['status-code']);
         $this->assertEquals('Test Database', $database['body']['name']);
+        $this->assertEquals('mysql', $database['body']['type']);
+
+        // testing to create a database with type
+        $database2 = $this->client->call(Client::METHOD_POST, '/databases', [
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+            'x-appwrite-key' => $this->getProject()['apiKey']
+        ], [
+            'databaseId' => ID::unique(),
+            'name' => 'Test Database with type',
+            'type' => 'mongodb'
+        ]);
+
+        $this->assertNotEmpty($database2['body']['$id']);
+        $this->assertEquals(201, $database2['headers']['status-code']);
+        $this->assertEquals('Test Database with type', $database2['body']['name']);
+        $this->assertEquals('mongodb', $database2['body']['type']);
+
+        // cleanup(for database2)
+        $databaseId = $database2['body']['$id'];
+
+        $response = $this->client->call(Client::METHOD_DELETE, '/databases/' . $databaseId, [
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+            'x-appwrite-key' => $this->getProject()['apiKey']
+        ]);
+
+        $this->assertEquals(204, $response['headers']['status-code']);
 
         return ['databaseId' => $database['body']['$id']];
     }
