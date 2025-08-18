425d424e7c34cff0a82956b9c55de55e2cbeac33
Jake Barnby <jakeb994@gmail.com>
2025-08-19 02:17:31 +1200

    Fix index context

--- src/Appwrite/Platform/Modules/Databases/Http/Databases/Collections/Indexes/Action.php
+++ src/Appwrite/Platform/Modules/Databases/Http/Databases/Collections/Indexes/Action.php
@@ -19,8 +19,9 @@ abstract class Action extends UtopiaAction
 
     public function setHttpPath(string $path): UtopiaAction
     {
-        // Context is automatically set by Documents/Action.php setHttpPath for tablesdb paths
-
+        if (\str_contains($path, '/tablesdb')) {
+            $this->context = COLUMN_INDEX;
+        }
         return parent::setHttpPath($path);
     }
 
