4478edc211bbbf8540ee83472611c9badc0b7ff5
Damodar Lohani <dlohani48@gmail.com>
2025-09-10 04:25:21 +0000

    fix linter

--- app/config/console.php
+++ app/config/console.php
@@ -4,8 +4,6 @@
  * Initializes console project document.
  */
 
-use Appwrite\Network\Validator\Origin;
-use Appwrite\Auth\Auth;
 use Appwrite\Network\Platform;
 use Utopia\Database\Helpers\ID;
 use Utopia\System\System;
