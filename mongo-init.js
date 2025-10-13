f78f08bff99dae08914a31feb8f62a54375a67c5
Jake Barnby <jakeb994@gmail.com>
2025-10-13 18:23:43 +1300

    Transaction support

--- mongo-init.js
+++ mongo-init.js
@@ -1,14 +1,15 @@
 // mongo-init.js
 
 // Switch to the admin database
-const db = db.getSiblingDB('admin');
+const adminDb = db.getSiblingDB('admin');
 
 // Get username and password from environment variables
 const username = process.env.MONGO_INITDB_USERNAME;
 const password = process.env.MONGO_INITDB_PASSWORD;
 const database = process.env.MONGO_INITDB_DATABASE;
 
-db.createUser({
+// Create the user
+adminDb.createUser({
   user: username,
   pwd: password,
   roles: [
