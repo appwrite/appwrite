7bbe08f23e25115c62c43ea4ea7d54eaad77eaa0
shimon <shimon@appwrite.io>
2025-10-19 14:38:32 +0300

    Enhance XList classes to support 'includeTotal' parameter for controlling total count in database queries

--- src/Appwrite/Platform/Modules/Databases/Http/TablesDB/Tables/XList.php
+++ src/Appwrite/Platform/Modules/Databases/Http/TablesDB/Tables/XList.php
@@ -12,6 +12,7 @@ use Appwrite\Utopia\Response as UtopiaResponse;
 use Utopia\Database\Validator\UID;
 use Utopia\Swoole\Response as SwooleResponse;
 use Utopia\Validator\Text;
+use Utopia\Validator\Boolean;
 
 class XList extends CollectionXList
 {
@@ -51,6 +52,7 @@ class XList extends CollectionXList
             ->param('databaseId', '', new UID(), 'Database ID.')
             ->param('queries', [], new Tables(), 'Array of query strings generated using the Query class provided by the SDK. [Learn more about queries](https://appwrite.io/docs/queries). Maximum of ' . APP_LIMIT_ARRAY_PARAMS_SIZE . ' queries are allowed, each ' . APP_LIMIT_ARRAY_ELEMENT_SIZE . ' characters long. You may filter on the following columns: ' . implode(', ', Tables::ALLOWED_COLUMNS), true)
             ->param('search', '', new Text(256), 'Search term to filter your list results. Max length: 256 chars.', true)
+            ->param('includeTotal', true, new Boolean(), 'When set to false, the total countreturend will be 0 and will not be calculated.', true)
             ->inject('response')
             ->inject('dbForProject')
             ->callback($this->action(...));
--- src/Appwrite/Platform/Modules/Databases/Http/TablesDB/XList.php
+++ src/Appwrite/Platform/Modules/Databases/Http/TablesDB/XList.php
@@ -11,6 +11,7 @@ use Appwrite\Utopia\Database\Validator\Queries\Databases;
 use Appwrite\Utopia\Response as UtopiaResponse;
 use Utopia\Swoole\Response as SwooleResponse;
 use Utopia\Validator\Text;
+use Utopia\Validator\Boolean;
 
 class XList extends DatabaseXList
 {
@@ -44,6 +45,7 @@ class XList extends DatabaseXList
             ))
             ->param('queries', [], new Databases(), 'Array of query strings generated using the Query class provided by the SDK. [Learn more about queries](https://appwrite.io/docs/queries). Maximum of ' . APP_LIMIT_ARRAY_PARAMS_SIZE . ' queries are allowed, each ' . APP_LIMIT_ARRAY_ELEMENT_SIZE . ' characters long. You may filter on the following columns: ' . implode(', ', Databases::ALLOWED_ATTRIBUTES), true)
             ->param('search', '', new Text(256), 'Search term to filter your list results. Max length: 256 chars.', true)
+            ->param('includeTotal', true, new Boolean(), 'When set to false, the total countreturend will be 0 and will not be calculated.', true)
             ->inject('response')
             ->inject('dbForProject')
             ->callback($this->action(...));
