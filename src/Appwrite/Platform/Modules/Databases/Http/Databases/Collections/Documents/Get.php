3e876f5bd8ca275f0035305d3788aad0276dda96
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2025-10-30 12:57:41 +0530

    fixed merge conflicts

--- src/Appwrite/Platform/Modules/Databases/Http/Databases/Collections/Documents/Get.php
+++ src/Appwrite/Platform/Modules/Databases/Http/Databases/Collections/Documents/Get.php
@@ -65,7 +65,6 @@ class Get extends Action
             ->param('documentId', '', fn (Database $dbForProject) => new UID($dbForProject->getAdapter()->getMaxUIDLength()), 'Document ID.', false, ['dbForProject'])
             ->param('queries', [], new ArrayList(new Text(APP_LIMIT_ARRAY_ELEMENT_SIZE), APP_LIMIT_ARRAY_PARAMS_SIZE), 'Array of query strings generated using the Query class provided by the SDK. [Learn more about queries](https://appwrite.io/docs/queries). Maximum of ' . APP_LIMIT_ARRAY_PARAMS_SIZE . ' queries are allowed, each ' . APP_LIMIT_ARRAY_ELEMENT_SIZE . ' characters long.', true)
             ->param('transactionId', null, new UID(), 'Transaction ID to read uncommitted changes within the transaction.', true)
-            ->param('transactionId', null, new UID(), 'Transaction ID to read uncommitted changes within the transaction.', true)
             ->inject('response')
             ->inject('dbForProject')
             ->inject('getDatabasesDB')
