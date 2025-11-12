75045ffd58673fad7ec92cac7367056e18d95cfb
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2025-11-12 16:28:15 +0530

    added constants for the vector dimension

--- src/Appwrite/Platform/Modules/Databases/Http/VectorDB/Collections/Update.php
+++ src/Appwrite/Platform/Modules/Databases/Http/VectorDB/Collections/Update.php
@@ -59,7 +59,7 @@ class Update extends CollectionAction
             ->param('databaseId', '', new UID(), 'Database ID.')
             ->param('collectionId', '', new UID(), 'Collection ID.')
             ->param('name', null, new Text(128), 'Collection name. Max length: 128 chars.')
-            ->param('dimensions', null, new Range(1, 16000), 'Embedding dimensions.', true)
+            ->param('dimensions', null, new Range(MIN_VECTOR_DIMENSION, MAX_VECTOR_DIMENSION), 'Embedding dimensions.', true)
             ->param('permissions', null, new Permissions(APP_LIMIT_ARRAY_PARAMS_SIZE), 'An array of permission strings. By default, the current permissions are inherited. [Learn more about permissions](https://appwrite.io/docs/permissions).', true)
             ->param('documentSecurity', false, new Boolean(true), 'Enables configuring permissions for individual documents. A user needs one of document or collection level permissions to access a document. [Learn more about permissions](https://appwrite.io/docs/permissions).', true)
             ->param('enabled', true, new Boolean(), 'Is collection enabled? When set to \'disabled\', users cannot access the collection but Server SDKs with and API key can still read and write to the collection. No data is lost when this is toggled.', true)
