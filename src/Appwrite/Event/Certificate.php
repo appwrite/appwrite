37833782883f58cfde6247667c82a8fb576c7d8b
Hemachandar <hmac.devo@gmail.com>
2025-12-18 15:14:30 +0530

    add action to payload

--- src/Appwrite/Event/Certificate.php
+++ src/Appwrite/Event/Certificate.php
@@ -127,7 +127,8 @@ class Certificate extends Event
             'project' => $this->project,
             'domain' => $this->domain,
             'skipRenewCheck' => $this->skipRenewCheck,
-            'validationDomain' => $this->validationDomain
+            'validationDomain' => $this->validationDomain,
+            'action' => $this->action
         ];
     }
 }
