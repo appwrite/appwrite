aa17eeb6e356c15544296aab6cef84cb3a789c4e
Damodar Lohani <lohanidamodar@users.noreply.github.com>
2025-12-28 14:19:28 +0545

    Disable filters for platform and project databases - Stat resources
    - Disable filters for platform and project databases - Stat resources

--- src/Appwrite/Platform/Workers/StatsResources.php
+++ src/Appwrite/Platform/Workers/StatsResources.php
@@ -100,6 +100,9 @@ class StatsResources extends Action
             return;
         }
 
+        $dbForPlatform->disableFilters();
+        $dbForProject->disableFilters();
+
         try {
 
             $region = $project->getAttribute('region');
