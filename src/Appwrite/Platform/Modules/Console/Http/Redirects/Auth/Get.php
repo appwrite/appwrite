69ce99d1b43dd9cb5e33d17136cad689f95090c2
Darshan <itznotabug@gmail.com>
2025-12-31 11:52:41 +0530

    feat: console module.

---
+++ src/Appwrite/Platform/Modules/Console/Http/Redirects/Auth/Get.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Appwrite\Platform\Modules\Console\Http\Redirects\Auth;
+
+use Appwrite\Platform\Modules\Console\Http\Redirects\Base;
+
+class Get extends Base
+{
+    public static function getName(): string
+    {
+        return 'consoleRedirectAuth';
+    }
+
+    protected function getPath(): string
+    {
+        return 'auth/*';
+    }
+}
