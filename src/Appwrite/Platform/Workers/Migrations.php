53b888672950cf0a47bcc4c8601bb7865a8745ec
fogelito <fogelshmuel@gmail.com>
2026-02-02 11:29:38 +0200

    Remove env variable

--- src/Appwrite/Platform/Workers/Migrations.php
+++ src/Appwrite/Platform/Workers/Migrations.php
@@ -333,11 +333,6 @@ class Migrations extends Action
 
         $transfer = $source = $destination = null;
 
-        //        $endpoint = System::getEnv('_APP_MIGRATION_ENDPOINT');
-        //        if (empty($endpoint)) {
-        //            throw new \Exception('_APP_MIGRATION_ENDPOINT env is empty');
-        //        }
-
         $protocol = System::getEnv('_APP_OPTIONS_FORCE_HTTPS') === 'disabled' ? 'http' : 'https';
         $endpoint = $protocol . '://' . $platform['apiHostname'] . '/v1';
 
