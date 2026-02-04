26b04476573dca30cbfe3c513b54acf300fdac9c
Hemachandar <hmac.devo@gmail.com>
2026-02-04 20:41:46 +0530

    add to http

--- src/Appwrite/Platform/Modules/VCS/Services/Http.php
+++ src/Appwrite/Platform/Modules/VCS/Services/Http.php
@@ -2,6 +2,9 @@
 
 namespace Appwrite\Platform\Modules\VCS\Services;
 
+use Appwrite\Platform\Modules\VCS\Http\Installations\Get as GetInstallation;
+use Appwrite\Platform\Modules\VCS\Http\Installations\Delete as DeleteInstallation;
+use Appwrite\Platform\Modules\VCS\Http\Installations\XList as ListInstallations;
 use Utopia\Platform\Service;
 
 class Http extends Service
@@ -9,5 +12,10 @@ class Http extends Service
     public function __construct()
     {
         $this->type = Service::TYPE_HTTP;
+
+        // Installations
+        $this->addAction(GetInstallation::getName(), new GetInstallation());
+        $this->addAction(ListInstallations::getName(), new ListInstallations());
+        $this->addAction(DeleteInstallation::getName(), new DeleteInstallation());
     }
 }
