e09dd98f360b680bbcbaea24101dc63de3f21740
Matej Bačo <matejbaco2000@gmail.com>
2026-02-09 13:30:46 +0100

    Fix site vcs deployment bug too

--- src/Appwrite/Platform/Modules/Compute/Base.php
+++ src/Appwrite/Platform/Modules/Compute/Base.php
@@ -205,6 +205,12 @@ class Base extends Action
             } catch (\Throwable $error) {
                 // Ignore; deployment can continue
             }
+        } else {
+            // Fallback till we have tag support here
+            // Goal is to set providerBranch, so build worker knows what to clone as base
+            // Without this, clone command would be cloning empty branch, and failing
+            $providerBranch = $site->getAttribute('providerBranch', 'main');
+            $branchUrl = "https://github.com/$owner/$repositoryName/tree/$providerBranch";
         }
 
         $repositoryUrl = "https://github.com/$owner/$repositoryName";
