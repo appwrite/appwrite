fb595ced55691b139d6bdb21f1775c49c1c0b7b7
Damodar Lohani <dlohani48@gmail.com>
2026-02-11 01:26:17 +0000

    Feat: Restore project status attribute in project creation

--- src/Appwrite/Platform/Modules/Projects/Http/Projects/Create.php
+++ src/Appwrite/Platform/Modules/Projects/Http/Projects/Create.php
@@ -119,8 +119,7 @@ class Create extends Action
             'membershipsUserName' => false,
             'membershipsUserEmail' => false,
             'membershipsMfa' => false,
-            'invalidateSessions' => true,
-            'status' => PROJECT_STATUS_ACTIVE,
+            'invalidateSessions' => true
         ];
 
         foreach ($auth as $method) {
@@ -193,6 +192,7 @@ class Create extends Action
                 'search' => implode(' ', [$projectId, $name]),
                 'database' => $dsn,
                 'labels' => [],
+                'status' => PROJECT_STATUS_ACTIVE,
             ]));
         } catch (Duplicate) {
             throw new Exception(Exception::PROJECT_ALREADY_EXISTS);
