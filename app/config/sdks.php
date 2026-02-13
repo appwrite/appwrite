2da8aa21543825e87b6971d5e02d07bbeee1509a
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-02-13 22:22:18 +0530

    bump sdk

--- app/config/sdks.php
+++ app/config/sdks.php
@@ -11,7 +11,7 @@ return [
             [
                 'key' => 'web',
                 'name' => 'Web',
-                'version' => '22.2.0',
+                'version' => '22.3.0',
                 'url' => 'https://github.com/appwrite/sdk-for-web',
                 'package' => 'https://www.npmjs.com/package/appwrite',
                 'enabled' => true,
@@ -60,7 +60,7 @@ return [
             [
                 'key' => 'flutter',
                 'name' => 'Flutter',
-                'version' => '21.2.0',
+                'version' => '21.3.0',
                 'url' => 'https://github.com/appwrite/sdk-for-flutter',
                 'package' => 'https://pub.dev/packages/appwrite',
                 'enabled' => true,
@@ -117,7 +117,7 @@ return [
                 'key' => 'android',
                 'name' => 'Android',
                 'namespace' => 'io.appwrite',
-                'version' => '12.2.0',
+                'version' => '12.3.0',
                 'url' => 'https://github.com/appwrite/sdk-for-android',
                 'package' => 'https://search.maven.org/artifact/io.appwrite/sdk-for-android',
                 'enabled' => true,
@@ -140,7 +140,7 @@ return [
             [
                 'key' => 'react-native',
                 'name' => 'React Native',
-                'version' => '0.22.0',
+                'version' => '0.23.0',
                 'url' => 'https://github.com/appwrite/sdk-for-react-native',
                 'package' => 'https://npmjs.com/package/react-native-appwrite',
                 'enabled' => true,
