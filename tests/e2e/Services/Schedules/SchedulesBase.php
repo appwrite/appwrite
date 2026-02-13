3fde05e024c4c536880df3e27ef33fe7d979bcc5
Prem Palanisamy <i.prem.tsd@gmail.com>
2026-02-13 18:37:48 +0000

    Add resource validation, admin scopes, and events for schedules

--- tests/e2e/Services/Schedules/SchedulesBase.php
+++ tests/e2e/Services/Schedules/SchedulesBase.php
@@ -3,6 +3,7 @@
 namespace Tests\E2E\Services\Schedules;
 
 use Tests\E2E\Client;
+use Utopia\Database\Helpers\ID;
 
 trait SchedulesBase
 {
@@ -28,5 +29,19 @@ trait SchedulesBase
             'content-type' => 'application/json',
             'x-appwrite-project' => $this->getProject()['$id'],
         ], $this->getHeaders()), $params);
+    }
+
+    protected function createFunction(array $params = []): array
+    {
+        return $this->client->call(Client::METHOD_POST, '/functions', array_merge([
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+        ], $this->getHeaders()), array_merge([
+            'functionId' => ID::unique(),
+            'name' => 'Test Schedule Function',
+            'runtime' => 'node-22',
+            'entrypoint' => 'index.js',
+            'execute' => ['any'],
+        ], $params));
     }
 }
