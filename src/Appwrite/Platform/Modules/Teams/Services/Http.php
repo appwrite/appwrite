b9d275b22856af1e6d2fa4d840a21dcd4c3330f9
Hemachandar <hmac.devo@gmail.com>
2026-02-19 13:47:55 +0530

    lint

--- src/Appwrite/Platform/Modules/Teams/Services/Http.php
+++ src/Appwrite/Platform/Modules/Teams/Services/Http.php
@@ -2,12 +2,12 @@
 
 namespace Appwrite\Platform\Modules\Teams\Services;
 
-use Utopia\Platform\Service;
 use Appwrite\Platform\Modules\Teams\Http\Teams\Create as CreateTeam;
-use Appwrite\Platform\Modules\Teams\Http\Teams\Get as GetTeam;
-use Appwrite\Platform\Modules\Teams\Http\Teams\XList as ListTeams;
 use Appwrite\Platform\Modules\Teams\Http\Teams\Delete as DeleteTeam;
+use Appwrite\Platform\Modules\Teams\Http\Teams\Get as GetTeam;
 use Appwrite\Platform\Modules\Teams\Http\Teams\Name\Update as UpdateTeamName;
+use Appwrite\Platform\Modules\Teams\Http\Teams\XList as ListTeams;
+use Utopia\Platform\Service;
 
 class Http extends Service
 {
