5a6cfdbe4c5d97e50911e25338c486aebc1c4f2f
Prem Palanisamy <i.prem.tsd@gmail.com>
2026-02-25 06:42:46 +0000

    fix: revert composer to 1.8.x and remove child resource counts from MigrationReport

--- src/Appwrite/Utopia/Response/Model/MigrationReport.php
+++ src/Appwrite/Utopia/Response/Model/MigrationReport.php
@@ -59,29 +59,11 @@ class MigrationReport extends Model
                 'default' => 0,
                 'example' => 5,
             ])
-            ->addRule(Resource::TYPE_ENVIRONMENT_VARIABLE, [
-                'type' => self::TYPE_INTEGER,
-                'description' => 'Number of environment variables to be migrated.',
-                'default' => 0,
-                'example' => 10,
-            ])
             ->addRule(Resource::TYPE_SITE, [
                 'type' => self::TYPE_INTEGER,
                 'description' => 'Number of sites to be migrated.',
                 'default' => 0,
                 'example' => 5,
-            ])
-            ->addRule(Resource::TYPE_SITE_DEPLOYMENT, [
-                'type' => self::TYPE_INTEGER,
-                'description' => 'Number of site deployments to be migrated.',
-                'default' => 0,
-                'example' => 5,
-            ])
-            ->addRule(Resource::TYPE_SITE_VARIABLE, [
-                'type' => self::TYPE_INTEGER,
-                'description' => 'Number of site variables to be migrated.',
-                'default' => 0,
-                'example' => 10,
             ])
             ->addRule('size', [
                 'type' => self::TYPE_INTEGER,
