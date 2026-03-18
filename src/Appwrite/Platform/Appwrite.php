ba94bff8d47092d0d2b24bf686d05b46353b9bb8
Matej Bačo <matejbaco2000@gmail.com>
2026-03-18 14:48:31 +0100

    Public project variables API

--- src/Appwrite/Platform/Appwrite.php
+++ src/Appwrite/Platform/Appwrite.php
@@ -15,6 +15,7 @@ use Appwrite\Platform\Modules\Sites;
 use Appwrite\Platform\Modules\Storage;
 use Appwrite\Platform\Modules\Teams;
 use Appwrite\Platform\Modules\Tokens;
+use Appwrite\Platform\Modules\Variables;
 use Appwrite\Platform\Modules\VCS;
 use Appwrite\Platform\Modules\Webhooks;
 use Utopia\Platform\Platform;
@@ -38,5 +39,6 @@ class Appwrite extends Platform
         $this->addModule(new Storage\Module());
         $this->addModule(new VCS\Module());
         $this->addModule(new Webhooks\Module());
+        $this->addModule(new Variables\Module());
     }
 }
