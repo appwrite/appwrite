2db1d6efc7c82823fdfaf6fc23e95047c937174f
Hemachandar <hmac.devo@gmail.com>
2026-03-21 15:37:57 +0530

    add success

--- app/controllers/api/projects.php
+++ app/controllers/api/projects.php
@@ -1457,7 +1457,7 @@ Http::patch('/v1/projects/:projectId/smtp')
                 throw new Exception(Exception::PROJECT_SMTP_CONFIG_INVALID, $error->getMessage());
             } finally {
                 $duration = microtime(true) - $validationStartTime;
-                $smtpVerificationDuration->record($duration, ['projectId' => $projectId]);
+                $smtpVerificationDuration->record($duration, ['projectId' => $projectId, 'success' => isset($valid) && $valid]);
             }
         }
 
