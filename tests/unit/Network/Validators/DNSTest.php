74b00ded67d3abb6c826b5a422110289b5c58bff
Hemachandar <hmac.devo@gmail.com>
2026-03-24 17:25:18 +0530

    run dns test in coroutine

--- tests/unit/Network/Validators/DNSTest.php
+++ tests/unit/Network/Validators/DNSTest.php
@@ -10,44 +10,52 @@ class DNSTest extends TestCase
 {
     public function testSingleDNSServer(): void
     {
-        $validator = new DNS('appwrite.io', Record::TYPE_CNAME, ['8.8.8.8']);
+        \Swoole\Coroutine\run(function () {
+            $validator = new DNS('appwrite.io', Record::TYPE_CNAME, ['8.8.8.8']);
 
-        $this->assertEquals(false, $validator->isValid(''));
-        $this->assertEquals(false, $validator->isValid(null));
-        $this->assertEquals('string', $validator->getType());
+            $this->assertEquals(false, $validator->isValid(''));
+            $this->assertEquals(false, $validator->isValid(null));
+            $this->assertEquals('string', $validator->getType());
+        });
     }
 
     public function testMultipleDNSServers(): void
     {
-        $validator = new DNS('appwrite.io', Record::TYPE_CNAME, ['8.8.8.8', '1.1.1.1']);
+        \Swoole\Coroutine\run(function () {
+            $validator = new DNS('appwrite.io', Record::TYPE_CNAME, ['8.8.8.8', '1.1.1.1']);
 
-        $this->assertEquals(false, $validator->isValid(''));
-        $this->assertEquals(false, $validator->isValid(null));
-        $this->assertEquals('string', $validator->getType());
+            $this->assertEquals(false, $validator->isValid(''));
+            $this->assertEquals(false, $validator->isValid(null));
+            $this->assertEquals('string', $validator->getType());
+        });
     }
 
     public function testValidationFailure(): void
     {
-        $validator = new DNS('invalid-target.example.com', Record::TYPE_CNAME, ['8.8.8.8', '1.1.1.1']);
+        \Swoole\Coroutine\run(function () {
+            $validator = new DNS('invalid-target.example.com', Record::TYPE_CNAME, ['8.8.8.8', '1.1.1.1']);
 
-        $result = $validator->isValid('nonexistent-domain-' . \uniqid() . '.com');
+            $result = $validator->isValid('nonexistent-domain-' . \uniqid() . '.com');
 
-        $this->assertEquals(false, $result);
-        $this->assertIsInt($validator->count);
-        $this->assertIsString($validator->value);
-        $this->assertIsArray($validator->records);
-        $this->assertIsString($validator->getDescription());
+            $this->assertEquals(false, $result);
+            $this->assertIsInt($validator->count);
+            $this->assertIsString($validator->value);
+            $this->assertIsArray($validator->records);
+            $this->assertIsString($validator->getDescription());
+        });
     }
 
     public function testCoreDNSFailure(): void
     {
-        // CoreDNS is configured to return cname.localhost. for stage.webapp.com
-        $validator = new DNS('cname.localhost.', Record::TYPE_CNAME, ['172.16.238.100', '8.8.8.8']);
+        \Swoole\Coroutine\run(function () {
+            // CoreDNS is configured to return cname.localhost. for stage.webapp.com
+            $validator = new DNS('cname.localhost.', Record::TYPE_CNAME, ['172.16.238.100', '8.8.8.8']);
 
-        $result = $validator->isValid('stage.webapp.com');
-        $this->assertEquals(false, $result);
+            $result = $validator->isValid('stage.webapp.com');
+            $this->assertEquals(false, $result);
 
-        $result = $validator->isValid('stage-wrong-cname.webapp.com');
-        $this->assertEquals(false, $result);
+            $result = $validator->isValid('stage-wrong-cname.webapp.com');
+            $this->assertEquals(false, $result);
+        });
     }
 }
