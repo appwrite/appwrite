e4bac5a5f74b07fd1e20db9cf3f281236ae3ebcd
Prem Palanisamy <i.prem.tsd@gmail.com>
2026-04-06 02:36:44 +0100

    Fix shared mode routing for documentsdb/vectorsdb pools

--- app/worker.php
+++ app/worker.php
@@ -245,7 +245,8 @@ Server::setResource('getDatabasesDB', function (Cache $cache, Registry $register
         }
 
         $pools = $register->get('pools');
-        $pool = $pools->get($databaseDSN->getHost());
+        $databaseHost = $databaseDSN->getHost();
+        $pool = $pools->get($databaseHost);
 
         $adapter = new DatabasePool($pool);
         $database = new Database($adapter, $cache);
@@ -254,9 +255,27 @@ Server::setResource('getDatabasesDB', function (Cache $cache, Registry $register
             ->setAuthorization($authorization);
         $database->getAdapter()->setSupportForAttributes($databaseType !== DOCUMENTSDB);
 
-        $sharedTables = \explode(',', System::getEnv('_APP_DATABASE_SHARED_TABLES', ''));
+        $sharedTables = \array_filter(\explode(',', System::getEnv('_APP_DATABASE_SHARED_TABLES', '')));
 
-        if (\in_array($dsn->getHost(), $sharedTables, true)) {
+        if ($databaseHost !== $dsn->getHost()) {
+            $dbTypeSharedTables = match ($databaseType) {
+                DOCUMENTSDB => \array_filter(\explode(',', System::getEnv('_APP_DATABASE_DOCUMENTSDB_SHARED_TABLES', ''))),
+                VECTORSDB => \array_filter(\explode(',', System::getEnv('_APP_DATABASE_VECTORSDB_SHARED_TABLES', ''))),
+                default => [],
+            };
+
+            if (\in_array($databaseHost, $dbTypeSharedTables)) {
+                $database
+                    ->setSharedTables(true)
+                    ->setTenant($projectDocument->getSequence())
+                    ->setNamespace($dsn->getParam('namespace'));
+            } else {
+                $database
+                    ->setSharedTables(false)
+                    ->setTenant(null)
+                    ->setNamespace('_' . $projectDocument->getSequence());
+            }
+        } elseif (\in_array($dsn->getHost(), $sharedTables, true)) {
             $database
                 ->setSharedTables(true)
                 ->setTenant($projectDocument->getSequence())
