971858078c5e8a88b597da9fe3e9337fe7736ca4
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-04-30 16:50:04 +0530

    updated

--- app/config/scopes/project.php
+++ app/config/scopes/project.php
@@ -340,7 +340,7 @@ return [
     'presences.read' => [
         'description' => 'Access to read your project\'s presences',
     ],
-    'presences.read' => [
+    'presences.write' => [
         'description' => 'Access to create, update, and delete your project\'s presences',
     ],
 ];
