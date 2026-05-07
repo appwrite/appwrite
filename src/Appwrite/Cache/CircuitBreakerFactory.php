3a21eb9b092c6d5469c40e09418f478333047e5f
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-05-07 14:03:20 +0530

    Allow cache circuit breaker metric prefix override

--- src/Appwrite/Cache/CircuitBreakerFactory.php
+++ src/Appwrite/Cache/CircuitBreakerFactory.php
@@ -12,14 +12,14 @@ class CircuitBreakerFactory
     private const SUCCESS_THRESHOLD = 2;
     private const METRIC_PREFIX = 'appwrite';
 
-    public static function create(?Telemetry $telemetry = null): CircuitBreaker
+    public static function create(?Telemetry $telemetry = null, string $metricPrefix = self::METRIC_PREFIX): CircuitBreaker
     {
         return new CircuitBreaker(
             threshold: self::THRESHOLD,
             timeout: self::TIMEOUT,
             successThreshold: self::SUCCESS_THRESHOLD,
             telemetry: $telemetry,
-            metricPrefix: self::METRIC_PREFIX,
+            metricPrefix: $metricPrefix,
         );
     }
 }
