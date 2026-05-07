9242727811277ecfc9113974e291bca94eadb545
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-05-07 18:11:52 +0530

    Use default circuit breaker metric names

--- src/Appwrite/Cache/CircuitBreakerFactory.php
+++ src/Appwrite/Cache/CircuitBreakerFactory.php
@@ -10,16 +10,14 @@ class CircuitBreakerFactory
     private const THRESHOLD = 3;
     private const TIMEOUT = 30;
     private const SUCCESS_THRESHOLD = 2;
-    private const METRIC_PREFIX = 'appwrite';
 
-    public static function create(?Telemetry $telemetry = null, string $metricPrefix = self::METRIC_PREFIX): CircuitBreaker
+    public static function create(?Telemetry $telemetry = null): CircuitBreaker
     {
         return new CircuitBreaker(
             threshold: self::THRESHOLD,
             timeout: self::TIMEOUT,
             successThreshold: self::SUCCESS_THRESHOLD,
             telemetry: $telemetry,
-            metricPrefix: $metricPrefix,
         );
     }
 }
