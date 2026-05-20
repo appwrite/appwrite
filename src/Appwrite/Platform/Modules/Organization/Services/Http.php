cbcf86e362ef271979ee1f7ece03b1bb82b4cf84
Matej Bačo <matejbaco2000@gmail.com>
2026-05-20 13:49:40 +0200

    Add missing delete endpoint

--- src/Appwrite/Platform/Modules/Organization/Services/Http.php
+++ src/Appwrite/Platform/Modules/Organization/Services/Http.php
@@ -4,6 +4,7 @@ namespace Appwrite\Platform\Modules\Organization\Services;
 
 use Appwrite\Platform\Modules\Organization\Http\Init as Init;
 use Appwrite\Platform\Modules\Organization\Http\Projects\Create as CreateProject;
+use Appwrite\Platform\Modules\Organization\Http\Projects\Delete as DeleteProject;
 use Appwrite\Platform\Modules\Organization\Http\Projects\Get as GetProject;
 use Appwrite\Platform\Modules\Organization\Http\Projects\Update as UpdateProject;
 use Appwrite\Platform\Modules\Organization\Http\Projects\XList as ListProjects;
@@ -23,5 +24,6 @@ class Http extends Service
         $this->addAction(ListProjects::getName(), new ListProjects());
         $this->addAction(GetProject::getName(), new GetProject());
         $this->addAction(UpdateProject::getName(), new UpdateProject());
+        $this->addAction(DeleteProject::getName(), new DeleteProject());
     }
 }
