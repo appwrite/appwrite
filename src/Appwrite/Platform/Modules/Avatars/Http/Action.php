13d997ec2c3ea8aa7ed726025394c294ced531c6
fogelito <fogelshmuel@gmail.com>
2026-06-24 08:34:42 +0300

    Partial payloads

--- src/Appwrite/Platform/Modules/Avatars/Http/Action.php
+++ src/Appwrite/Platform/Modules/Avatars/Http/Action.php
@@ -111,7 +111,11 @@ class Action extends PlatformAction
                         ->setAttribute('providerRefreshToken', $refreshToken)
                         ->setAttribute('providerAccessTokenExpiry', DateTime::addSeconds(new \DateTime(), (int)$oauth2->getAccessTokenExpiry('')));
 
-                    $authorization->skip(fn () => $dbForProject->updateDocument('sessions', $gitHubSession->getId(), $gitHubSession));
+                    $authorization->skip(fn () => $dbForProject->updateDocument('sessions', $gitHubSession->getId(), new Document([
+                        'providerAccessToken' => $accessToken,
+                        'providerRefreshToken' => $refreshToken,
+                        'providerAccessTokenExpiry' => $gitHubSession->getAttribute('providerAccessTokenExpiry'),
+                    ])));
 
                     $dbForProject->purgeCachedDocument('users', $user->getId());
                 } catch (Throwable $err) {
