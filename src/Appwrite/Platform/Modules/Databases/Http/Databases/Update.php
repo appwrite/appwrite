22fa41ae463a5fd144e4dc288d0d1a3418704095
fogelito <fogelshmuel@gmail.com>
2026-06-24 09:59:05 +0300

    Update.php

--- src/Appwrite/Platform/Modules/Databases/Http/Databases/Update.php
+++ src/Appwrite/Platform/Modules/Databases/Http/Databases/Update.php
@@ -74,10 +74,8 @@ class Update extends Action
             throw new Exception(Exception::DATABASE_NOT_FOUND, params: [$databaseId]);
         }
 
-        $searchName = $name ?: $database->getAttribute('name');
+        $searchName = $name ?? $database->getAttribute('name');
 
-        // Persist only the attributes this endpoint changes so concurrent
-        // updates to the database aren't clobbered by writing the full document.
         $updates = new Document([
             'enabled' => $enabled,
             'search' => implode(' ', [$databaseId, $searchName]),
