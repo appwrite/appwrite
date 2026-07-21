9621ce2b8db02aec28ee5afd4a7138f503118de6
Matej Bačo <matejbaco2000@gmail.com>
2026-07-21 17:22:20 +0200

    remove devkey creation from sdks

--- src/Appwrite/Platform/Modules/Projects/Http/DevKeys/Create.php
+++ src/Appwrite/Platform/Modules/Projects/Http/DevKeys/Create.php
@@ -3,10 +3,6 @@
 namespace Appwrite\Platform\Modules\Projects\Http\DevKeys;
 
 use Appwrite\Extend\Exception;
-use Appwrite\SDK\AuthType;
-use Appwrite\SDK\ContentType;
-use Appwrite\SDK\Method;
-use Appwrite\SDK\Response as SDKResponse;
 use Appwrite\Utopia\Response;
 use Utopia\Database\Database;
 use Utopia\Database\Document;
@@ -32,22 +28,6 @@ class Create extends Action
             ->desc('Create dev key')
             ->groups(['api', 'projects'])
             ->label('scope', 'devKeys.write')
-            ->label('sdk', new Method(
-                namespace: 'projects',
-                group: 'devKeys',
-                name: 'createDevKey',
-                description: <<<EOT
-                Create a new project dev key. Dev keys are project specific and allow you to bypass rate limits and get better error logging during development. Strictly meant for development purposes only.
-                EOT,
-                auth: [AuthType::ADMIN],
-                responses: [
-                    new SDKResponse(
-                        code: Response::STATUS_CODE_CREATED,
-                        model: Response::MODEL_DEV_KEY
-                    )
-                ],
-                contentType: ContentType::JSON
-            ))
             ->param('projectId', '', fn (Database $dbForProject) => new UID($dbForProject->getAdapter()->getMaxUIDLength()), 'Project unique ID.', false, ['dbForProject'])
             ->param('name', null, new Text(128), 'Key name. Max length: 128 chars.')
             ->param('expire', null, new DatetimeValidator(), 'Expiration time in [ISO 8601](https://www.iso.org/iso-8601-date-and-time-format.html) format.', false)
