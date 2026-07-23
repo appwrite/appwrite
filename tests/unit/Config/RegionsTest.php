6f7c804fadf0b865635b57a8baa76a3234068c07
Ibaraki Douji <ibaraki@ibaraki.app>
2026-07-23 22:10:43 +0200

    Update environment variables and documentation for multi-region support

--- tests/unit/Config/RegionsTest.php
+++ tests/unit/Config/RegionsTest.php
@@ -9,7 +9,36 @@ use PHPUnit\Framework\TestCase;
 
 final class RegionsTest extends TestCase
 {
-    public function testParseCatalog(): void
+    public function testParseAppwriteRegionsCatalog(): void
+    {
+        $catalog = Regions::parse(\json_encode([
+            'default' => [
+                '$id' => 'default',
+                'name' => 'Default',
+                'disabled' => true,
+                'default' => false,
+            ],
+            'fra' => [
+                '$id' => 'fra',
+                'name' => 'Frankfurt',
+                'disabled' => false,
+                'default' => true,
+            ],
+            'nyc' => [
+                '$id' => 'nyc',
+                'name' => 'New York',
+                'disabled' => false,
+                'default' => false,
+            ],
+        ]));
+
+        $this->assertEquals(['default', 'fra', 'nyc'], \array_keys($catalog));
+        $this->assertTrue($catalog['fra']['default']);
+        $this->assertTrue($catalog['default']['disabled']);
+        $this->assertEquals('New York', $catalog['nyc']['name']);
+    }
+
+    public function testParseCustomRegionsCatalog(): void
     {
         $catalog = Regions::parse(\json_encode([
             'default' => [
@@ -34,7 +63,6 @@ final class RegionsTest extends TestCase
 
         $this->assertEquals(['default', 'france', 'japan'], \array_keys($catalog));
         $this->assertTrue($catalog['france']['default']);
-        $this->assertTrue($catalog['default']['disabled']);
         $this->assertEquals('Japan', $catalog['japan']['name']);
     }
 
@@ -42,15 +70,15 @@ final class RegionsTest extends TestCase
     {
         $catalog = Regions::parse(\json_encode([
             [
-                '$id' => 'eu-west-1',
-                'name' => 'EU West',
+                '$id' => 'sfo',
+                'name' => 'San Francisco',
                 'disabled' => false,
                 'default' => true,
             ],
         ]));
 
-        $this->assertArrayHasKey('eu-west-1', $catalog);
-        $this->assertTrue($catalog['eu-west-1']['default']);
+        $this->assertArrayHasKey('sfo', $catalog);
+        $this->assertTrue($catalog['sfo']['default']);
     }
 
     public function testParseRejectsInvalidJson(): void
@@ -63,9 +91,9 @@ final class RegionsTest extends TestCase
     {
         $this->expectException(\InvalidArgumentException::class);
         Regions::parse(\json_encode([
-            'France' => [
-                '$id' => 'France',
-                'name' => 'France',
+            'FRA' => [
+                '$id' => 'FRA',
+                'name' => 'Frankfurt',
                 'default' => true,
             ],
         ]));
@@ -75,36 +103,51 @@ final class RegionsTest extends TestCase
     {
         $this->expectException(\InvalidArgumentException::class);
         Regions::parse(\json_encode([
-            'france' => [
-                '$id' => 'france',
-                'name' => 'France',
+            'fra' => [
+                '$id' => 'fra',
+                'name' => 'Frankfurt',
                 'default' => true,
             ],
-            'japan' => [
-                '$id' => 'japan',
-                'name' => 'Japan',
+            'nyc' => [
+                '$id' => 'nyc',
+                'name' => 'New York',
                 'default' => true,
             ],
         ]));
     }
 
-    public function testPoolKeyMatchesRegion(): void
+    public function testPoolKeyMatchesAppwriteRegions(): void
+    {
+        $this->assertTrue(Regions::poolKeyMatchesRegion('database_db_fra_main', 'fra'));
+        $this->assertTrue(Regions::poolKeyMatchesRegion('database_db_nyc_main', 'nyc'));
+        $this->assertFalse(Regions::poolKeyMatchesRegion('database_db_frankfurt_main', 'fra'));
+        $this->assertFalse(Regions::poolKeyMatchesRegion('database_db_nyc_main', 'fra'));
+    }
+
+    public function testPoolKeyMatchesCustomRegions(): void
     {
         $this->assertTrue(Regions::poolKeyMatchesRegion('database_db_france_main', 'france'));
+        $this->assertTrue(Regions::poolKeyMatchesRegion('database_db_japan_main', 'japan'));
         $this->assertTrue(Regions::poolKeyMatchesRegion('database_db_eu-west-1_main', 'eu-west-1'));
         $this->assertFalse(Regions::poolKeyMatchesRegion('database_db_franceville_main', 'france'));
         $this->assertFalse(Regions::poolKeyMatchesRegion('database_db_australia_main', 'us'));
         $this->assertFalse(Regions::poolKeyMatchesRegion('database_db_france_main', 'fra'));
     }
 
-    public function testFilterPoolKeysForRegion(): void
+    public function testFilterPoolKeysForAppwriteAndCustomRegions(): void
     {
         $keys = [
+            'database_db_fra_main',
+            'database_db_nyc_main',
             'database_db_france_main',
             'database_db_japan_main',
-            'database_db_franceville_main',
+            'database_db_frankfurt_main',
         ];
 
+        $this->assertEquals(
+            ['database_db_fra_main'],
+            Regions::filterPoolKeysForRegion($keys, 'fra')
+        );
         $this->assertEquals(
             ['database_db_france_main'],
             Regions::filterPoolKeysForRegion($keys, 'france')
