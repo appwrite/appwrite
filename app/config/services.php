8cbad640a1cd16fcf52b63d8a192ab18b9336c80
harsh mahajan <harshmahajan2345@gmail.com>
2026-08-04 15:35:11 +0530

    Revert "refactor(users): drop redundant controller comment"
    This reverts commit 8b3be566b80240879296ed90282703d9c92ce526.

--- app/config/services.php
+++ app/config/services.php
@@ -174,7 +174,7 @@ return [
         'name' => 'Users',
         'subtitle' => 'The Users service allows you to manage your project users.',
         'description' => '/docs/services/users.md',
-        'controller' => '',
+        'controller' => '', // Uses modules
         'sdk' => true,
         'docs' => true,
         'docsUrl' => 'https://appwrite.io/docs/server/users',
