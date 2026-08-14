aff1e0b2bbb9ae1807d5c6ab719fdb99fb63e844
Jake Barnby <jakeb994@gmail.com>
2026-08-14 23:26:47 +1200

    fix: expose Mongo attribute support and bump database
    Mongo structure checks were off because DefinedAttributes ignored the
    adapter's support flag. Console tests then treated Mongo as schemaless
    and expected 400s that never fired.

--- src/Appwrite/Platform/Modules/Console/Http/Variables/Get.php
+++ src/Appwrite/Platform/Modules/Console/Http/Variables/Get.php
@@ -97,6 +97,7 @@ class Get extends Action
             '_APP_OPTIONS_FORCE_HTTPS' => System::getEnv('_APP_OPTIONS_FORCE_HTTPS'),
             '_APP_DOMAINS_NAMESERVERS' => System::getEnv('_APP_DOMAINS_NAMESERVERS'),
             '_APP_DB_ADAPTER' => System::getEnv('_APP_DB_ADAPTER', 'mariadb'),
+            'supportForAttributes' => $adapter->supports(Capability::DefinedAttributes),
             'supportForRelationships' => $adapter->hasFeature(FeatureRelationships::class),
             'supportForOperators' => $adapter->supports(Capability::Operators),
             'supportForSpatials' => $adapter->supports(Capability::SpatialIndexNull)
