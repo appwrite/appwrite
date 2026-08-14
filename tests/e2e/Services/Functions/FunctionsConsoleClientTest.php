80770287ce4e080d23607d30e213586adce0e20c
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-08-14 12:26:07 +0530

    fix(tests): call variables endpoint directly

--- tests/e2e/Services/Functions/FunctionsConsoleClientTest.php
+++ tests/e2e/Services/Functions/FunctionsConsoleClientTest.php
@@ -281,7 +281,10 @@ final class FunctionsConsoleClientTest extends Scope
         /**
          * Test for FAILURE
          */
-        $response = $this->listVariables($data['functionId'], [
+        $response = $this->client->call(Client::METHOD_GET, '/functions/' . $data['functionId'] . '/variables', array_merge([
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+        ], $this->getHeaders()), [
             'queries' => [
                 Query::search('key', 'APP_TEST')->toString(),
             ],
