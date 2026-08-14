b41981e6f3424173b607da1a2558dc568d5ebb11
Jake Barnby <jakeb994@gmail.com>
2026-08-15 06:06:45 +1200

    test: reject union and time-bucket group queries

--- tests/e2e/Services/Databases/QueryRejectedMethods.php
+++ tests/e2e/Services/Databases/QueryRejectedMethods.php
@@ -2,6 +2,76 @@
 
 namespace Tests\E2E\Services\Databases;
 
+use Tests\E2E\Client;
+use Utopia\Database\Query;
+
 trait QueryRejectedMethods
 {
+    public function testGroupByIntervalRejected(): void
+    {
+        $data = $this->setupDocuments();
+        $databaseId = $data['databaseId'];
+
+        $result = $this->client->call(Client::METHOD_GET, $this->getRecordUrl($databaseId, $data['moviesId']), array_merge([
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+        ], $this->getHeaders()), [
+            'queries' => ['{"method":"groupByInterval","attribute":"birthDay","values":["1h"]}'],
+        ]);
+
+        $this->assertSame(400, $result['headers']['status-code']);
+        $this->assertSame('general_query_invalid', $result['body']['type']);
+    }
+
+    public function testGroupByTimeBucketRejected(): void
+    {
+        $data = $this->setupDocuments();
+        $databaseId = $data['databaseId'];
+
+        $result = $this->client->call(Client::METHOD_GET, $this->getRecordUrl($databaseId, $data['moviesId']), array_merge([
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+        ], $this->getHeaders()), [
+            'queries' => ['{"method":"groupByTimeBucket","attribute":"birthDay","values":["1h"]}'],
+        ]);
+
+        $this->assertSame(400, $result['headers']['status-code']);
+        $this->assertSame('general_query_invalid', $result['body']['type']);
+    }
+
+    public function testUnionRejected(): void
+    {
+        $data = $this->setupAnalyticsFixture();
+        $databaseId = $data['databaseId'];
+
+        $result = $this->client->call(Client::METHOD_GET, $this->getRecordUrl($databaseId, $data['ordersId']), array_merge([
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+        ], $this->getHeaders()), [
+            'queries' => [
+                Query::union([Query::equal('status', ['paid'])])->toString(),
+            ],
+        ]);
+
+        $this->assertSame(400, $result['headers']['status-code']);
+        $this->assertSame('general_query_invalid', $result['body']['type']);
+    }
+
+    public function testUnionAllRejected(): void
+    {
+        $data = $this->setupAnalyticsFixture();
+        $databaseId = $data['databaseId'];
+
+        $result = $this->client->call(Client::METHOD_GET, $this->getRecordUrl($databaseId, $data['ordersId']), array_merge([
+            'content-type' => 'application/json',
+            'x-appwrite-project' => $this->getProject()['$id'],
+        ], $this->getHeaders()), [
+            'queries' => [
+                Query::unionAll([Query::equal('status', ['paid'])])->toString(),
+            ],
+        ]);
+
+        $this->assertSame(400, $result['headers']['status-code']);
+        $this->assertSame('general_query_invalid', $result['body']['type']);
+    }
 }
