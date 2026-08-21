39f2cfe662f9fba0568ec1419abd0605040d4bcc
Jake Barnby <jakeb994@gmail.com>
2026-08-21 23:25:01 +1200

    (fix): use ColumnType::BigInteger for bigint attribute type

--- src/Appwrite/Platform/Modules/Databases/Http/Databases/Collections/Attributes/BigInt/Create.php
+++ src/Appwrite/Platform/Modules/Databases/Http/Databases/Collections/Attributes/BigInt/Create.php
@@ -17,6 +17,7 @@ use Utopia\Database\Validator\Authorization;
 use Utopia\Database\Validator\Key;
 use Utopia\Database\Validator\UID;
 use Utopia\Http\Adapter\Swoole\Response as SwooleResponse;
+use Utopia\Query\Schema\ColumnType;
 use Utopia\Validator\Boolean;
 use Utopia\Validator\Integer;
 use Utopia\Validator\Nullable;
@@ -96,7 +97,7 @@ class Create extends Action
 
         $attribute = $this->createAttribute($databaseId, $collectionId, new Document([
             'key' => $key,
-            'type' => 'bigint',
+            'type' => ColumnType::BigInteger->value,
             'size' => 8,
             'required' => $required,
             'default' => $default,
