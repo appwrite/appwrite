dfb64da9ab5e2f19f0019f0c0f7b5d8b11cccc43
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-08-24 11:32:16 +0530

    added runtime deps in the broker

--- src/Utopia/Mqtt/Broker.php
+++ src/Utopia/Mqtt/Broker.php
@@ -2,7 +2,176 @@
 
 namespace Utopia\Mqtt;
 
+use Appwrite\Utopia\Database\Documents\User;
+use Swoole\Coroutine;
+use Swoole\Runtime;
 use Swoole\Server;
+use Utopia\Cache\Adapter\Pool as CachePool;
+use Utopia\Cache\Adapter\Sharding;
+use Utopia\Cache\Cache;
+use Utopia\Config\Config;
+use Utopia\Database\Adapter\Pool as DatabasePool;
+use Utopia\Database\Database;
+use Utopia\Database\Document;
+use Utopia\DSN\DSN;
+use Utopia\System\System;
+
+// Currently using the realtime init only as its matching the requirements
+// TODO: scope the connection init to a separate mqtt connection
+require_once __DIR__ . '/../../../app/init.php';
+
+if (System::getEnv('_APP_EDITION', 'self-hosted') === 'self-hosted') {
+    require_once __DIR__ . '/../../../app/init/span.php';
+}
+
+/** @var Registry $register */
+$register = $GLOBALS['register'] ?? throw new \RuntimeException('Registry not initialized');
+
+$registerConnectionResources ??= require __DIR__ . '/../../../app/init/realtime/connection.php';
+
+Runtime::enableCoroutine(SWOOLE_HOOK_ALL);
+
+
+// Allows overriding
+if (!function_exists('getConsoleDB')) {
+    function getConsoleDB(): Database
+    {
+        $ctx = Coroutine::getContext();
+
+        if (isset($ctx['dbForPlatform'])) {
+            return $ctx['dbForPlatform'];
+        }
+
+        global $register;
+
+        /** @var Group $pools */
+        $pools = $register->get('pools');
+
+        $adapter = new DatabasePool($pools->get('console'));
+        $database = new Database($adapter, getCache());
+        $database
+            ->setDatabase(APP_DATABASE)
+            ->setNamespace('_console')
+            ->setMetadata('host', \gethostname())
+            ->setMetadata('project', '_console');
+        $database->setDocumentType('users', User::class);
+        return $ctx['dbForPlatform'] = $database;
+    }
+}
+
+// Allows overriding
+if (!function_exists('getProjectDB')) {
+    function getProjectDB(Document $project): Database
+    {
+        $ctx = Coroutine::getContext();
+
+        if (!isset($ctx['dbForProject'])) {
+            $ctx['dbForProject'] = [];
+        }
+
+        if (isset($ctx['dbForProject'][$project->getSequence()])) {
+            return $ctx['dbForProject'][$project->getSequence()];
+        }
+
+        global $register;
+
+        /** @var Group $pools */
+        $pools = $register->get('pools');
+
+        if ($project->isEmpty() || $project->getId() === 'console') {
+            return getConsoleDB();
+        }
+
+        try {
+            $dsn = new DSN($project->getAttribute('database'));
+        } catch (\InvalidArgumentException) {
+            // TODO: Temporary until all projects are using shared tables
+            $dsn = new DSN('mysql://' . $project->getAttribute('database'));
+        }
+
+        $adapter = new DatabasePool($pools->get($dsn->getHost()));
+        $database = new Database($adapter, getCache());
+
+        $sharedTables = \explode(',', System::getEnv('_APP_DATABASE_SHARED_TABLES', ''));
+
+        if (\in_array($dsn->getHost(), $sharedTables)) {
+            $collections = Config::getParam('collections', []);
+            $projectCollections = $collections['projects'] ?? [];
+            $projectsGlobalCollections = array_keys($projectCollections);
+            $projectsGlobalCollections[] = 'audit';
+
+            $database
+                ->setSharedTables(true)
+                ->setGlobalCollections($projectsGlobalCollections)
+                ->setTenant($project->getSequence())
+                ->setNamespace($dsn->getParam('namespace'));
+        } else {
+            $database
+                ->setSharedTables(false)
+                ->setTenant(null)
+                ->setNamespace('_' . $project->getSequence());
+        }
+
+        $database
+            ->setDatabase(APP_DATABASE)
+            ->setMetadata('host', \gethostname())
+            ->setMetadata('project', $project->getId());
+
+        $database->setDocumentType('users', User::class);
+
+        return $ctx['dbForProject'][$project->getSequence()] = $database;
+    }
+}
+
+// Allows overriding
+if (!function_exists('getCache')) {
+    function getCache(): Cache
+    {
+        $ctx = Coroutine::getContext();
+
+        if (isset($ctx['cache'])) {
+            return $ctx['cache'];
+        }
+
+        global $register;
+
+        $pools = $register->get('pools'); /** @var Group $pools */
+
+        $list = Config::getParam('pools-cache', []);
+        $adapters = [];
+
+        foreach ($list as $value) {
+            $adapters[] = new CachePool($pools->get($value));
+        }
+
+        return $ctx['cache'] = new Cache(new Sharding($adapters));
+    }
+}
+
+// Allows overriding
+if (!function_exists('getRedis')) {
+    function getRedis(): \Redis
+    {
+        $ctx = Coroutine::getContext();
+
+        if (isset($ctx['redis'])) {
+            return $ctx['redis'];
+        }
+
+        $host = System::getEnv('_APP_REDIS_HOST', 'localhost');
+        $port = System::getEnv('_APP_REDIS_PORT', 6379);
+        $pass = System::getEnv('_APP_REDIS_PASS', '');
+
+        $redis = new \Redis();
+        @$redis->pconnect($host, (int)$port);
+        if ($pass) {
+            $redis->auth($pass);
+        }
+        $redis->setOption(\Redis::OPT_READ_TIMEOUT, -1);
+
+        return $ctx['redis'] = $redis;
+    }
+}
 
 class Broker
 {
