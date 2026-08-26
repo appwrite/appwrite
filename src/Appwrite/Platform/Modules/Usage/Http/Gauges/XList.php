0d6fbeb68b02e0d192387aa74691c5dbd334cf3b
Damodar Lohani <lohanidamodar@users.noreply.github.com>
2026-08-26 12:16:30 +0545

    Revert "chore: bump utopia-php/audit to 4.0.0 and utopia-php/usage to 0.15.0"

--- src/Appwrite/Platform/Modules/Usage/Http/Gauges/XList.php
+++ src/Appwrite/Platform/Modules/Usage/Http/Gauges/XList.php
@@ -12,7 +12,6 @@ use Appwrite\Utopia\Response;
 use Utopia\Database\Document;
 use Utopia\Database\Validator\Datetime as DatetimeValidator;
 use Utopia\Platform\Enum;
-use Utopia\Query\Method as QueryMethod;
 use Utopia\Query\Query;
 use Utopia\System\System;
 use Utopia\Usage\Tenant;
@@ -50,16 +49,16 @@ class XList extends Action
     protected const VALID_FILTER_ATTRIBUTES = ['service', 'resourceType', 'resourceId', 'ordinal'];
 
     /**
-     * Query methods supported on the filter surface. Gauges only
+     * Query::TYPE_* values supported on the filter surface. Gauges only
      * carry short low-cardinality string dimensions, so equality + null
      * presence is what makes sense; we skip contains/startsWith here since
      * they're rarely useful on values like 'bucket' / 'function'.
      */
     protected const VALID_FILTER_METHODS = [
-        QueryMethod::Equal,
-        QueryMethod::NotEqual,
-        QueryMethod::IsNull,
-        QueryMethod::IsNotNull,
+        Query::TYPE_EQUAL,
+        Query::TYPE_NOT_EQUAL,
+        Query::TYPE_IS_NULL,
+        Query::TYPE_IS_NOT_NULL,
     ];
 
     public static function getName(): string
