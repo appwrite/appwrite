6caa879ca6c5614de7a196ddaae4909cdc62ab97
harsh mahajan <harshmahajan2345@gmail.com>
2026-09-02 20:16:59 +0530

    List installation requests without query support
    A project holds at most one request per requester, so there is nothing to
    page or filter server-side; the queries param and its validator only added
    cursor and parsing plumbing for a list that fits in one page.

--- src/Appwrite/Platform/Modules/VCS/Http/Requests/XList.php
+++ src/Appwrite/Platform/Modules/VCS/Http/Requests/XList.php
@@ -2,19 +2,14 @@
 
 namespace Appwrite\Platform\Modules\VCS\Http\Requests;
 
-use Appwrite\Extend\Exception;
 use Appwrite\Platform\Action;
 use Appwrite\SDK\AuthType;
 use Appwrite\SDK\Method;
 use Appwrite\SDK\Response as SDKResponse;
-use Appwrite\Utopia\Database\Validator\Queries\InstallationRequests;
 use Appwrite\Utopia\Response;
 use Utopia\Database\Database;
 use Utopia\Database\Document;
-use Utopia\Database\Exception\Order as OrderException;
-use Utopia\Database\Exception\Query as QueryException;
 use Utopia\Database\Query;
-use Utopia\Database\Validator\Query\Cursor;
 use Utopia\Platform\Scope\HTTP;
 use Utopia\Validator\Boolean;
 
@@ -49,7 +44,6 @@ class XList extends Action
                     )
                 ]
             ))
-            ->param('queries', [], new InstallationRequests(), 'Array of query strings generated using the Query class provided by the SDK. [Learn more about queries](https://appwrite.io/docs/queries). Maximum of ' . APP_LIMIT_ARRAY_PARAMS_SIZE . ' queries are allowed, each ' . APP_LIMIT_ARRAY_ELEMENT_SIZE . ' characters long. You may filter on the following attributes: ' . implode(', ', InstallationRequests::ALLOWED_ATTRIBUTES), true)
             ->param('total', true, new Boolean(true), 'When set to false, the total count returned will be 0 and will not be calculated.', true)
             ->inject('response')
             ->inject('project')
@@ -58,53 +52,16 @@ class XList extends Action
     }
 
     public function action(
-        array $queries,
         bool $includeTotal,
         Response $response,
         Document $project,
         Database $dbForPlatform
     ) {
-        try {
-            $queries = Query::parseQueries($queries);
-        } catch (QueryException $e) {
-            throw new Exception(Exception::GENERAL_QUERY_INVALID, $e->getMessage());
-        }
-
-        $queries[] = Query::equal('projectInternalId', [$project->getSequence()]);
-
-        $cursor = \array_filter($queries, function ($query) {
-            return \in_array($query->getMethod(), [Query::TYPE_CURSOR_AFTER, Query::TYPE_CURSOR_BEFORE]);
-        });
-        $cursor = reset($cursor);
-        if ($cursor) {
-            /** @var Query $cursor */
-
-            $validator = new Cursor();
-            if (!$validator->isValid($cursor)) {
-                throw new Exception(Exception::GENERAL_QUERY_INVALID, $validator->getDescription());
-            }
-
-            $requestId = $cursor->getValue();
-            $cursorDocument = $dbForPlatform->getDocument('installationRequests', $requestId);
-
-            if ($cursorDocument->isEmpty()) {
-                throw new Exception(Exception::GENERAL_CURSOR_NOT_FOUND, "Installation request '{$requestId}' for the 'cursor' value not found.");
-            }
-
-            $cursor->setValue($cursorDocument);
-        }
-
-        $filterQueries = Query::groupByType($queries)['filters'];
-        try {
-            $results = $dbForPlatform->find('installationRequests', $queries);
-            $total = $includeTotal ? $dbForPlatform->count('installationRequests', $filterQueries, APP_LIMIT_COUNT) : 0;
-        } catch (OrderException $e) {
-            throw new Exception(Exception::DATABASE_QUERY_ORDER_NULL, "The order attribute '{$e->getAttribute()}' had a null value. Cursor pagination requires all documents order attribute values are non-null.");
-        }
+        $queries = [Query::equal('projectInternalId', [$project->getSequence()])];
 
         $response->dynamic(new Document([
-            'requests' => $results,
-            'total' => $total,
+            'requests' => $dbForPlatform->find('installationRequests', $queries),
+            'total' => $includeTotal ? $dbForPlatform->count('installationRequests', $queries, APP_LIMIT_COUNT) : 0,
         ]), Response::MODEL_INSTALLATION_REQUEST_LIST);
     }
 }
