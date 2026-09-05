b72ed992bc40558d55b9fc82b47d6428dddd5fd2
loks0n <22452787+loks0n@users.noreply.github.com>
2026-09-05 14:16:12 +0100

    refactor: remove redundant CLI task span attribute

--- app/cli.php
+++ app/cli.php
@@ -188,7 +188,6 @@ $cli
     ->inject('error')
     ->action(function (Throwable $error) use ($taskName, &$exitCode) {
         $span = Span::current() ?? Span::init("task.$taskName");
-        Span::add('cli.task', $taskName);
         $span->finish(error: $error);
 
         $exitCode = 1;
@@ -197,7 +196,6 @@ $cli
 
 $cli->init()->action(function () use ($taskName) {
     Span::init("task.$taskName");
-    Span::add('cli.task', $taskName);
 });
 
 $cli->shutdown()->action(function () {
