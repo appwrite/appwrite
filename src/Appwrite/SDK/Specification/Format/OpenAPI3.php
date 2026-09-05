5ba4e15a31bb0ce9ffdc182007a1b6f274b93270
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-09-05 15:51:46 +0100

    feat: remove OpenAPI cookies extension

--- src/Appwrite/SDK/Specification/Format/OpenAPI3.php
+++ src/Appwrite/SDK/Specification/Format/OpenAPI3.php
@@ -279,7 +279,6 @@ class OpenAPI3 extends Format
                 'deprecated' => $sdk->isDeprecated(),
                 'x-appwrite' => [ // Appwrite related metadata
                     'group' => $sdk->getGroup(),
-                    'cookies' => $route->getLabel('sdk.cookies', false),
                     'demo' => \strtolower($namespace) . '/' . Template::fromCamelCaseToDash($methodName) . '.md',
                     'rate-limit' => $route->getLabel('abuse-limit', 0),
                     'rate-time' => $route->getLabel('abuse-time', 3600),
