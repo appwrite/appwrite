52ba95b2c2ab381eff125c7a819e54e67a44f7c9
Torsten Dittmann <torsten.dittmann@proton.me>
2026-09-09 18:50:31 +0400

    test: remove Autogravity version pin assertion

--- tests/unit/Docker/Compose/GeneratorTest.php
+++ tests/unit/Docker/Compose/GeneratorTest.php
@@ -69,7 +69,6 @@ final class GeneratorTest extends TestCase
         $this->assertArrayHasKey('appwrite-task-interval', $compose['services']);
         $this->assertArrayHasKey('appwrite-embedding', $compose['services']);
         $this->assertArrayHasKey('appwrite-autogravity', $compose['services']);
-        $this->assertSame('ghcr.io/appwrite/autogravity:0.0.8', $compose['services']['appwrite-autogravity']['image']);
         $this->assertArrayNotHasKey('profiles', $compose['services']['appwrite-worker']);
         $this->assertArrayNotHasKey('profiles', $compose['services']['appwrite-task-scheduler']);
     }
