443202eedfb041266d8e17768b45949b63a99ad8
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-09-09 23:55:24 +0100

    test: isolate users pagination fixtures without relaxing cursor assertions

--- tests/e2e/Services/Users/UsersBase.php
+++ tests/e2e/Services/Users/UsersBase.php
@@ -935,12 +935,12 @@ trait UsersBase
 
     public function testListUsers(): void
     {
+        // Cursor assertions require users created in order, without earlier test data.
+        self::$project = $this->getProject(true);
         $data = $this->setupUser();
         $this->setupUser1();
         $this->setupHashedPasswordUsers();
-        // In --functional mode, this test runs independently with 9 users created above
-        // (setupUser: 1 + setupUser1: 1 + setupHashedPasswordUsers: 7)
-        // In sequential mode, there may be more users from other tests
+        // setupUser: 1 + setupUser1: 1 + setupHashedPasswordUsers: 7
         $minUsers = 9;
 
         /**
