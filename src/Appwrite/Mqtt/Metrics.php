4a4b0fb374987a64ea3ace0ca290e8cb0d94050a
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-09-14 10:22:51 +0530

    feat(mqtt): add broker telemetry instruments
    Declare histograms for auth/connection duration and message size, counters for
    bytes received/sent, reauth {result}, and puback received.

--- src/Appwrite/Mqtt/Metrics.php
+++ src/Appwrite/Mqtt/Metrics.php
@@ -4,6 +4,7 @@ namespace Appwrite\Mqtt;
 
 use Utopia\Telemetry\Adapter as Telemetry;
 use Utopia\Telemetry\Counter;
+use Utopia\Telemetry\Histogram;
 use Utopia\Telemetry\UpDownCounter;
 
 /**
@@ -22,6 +23,13 @@ class Metrics
     public Counter $messagesDelivered;
     public Counter $messagesDropped;
     public Counter $messagesAcked;
+    public Counter $pubacksReceived;
+    public Counter $reauth;
+    public Counter $bytesReceived;
+    public Counter $bytesSent;
+    public Histogram $authDuration;
+    public Histogram $connectionDuration;
+    public Histogram $messageSize;
 
     public function __construct(Telemetry $telemetry)
     {
@@ -32,5 +40,12 @@ class Metrics
         $this->messagesDelivered = $telemetry->createCounter('mqtt.messages.delivered');
         $this->messagesDropped = $telemetry->createCounter('mqtt.messages.dropped');
         $this->messagesAcked = $telemetry->createCounter('mqtt.messages.acked');
+        $this->pubacksReceived = $telemetry->createCounter('mqtt.puback.received');
+        $this->reauth = $telemetry->createCounter('mqtt.reauth');
+        $this->bytesReceived = $telemetry->createCounter('mqtt.bytes.received', 'By');
+        $this->bytesSent = $telemetry->createCounter('mqtt.bytes.sent', 'By');
+        $this->authDuration = $telemetry->createHistogram('mqtt.auth.duration', 's');
+        $this->connectionDuration = $telemetry->createHistogram('mqtt.connection.duration', 's');
+        $this->messageSize = $telemetry->createHistogram('mqtt.message.size', 'By');
     }
 }
