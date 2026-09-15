264f03038d18003527166819bcd5d006f815b6e9
harsh mahajan <harshmahajan2345@gmail.com>
2026-09-15 14:53:04 +0530

    chore(variables): introduce _APP_CONSOLE_URL in 2.2.1

--- app/config/variables.php
+++ app/config/variables.php
@@ -244,7 +244,7 @@ return [
             [
                 'name' => '_APP_CONSOLE_URL',
                 'description' => 'Origin of the Appwrite console web app, such as https://console.example.com, used when the backend generates links to the console (OAuth callbacks, emails, error page CTAs, VCS comments and commit statuses). Set it when the console is served on a different host than the API. When empty, links use _APP_CONSOLE_DOMAIN (or _APP_DOMAIN) over https, or over http when _APP_OPTIONS_FORCE_HTTPS is disabled.',
-                'introduction' => '2.1.1',
+                'introduction' => '2.2.1',
                 'default' => '',
                 'required' => false,
                 'question' => '',
