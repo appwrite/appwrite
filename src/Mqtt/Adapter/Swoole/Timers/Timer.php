51b38807c50a309cbb18e3d85f5f2dec0fa2c61d
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-09-17 18:54:20 +0530

    feat: add Timer interface and Keepalive-backed TimingWheel

---
+++ src/Mqtt/Adapter/Swoole/Timers/Timer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Utopia\Mqtt\Adapter\Swoole\Timers;
+
+use Utopia\Mqtt\Adapter;
+
+interface Timer
+{
+    public function schedule(int $id, float $expiresAt): void;
+
+    public function remove(int $id): void;
+
+    public function onExpire(callable $callback): void;
+
+    public function start(Adapter $adapter): void;
+}
---
+++ src/Mqtt/Adapter/Swoole/Timers/TimingWheel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Utopia\Mqtt\Adapter\Swoole\Timers;
+
+use Utopia\Mqtt\Adapter;
+use Utopia\Mqtt\Keepalive;
+
+class TimingWheel implements Timer
+{
+    /** @var callable|null */
+    private $onExpire = null;
+
+    public function __construct(private readonly Keepalive $wheel = new Keepalive())
+    {
+    }
+
+    public function schedule(int $id, float $expiresAt): void
+    {
+        $this->wheel->schedule($id, $expiresAt);
+    }
+
+    public function remove(int $id): void
+    {
+        $this->wheel->remove($id);
+    }
+
+    public function onExpire(callable $callback): void
+    {
+        $this->onExpire = $callback;
+    }
+
+    public function start(Adapter $adapter): void
+    {
+        $adapter->tick($this->wheel->interval, function (): void {
+            foreach ($this->wheel->drain((int) \microtime(true)) as $id) {
+                if ($this->onExpire !== null) {
+                    \call_user_func($this->onExpire, $id);
+                }
+            }
+        });
+    }
+}
