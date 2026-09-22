29fc24b7a788060dd1c95c7b838ef800efeb01bd
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-09-22 12:36:56 +0100

    style: format mongo-init.js with prettier
    Prettier flags the file on every commit that stages it, which blocks
    merging main into feature branches; quotes and trailing newline only.

--- mongo-init.js
+++ mongo-init.js
@@ -1,7 +1,7 @@
 // mongo-init.js
 
 // Switch to the admin database
-const adminDb = db.getSiblingDB('admin');
+const adminDb = db.getSiblingDB("admin");
 
 // Get username and password from environment variables
 const username = process.env.MONGO_INITDB_USERNAME;
@@ -12,7 +12,5 @@ const database = process.env.MONGO_INITDB_DATABASE;
 adminDb.createUser({
   user: username,
   pwd: password,
-  roles: [
-    { role: 'readWrite', db: database }
-  ]
+  roles: [{ role: "readWrite", db: database }],
 });
